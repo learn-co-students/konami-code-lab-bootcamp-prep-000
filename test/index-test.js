acad92514dc5854d310929c670d2e875288321a1
Charles Pletcher <cpletcher2@gmail.com>
2016-07-26 16:16:31 -0400

    Update tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,20 +1,5 @@
-const chai = require('chai')
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const spies = require('chai-spies')
-
-chai.use(spies)
-
-const expect = chai.expect
-
 describe('index', () => {
-  const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-  const src = fs.readFileSync(path.resolve(__dirname, '..', 'konami_code.js'), 'utf-8')
-
   const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-  jsdom({ html, src })
 
   function triggerKeyDown(which) {
     const keyboardEvent = document.createEvent("KeyboardEvent")
@@ -38,25 +23,25 @@ describe('index', () => {
     it('triggers an alert if the right code is entered', () => {
       init()
 
-      window.alert = chai.spy()
+      window.alert = expect.createSpy()
 
       for (let i = 0, l = code.length; i < l; i++) {
         triggerKeyDown(code[i])
       }
 
-      expect(window.alert).to.have.been.called.once
+      expect(window.alert).toHaveBeenCalled()
     })
 
     it('does not trigger an alert if the wrong code is entered', () => {
       init()
 
-      window.alert = chai.spy()
+      window.alert = expect.createSpy()
 
       for (let i = 0, l = code.length; i < l; i++) {
         triggerKeyDown(i)
       }
 
-      expect(window.alert).not.to.have.been.called
+      expect(window.alert).toNotHaveBeenCalled()
     })
   })
 })
