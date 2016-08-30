0b7a26b52bb0dcbaa0a8acc005425f5e326f599f
Frankie Ferreira <frankiemferreira@gmail.com>
2016-08-30 17:41:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
+
 function init() {
-  // your code here
+  var testing = []
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail)
+
+    if (key === 38 || 40 || 37 || 39 || 66 || 65) {
+      testing.push(key)
+
+      if (testing.length === 10) {
+        if (testing[0] === 38 && testing[1] === 38 && testing[2] === 40 && testing[3] === 40 && testing[4] === 37 && testing[5] == 39 && testing[6] === 37 && testing[7] === 39 && testing[8] === 66 && testing[9] === 65) {
+        return alert('Konami!')
+      }
+      }
+    }
+  })
 }
