90a07c1dcc8b2c388ca6657efe6bf63a8fa4af47
Dean Moriarty <tomada313@gmail.com>
2016-09-06 05:05:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+function init()
+{
+  var index = 0
+
+  document.body.addEventListener('keydown', function(e)
+  {
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index])
+    {
+        index++
+
+        if (code.length - 1 === index)
+        {
+          alert("YOU DID IT!")
+          index = 0
+        }
+    }
+    else
+    {
+        index = 0
+    }
+  })
 }
