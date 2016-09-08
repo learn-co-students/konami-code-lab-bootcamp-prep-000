fa0c87519b7d7ef95a2b5d336f259ad071f41ee8
Menachem Abraham <menachemabraham@gmail.com>
2016-09-08 04:53:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
 
 function init() {
-  // your code here
+  var body = document.querySelector('body');
+
+  body.addEventListener('keydown',handler)
 }
+
+function handler(e){
+  var key = parseInt(e.detail || e.which);
+
+  if (key === code[index]){
+    index++;
+
+    if (index === code.length - 1){
+      alert("NINJA!!!");
+    }
+  }else{
+    index = 0;
+  }
+}
+
+init();
