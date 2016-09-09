7ee0528a34c242a04c2b12860d912a14655a4a76
David Lewitan <dovidlwtn@gmail.com>
2016-09-09 20:57:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var prog = 0;
+  document.body.addEventListener('keydown',function(e) {
+    if (e.detail == code[prog] || e.which == code[prog]) {
+      prog++;
+    } else {
+      prog = 0;
+    }
+
+    if (prog == code.length) {
+       alert('Surprise!');
+       prog = 0;
+    }
+
+  });
 }
+
+init()
