ee4d9a567d370561a906b589392fd35972f9326b
Rachel Salois <rachel.salois@gmail.com>
2016-09-10 13:19:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
 
 function init() {
-  // your code here
-}
+  function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]) {
+      index++
+      if (index === code.length-1) {
+        alert("congrats!")
+        index = 0
+      }
+      }
+    else {index = 0}
+    }
+  }
