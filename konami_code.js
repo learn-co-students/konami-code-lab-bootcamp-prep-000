ce1d43f3fe3240b859c7462a8703fbd43ffeb907
Jin Wang <jin.wang.527@gmail.com>
2016-09-10 10:18:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index = 0
+
+  document.addEventListener('keydown', onKeyDownHandler)
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.which || e.detail)
+
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert('YOU DID IT!!')
+        index = 0
+      }
+    }
+
+    else {
+      index = 0
+    }
+  }
 }
