b8d26cdc7f52073f405d632b2552dce5d2c42bf3
Andres Duarte <andresduarte010@gmail.com>
2016-09-16 00:58:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-var index = 0
-
-function triggerKeyDown(e) {
-  const key = parseInt(e.detail || e.which)
-  if (key === code[index]) {
+function init(e) {
+  var index = 0
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]) {
       index++
-    if (index === code.length - 1) {
-      window.alert("RIGHT")
+      if (index === code.length - 1) {
+        alert("RIGHT")
+        index = 0
+      }
+    }
+    else {
       index = 0
     }
-  }
-  else {
-    index = 0
-  }
-}
-
-document.addEventListener("keydown", triggerKeyDown())
-
-function init() {
-  
+  })
 }
