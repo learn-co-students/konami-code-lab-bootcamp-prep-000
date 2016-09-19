403f3ef21b43418c087eb74663e64ba79d4bf27e
Rachel Salois <rachel.salois@gmail.com>
2016-09-19 23:03:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var index = 0;
 
 function init() {
+document.body.addEventListener('keydown', onKeyDownHandler)
   function onKeyDownHandler(e){
     const key = parseInt(e.detail || e.which)
     if (key === code[index]) {
