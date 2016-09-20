671936492786d3aea46c8909e07ee0cc94f1cf64
Marine Boudeau <m14868@gmail.com>
2016-09-20 23:16:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
 function init() {
-  // your code here
+  
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++
+   
+      if (index === code.length - 1) {
+        index = 0
+        alert("YOU DID IT!")
+      }
+    } else {
+      index = 0
+    }
+  })
 }
+// 
+// init()
