2f9ad1a0729da5da81b8cea292faa101aee1cc8f
Jonathan McAlpine <j.g.mcalpine@gmail.com>
2016-09-21 18:50:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 var count = 0;
-document.body.addEventListener('keydown', init);
 
-function init(e) {
-  const key = parseInt(e.which || e.detail);
+function init() {
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+  });
   if(key === code[count]) {
     count++;
 
