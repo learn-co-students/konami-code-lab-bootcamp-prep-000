d0e2d684fad25fd1fcadd8e9a4e5045252b3434f
Dhiraj Shah <dhiraj.shah.u@gmail.com>
2016-09-22 13:39:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0;
   document.body.addEventListener('keydown', function(e){
-    var index = 0;
     var key = parseInt(e.which || e.detail);
     console.log(key);
     if (key == codes[index]) {
