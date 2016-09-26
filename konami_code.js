bfe0e2fdc12dcddc66eec42c5945720d076aeff6
Alexander Boring <alexboring@hotmail.com>
2016-09-26 19:12:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const body = document.querySelector('body')
+
+
+
+var i = 0
+body.addEventListener('keydown', function(e) {
+
+ var key = parseInt(e.detail || e.which)
+  if (key === code[i]) {
+    i++
+
+    if (key === code[code.length - 1]) {
+      init()
+      i = 0
+    }
+  } else {
+    i = 0
+  }
+})
 
 function init() {
-  // your code here
+  alert("YOU DID IT!")
 }
