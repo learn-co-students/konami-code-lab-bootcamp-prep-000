7e7ba6b35836d16936040bf1755288a526dd6d92
Justin Mayerick <jmayeric@gmail.com>
2016-10-07 13:39:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,5 +4,17 @@ var index = 0
 
 
 function init() {
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]) {
+      index++
 
+      if (index === code.length - 1) {
+        alert("You are master of the universe!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
 }
