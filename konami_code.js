9258f9bfb962ae99a5a8293d23aac55d0f989b1c
Tayari Appiah <cjhhh.ta@gmail.com>
2016-10-17 17:47:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0
+  document.body.addEventListener('keydown', function(e){
+  let inputCode = parseInt(e.which || e.detail)
+    if(inputCode === code[index]){
+        index++
+      }
+    if(index === code.length - 1){
+      alert("Great job buddy!")
+        index = 0
+    }
+      else{
+        return e.stopPropagation()
+      }
+    })
 }
