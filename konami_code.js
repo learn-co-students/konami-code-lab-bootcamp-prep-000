2383415314c4671fdc99129fc850788400bee7f8
Luke O'Connor <luke.p.oconnor@gmail.com>
2016-10-18 14:04:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-const body = document.querySelector('body');
-
-body.addEventListener('keydown',init(e));
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 var index = 0;
 
 function init(e) {
   const key = parseInt(e.detail || e.which)
+
   if (key === code[index]) {
-    index++;
+    index++
+
     if (index === code.length - 1) {
-      alert('YOU DID IT');
-      index = 0;
+      alert("YOU DID IT!")
+      index = 0
     }
   } else {
-    index = 0;
+    index = 0
   }
-}
+};
+
+const bodski = document.getElementById('body');
+
+bodski.addEventListener('keydown', init);
