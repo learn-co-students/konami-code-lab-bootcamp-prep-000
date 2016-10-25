cba73da7cedf7fb89f17f92bdc2cbacb0e859024
Nick Fohs <nick.fohs@gmail.com>
2016-10-25 01:07:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+var index = 0
+
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      console.log(`${code[index]} for press of ${key}`)
+      ++index
+      console.log(`index is now ${index}`)
+
+        if (index === code.length - 1) {
+          alert(`HOORAY`)
+          index = 0
+        }
+      }
+})
 }
+
+init()
