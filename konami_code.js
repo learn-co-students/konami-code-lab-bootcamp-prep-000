30b2d892e7eee29f2326b16a8215ab5f89147d04
Nick Fohs <nick.fohs@gmail.com>
2016-10-25 01:10:24 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-
-var index = 0
-
-
 function init() {
+  var index = 0
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which)
 
@@ -17,8 +14,10 @@ function init() {
           alert(`HOORAY`)
           index = 0
         }
+      } else {
+        index = 0
       }
-})
+    })
 }
 
 init()
