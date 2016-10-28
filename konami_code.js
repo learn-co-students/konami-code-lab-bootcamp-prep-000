0afff59315db70dd5dd754270307b5261278a967
Joe Browm <jaybrizle@gmail.com>
2016-10-28 05:11:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var index = 0;
 function init() {
-  // your code here
+//  your code here
+
+  document.body.addEventListener('keydown', handler);
+}
+
+function handler(e) {
+  const key = parseInt(e.detail || e.which)
+  if(key === code[index]){
+    index++
+    if(index === code.length - 1){
+      alert('Woo');
+      index = 0
+    }
+  }else {
+    index = 0
+  }
 }
