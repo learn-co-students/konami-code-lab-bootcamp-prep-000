1e01800ec3349880db138b2dd8ee03fe8a7b3db6
Daniel Lempesis <danlrcn@hotmail.com>
2016-11-02 20:34:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
+function init(event) {
 
-function init() {
-  // your code here
+
+
+  document.body.addEventListener('keydown', function(event) {
+
+  const key = parseInt(event.detail || event.which)
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length - 1) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}) //close parentheses which was screwing me up.
 }
+//init(event)
+//  eventListener = document.body
+  //eventListener.addEventListening('keydown',function(event))
