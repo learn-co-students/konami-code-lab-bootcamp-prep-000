69ba0f9b51b4c6f1a9474ca3c021b9720e949084
Jennifer Wegar <jennyweegar@yahoo.com>
2016-11-03 14:57:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // your code here
+var index = 0;
+function init()
+{
+  document.addEventListener('keydown', function(event)
+  {
+    var key = parseInt(event.detail || event.which);
+    if (key === code[index])
+    {
+      index++;
+      if (index === code.length - 1)
+      {
+        alert('YOU DID IT!');
+        index = 0;
+      }
+    } else
+     {
+      index = 0;
+    }
+  });
 }
