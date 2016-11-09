59ee8177acb435833e3b1bab8d918858716b2094
Kevin Dam <kevinddam@gmail.com>
2016-11-09 21:22:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var keystrokes = []
 
 function init() {
-  // your code here
+
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', keyListener)
+}
+
+
+function keyListener(e){
+  keystrokes.push(e.detail || e.which)
+  for(let i = 0; i < keystrokes.length; i++){
+    if(keystrokes[i] != code[i]){
+      keystrokes = []
+      break
+    }
+    else if(keystrokes.length == 10){
+      alert('KONAMI')
+      keystrokes = []
+      break
+    }
+  }
 }
