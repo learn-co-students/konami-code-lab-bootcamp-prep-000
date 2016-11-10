6e859389dde7687ce43eda7d44d486c6ce9f7e7b
Yi-Xuan Lau <yixuan.lau@gmail.com>
2016-11-10 06:45:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+function init(){
+  document.body.addEventListener('keydown', onKeyDownHandler)
+  var index = 0
+  function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.which)
+    console.log(key)
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length - 1) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+  }
 }
