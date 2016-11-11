83b5131f64b4be67bac72accdc1a03efba1d7389
John Shelby <johnshelby@yahoo.com>
2016-11-11 19:56:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+    var index = 0
+  document.addEventListener('keydown',function(e){
+    const key = parseInt(e.which || e.detail)
+    if (key === code[index]){
+      index ++
+      if (index === code.length){
+        alert('wow!')
+      }
+    }else{index = 0}
+  })
 }
