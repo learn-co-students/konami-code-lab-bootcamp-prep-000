30504919d42367c86e6cf3f5f042824001c50982
Egor Lebedev <mattspring0@gmail.com>
2016-11-15 18:52:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-var i = 0;
+
 function init() {
+    let i = 0;
     document.body.addEventListener('keydown', function(e) {
-      if (e.keyCode === code[i]) {
+      if (e.detail === code[i]) {
           i++;
           if (i === code.length-1) {
               alert();
