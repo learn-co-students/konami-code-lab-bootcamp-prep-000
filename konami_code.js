0c63eda113e71fc7b5e482877c5d88e84d75985e
Devon Weldon <devon.w.weldon@gmail.com>
2016-11-22 23:24:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,16 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var index = 0;
 function init() {
-  // your code here
-}
+  document.body.addEventListener('keydown', function(event){
+    if(parseInt(event.which || event.detail) === code[index]){
+        index++
+        if(index === code.length ){
+          alert("You have cracked the Konami Code!!");
+          index = 0
+
+        }
+      } else {
+          index = 0
+          }
+        })
+      }
