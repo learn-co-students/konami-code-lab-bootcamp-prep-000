fb3735a4d15964fc9028ad519d59ce3c80e5413c
Dan Choi <ygdanchoi@gmail.com>
2016-11-26 23:13:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,18 +2,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var keepTracker = 0
 
 function init() {
-  document.body.addEventListener("keydown", onKeyDownHandler)
-}
-
-function onKeyDownHandler(e){
-  const key = parseInt(e.detail || e.which)
-  if (key === code[keepTracker]) {
-    keepTracker++
-    if (keepTracker === code.length - 1) {
-      alert("Congratulations!")
+  document.body.addEventListener("keydown", function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.which)
+    if (key === code[keepTracker]) {
+      keepTracker++
+      if (keepTracker === code.length - 1) {
+        alert("Congratulations!")
+        keepTracker = 0
+      }
+    } else {
       keepTracker = 0
     }
-  } else {
-    keepTracker = 0
   }
-}
+)}
