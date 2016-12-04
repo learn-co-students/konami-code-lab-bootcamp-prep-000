b1ec4f20a8ea90441e3d76be710692c7d84446d0
Elkhan Mamedov <elkhan.me@gmail.com>
2016-12-04 10:36:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,13 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function (event) {
+    const key = parseInt(event.detail || e.which)
+    if (key === code[index]) {
+      alert('Hello')
+    }
+  })
 }
+init()
