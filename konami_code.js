1679453b2e734c4cdcecf2f9680e10bed4c6329a
Scott Young <young.scottw@gmail.com>
2016-12-06 20:26:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-var index = 0
+var counter = 0
 
 function init() {
   const body = document.getElementsByTagName('body')[0]
@@ -7,15 +7,15 @@ function init() {
   body.addEventListener('keydown', function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
 
-    if (key === code[index]) {
-      index++
+    if (key === code[counter]) {
+      counter++
 
-      if (index === code.length - 1) {
+      if (counter === code.length - 1) {
         alert("Konami Code")
-        index = 0
+        counter = 0
       }
     } else {
-      index = 0
+      counter = 0
     }
   }
 )
