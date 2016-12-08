9d2b72b1d7c09837b731fdd494e35d28d583000c
Michael Lane <mikeyd.lane@gmail.com>
2016-12-08 05:04:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', (e) => konamiCheck(e))
+}
+
+function konamiCheck(e) {
+  var keyDown = parseInt(e.detail || e.which)
+  if (code[index] == keyDown) {
+    index++
+    if (index === code.length) {
+      alert('U R A Konami Komando')
+      var h1 = document.createElement('h1')
+
+      document.body.style.backgroundColor = '#f06d06';
+
+      h1.innerHTML = 'KONAMI KAT!'
+      h1.classList += 'konami';
+
+      document.body.appendChild(h1)
+
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
