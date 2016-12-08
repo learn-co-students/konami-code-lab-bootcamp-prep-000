f20487edf3a42dd9758836a411cb78efacb8c1fd
Kevin Adkins <kevinladkins@gmail.com>
2016-12-08 01:14:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,38 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-let body = document.body
+/* let body = document.body */
 var index = 0
-const key = parseInt(e.which || e.detail);
 
-function keyEvent(e) {
+
+
+
+/* function onKeyDownHandler(e) {
+  const key = parseInt(e.which || e.detail);
   if (key === code[index]) {
     index++
     if (index === code.length -1) {
-  } alert("You did it!!");
-} else {
+    alert("You did it!!");
+    index = 0;
+    }
+  } else {
   index = 0;
 }
-}
 
-function init(e) {
-   body.addEventListener("keydown", keyEvent);
+function init() {
+  document.body.addEventListener("keydown", function(e){onKeyDownHandler})
+} */
+
+function init() {
+  document.body.addEventListener("keydown", function(e){
+    const key = parseInt(e.which || e.detail);
+    if (key === code[index]) {
+      index++
+      if (index === code.length -1) {
+      alert("You did it!!");
+      index = 0;
+      }
+    } else {
+    index = 0;
+   }
+  )}
 }
