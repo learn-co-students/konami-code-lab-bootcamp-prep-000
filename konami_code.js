8f0203c944318dd173e7b4330f6962467d8181a1
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-12-11 18:28:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
 const input = document.querySelector('code')
@@ -8,12 +9,13 @@ const input = document.querySelector('code')
 input.addEventListener('keydown', function(e){
 const key = parseInt(e.detail || e.which);
 
-if(code[index] === key) {index++}
+if(code[index] === key) {index++;
 
-if(index === code.length - 1) {alert(“Huzzah!”)
+if(index === code.length - 1) {alert(“Congratulations, you are a zen master!”)
 
 var index = 0}
 
 else {return index
 var index = 0}
 })
+}
