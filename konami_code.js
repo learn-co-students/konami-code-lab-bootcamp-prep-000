9f881f2572715a73adea92851b21d4e002fa7da8
Chelsea Hicks <chelseamariehicks@gmail.com>
2016-12-11 14:42:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index = code
+const body = document.getElementById('body')
+body.addEventListener('keydown', function(event){alert("Congratulations to you!")})
+  const key = parseInt(e.detail || e.which)
+
+  if (){alert()}
 }
