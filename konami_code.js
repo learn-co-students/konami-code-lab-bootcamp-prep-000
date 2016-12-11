f0605b849fdcd262bc3729764ebfdf22d0227d13
Devin Rieger <dev0089@gmail.com>
2016-12-11 10:34:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,22 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 var index = 0;
 
-function init(e) {
-  var lastInput = parseInt(e.detail || e.which);
+function init() {
 
-  if (code[index] == lastInput) {
-    index++
+  document.querySelector('body').addEventListener("keydown", function(e) {
+    var lastInput = parseInt(e.detail || e.which);
 
-    if (index == code.length) {
-      alert('LOL NERD');
+    if (code[index] == lastInput) {
+      index++
+
+      if (index == code.length) {
+        alert('LOL NERD');
+        index = 0;
+      }
+    }
+    else {
       index = 0;
     }
   }
-  else {
-    index = 0;
-  }
-}
-
-document.querySelector('body').addEventListener("keydown", function() {
-  init(e);
-});
+)}
