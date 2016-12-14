70b080badeabe1f34d83bbe8df85e2d5d2b766dc
jason brown <j.grant.brown@gmail.com>
2016-12-14 04:16:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,39 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
+var index=0
+function init(e) {
   // your code here
+  const key=parseInt(e.detail || e.which)
+  if(key===code[index]){
+    index++
+if(key===code.length-1){
+  alert("Contra code entered: guns a blazin")
+index=0
 }
+}
+else{
+  index=0;
+}
+}
+
+/* NOTES FOR STARTER CODE
+const alphabet = ['a', 'b', 'c']
+
+// keep track of index outside of the event handler
+var index = 0
+
+// unattached event handler
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+
+  if (key === alphabet[index]) {
+    index++
+
+    if (index === alphabet.length - 1) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
+*/
