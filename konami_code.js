47f61fb2acad91847fedf3e0a24f51df386fd97d
Raul Vasquez <rvasqz86@gmail.com>
2016-12-17 07:08:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,12 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  document.querySelector("body").addEventListener('keydown', function eventHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    alphabet.forEach(item =>{
+      if (key === item) {
+        alert("YOU DID IT!")
+      }
+    })
+  })
 }
