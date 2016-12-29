64239dac0b563758ddafcc5e71ff420d807a907c
Amit Singh <amit_nyc@live.com>
2016-12-29 16:55:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  var index =[]
-if(window.addEventListener){
-  window.addEventListener('keydown',function(e){
-    index.push(e.keyCode);
-    if(keys.toString().indexOf()>=0){
-      init()
-    }
-  })
+     document.body.addEventListener('keydown', function init() {
+   alert()
+ })
+ const input = document.querySelectorAll('input')
+
+document.body.addEventListener('keydown', function init() {
+alert()
+})
 }
