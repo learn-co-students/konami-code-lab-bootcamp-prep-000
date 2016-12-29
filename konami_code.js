ffa5bbc1eb1aa874758608f52c56a570144b36f3
Amit Singh <amit_nyc@live.com>
2016-12-29 18:06:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-     document.body.addEventListener('keydown', function init() {
-   alert()
+     document.body.addEventListener('keydown', function(e){/// add a listener for keydown
+        const key = parseInt(e.detail || e.which)///var key equals to
+      if(key===40){/// if key equals to 40 create an alert
+   alert()}
  })
- const input = document.querySelectorAll('input')
-
-document.body.addEventListener('keydown', function init() {
-alert()
-})
 }
