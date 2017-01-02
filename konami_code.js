1ad92de87444427c907392506ad18eec4eea4da6
Ryan Christoffersen <ryanelliotchristo@gmail.com>
2017-01-02 18:29:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index = 0;
+  document.body.addEventListener('keydown', function(event) {
+    var key = parseInt(event.detail || event.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("konami code");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+init();
