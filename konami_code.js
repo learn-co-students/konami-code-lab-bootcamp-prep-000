6aed62733bc5638d60b69d95cb0cbfcb576c1451
Vagelis Kalimeris <aka9080@yahoo.gr>
2017-01-07 18:59:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+
 function init() {
-  // your code here
+  var index = 0
+  var body = document.body
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]) {
+      index++
+    }
+    else {
+      index = 0
+    }
+    if (index === code.length) {
+      alert("KONAMI CODE UNLOCKED!")
+      index = 0
+    }
+})
 }
+
+init()
