5183d5760a7b9a940aae1e9fa9586599cfd74500
Rachel Arkebauer <rarkebauer@gmail.com>
2017-01-11 23:28:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.querySelector('body');
+
+var i = 0;
+
 function init() {
   // your code here
+
+  body.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+    if(key===code[i]){
+        i++
+      if(i === code.length-1){
+        alert('YOU DID IT!');
+        i = 0;
+      }
+    } else{
+      i = 0;
+      }
+  })
 }
