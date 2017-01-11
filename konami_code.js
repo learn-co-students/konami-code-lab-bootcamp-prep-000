c7a0f53f7aff9bbb1664e83e4ded2e1097ce4f1f
dison ruan <dison.ruan@yahoo.com>
2017-01-11 19:37:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index = 0
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+
+    console.log(key)
+
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+
+  document.addEventListener('keydown', onKeyDownHandler)
+
 }
+
+/*
+
+*/
