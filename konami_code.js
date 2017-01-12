4664779b6d87e238f0ec31fe5ad597ac09a91304
Alex Pepper <axpepper@gmail.com>
2017-01-12 09:33:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  var index = 0
+
+  function capture(e) {
+    const key = parseInt(e.detail)
+    if (key===code[index]){
+      index++
+      if (index === code.length - 1) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    }else{
+      index=0
+    }
+  }
   // your code here
+  const main = document.getElementById('body')
+  document.body.addEventListener('keydown', capture)
 }
