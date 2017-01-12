c6fb467f7152c1f08b10fd06f7c9e3ba5431ac37
Julia Thompson <thompson.julia@gmail.com>
2017-01-12 12:28:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-const body = document.querySelector('body')
-
 function init(){
 
 var index = 0
 
-body.addEventListener('keydown', eventHandler)
+document.body.addEventListener('keydown', eventHandler)
 
 function eventHandler(e) {
   const key = parseInt(e.detail || e.which)
