0b887ed265863ecdbf1800143e9855d621b9a7bb
Juha  Bach <cdalsoniii@gmail.com>
2017-01-16 17:53:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
-}
+  function init() {		  function init() {
+   // your code here		 +
+   var index = 0
+
+ document.body.addEventListener('keydown', eventHandler)
+
+ function eventHandler(e) {
+   const key = parseInt(e.detail || e.which)
+   if(key === code[index]){
+     index++
+     //console.log("index ",index)
+     if(index === code.length){
+       alert("Congratulations, you won!")
+       index = 0
+     }
+   } else {
+     index = 0
+   }
+ }
+  }
