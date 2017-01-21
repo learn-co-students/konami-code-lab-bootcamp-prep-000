5bd762adfc593a96f6f4fc6d42eb582215183ea1
Sridevi Kuchibotla <skuchibotla1998@gmail.com>
2017-01-21 04:42:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-
+  var index = 0;
   document.body.addEventListener('keydown', function(e) {
-    var index = 0;
+
     const key = parseInt(e.detail || e.which);
 
     if(key === code[index]) {
       index++;
 
-      if (key === code.length - 1) {
+      if (index === code.length - 1) {
         alert("YOU DID IT!");
         index = 0;
       }
