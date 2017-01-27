b70856a13bf0cee1b7e0fe1d18a3df68ddf20c81
IKSHVAKU SINGAPURAM <ixs21870@ucmo.edu>
2017-01-27 04:32:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+
+var index =0;
+  document.body.addEventListener('keydown',onKeyDownHandler);
+
+  function onKeyDownHandler(e){
+    const key = parseInt(e.detail||e.which);
+    if(key===code[index]){
+index++;
+if(index===code.length-1){
+  alert("YOU DID IT");
+  index = 0
+}
+
+    }else{
+      e.preventDefault();
+      index =0;
+    }
+  }
+
+
+
   // your code here
+
 }
