4859f330050f547481d23670b462056a7bcc4d92
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-01-31 22:02:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+const body = document.querySelector('body')
+var userCode = []
 function init() {
-  // your code here
+  body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which)
+    for (let i = 0, l = code.length; i < l; i++) {
+      if (key === code[i]) {
+        userCode.push(key)
+
+        if (userCode.length === code.length) {
+          window.alert("YOU DID IT!")
+          i = 0
+          userCode = []
+        }
+      }
+    }
+  })
 }
+
+init()
