b94da22cd19a7d2a3a3e6a9c2c917f7a2f3e49d6
Elise Yohm <elisery@hotmail.com>
2017-02-02 22:35:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  const theBody = document.querySelector('body')
+  let magicNum = code.length - 1
+  var counter = 0
+
+  theBody.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[counter]) {
+      counter += 1
+    }
+
+    if (counter === magicNum) {
+      alert('You are a Konami Master!')
+      counter = 0
+    }
+  })
 }
