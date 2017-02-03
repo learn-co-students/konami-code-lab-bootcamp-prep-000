40f02ea1ec540c4ee23763ce6eb5848547331a2b
Bradley Grey <bradleyjgrey@gmail.com>
2017-02-03 01:09:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // your code here
-}
+    // your code here
+const konamiCode = document.querySelector('body')
+let magicNum = code.length - 1
+var counter = 0
+ 
+ konamiCode.addEventListener('keydown', function(e) {
+ const key = parseInt(e.detail || e.which)
+ 
+ if (key === code[counter]) {
+       counter += 1
+     }
+
+     if (counter === magicNum) {
+       alert('Yas Queen!')
+       counter = 0
+     }
+   })
+  }
