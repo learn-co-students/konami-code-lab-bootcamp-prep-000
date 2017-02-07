6768d8a5bbfd4e8d44f0ecf79ec3884a782a96b2
Luis Rincon <rincon_92@hotmail.com>
2017-02-07 03:33:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
+
 function init() {
-  // your code here
+  var index = 0;
+  var body = document.body;
+  body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.which || e.detail)
+    if (key === code[index]) {
+      index++;
+  /* Bottom if statement will only run if the top statement is true.
+  If bottom if statement is false, it will go back to the top if statement. */
+      if (index === code.length - 1) {
+        alert('YOU DID IT!');
+        index = 0;
+      }
+    } else {
+      // alert('Try again')
+      index = 0;
+    }
+  })
 }
+
+
+
+
+/*up, up, down, down, left, right, left, right, b, a */
