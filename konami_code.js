722f78bfe7e7ff1cd54fc580da99d791f7c975d8
Spencer Boakye <spencerboakye@gmail.com>
2017-02-13 08:18:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+var i = 0
+function keyDown(e){
+  const key = parseInt(e.detail || e.which)
+
+  if(key === code[i]){
+    i++
+    if(i === code.length){
+    alert('Congratulations!')
+    i = 0
+  }
+}else{
+  i = 0
+}//console.log(key)
+}
+
+function init(){
+  document.body.addEventListener('keydown', keyDown)
 }
