108a97d9862fd37dad4b6aa64b65c8adfab2a993
Maxwell Benton <maxwellbenton@gmail.com>
2017-02-15 01:25:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const body = document.querySelector("body");
+
+var index = 0;
+var arr = [];
+
+
 
 function init() {
-  // your code here
+
+  body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which)
+    //console.log(key)
+    if (key === code[index]) {
+      index++
+      arr.push(key)
+      console.log(key)
+      if (index === code.length) {
+        alert("YES!")
+      }
+    } else {
+      index = 0;
+      arr = [];
+    }
+
+    //console.log(e.which)
+    });
+
+
 }
