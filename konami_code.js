443867e5982a237734558c84c6ed4a1bfa00b9d0
Lanitta Horner <lanittad@hotmail.com>
2017-02-19 22:54:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0
+var newElement = document.querySelector('body')
+  newElement.addEventListener('keydown',function(event){
+    var key = parseInt(event.detail || event.which)
+    if(key === code[index]){
+      index++
+    if(index === code.length){
+      alert('You have found the magic key!')
+      index = 0
+    }
+  }else{
+    index = 0
+  }
+
+
+  })
+
+
 }
