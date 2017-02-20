4cfdb13f11e4edadc8f8c10edae1ed213c9578c8
Ammo Can <ammo@2-drops.com>
2017-02-20 00:04:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // your code here
+  var i = 0;
+  const input = document.body;
+
+  input.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[i]) {
+      i++;
+
+      if (i === code.length - 1) {
+        alert("You Sank My Battle Ship!");
+        i = 0;
+      }
+
+    } else {
+      i = 0;
+    }
+  });
 }
