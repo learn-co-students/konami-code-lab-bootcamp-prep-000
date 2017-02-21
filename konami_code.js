989f9069a5455870fe55681cd0c2694984224b55
Rhema Boyo <rhemaboyo@gmail.com>
2017-02-21 04:54:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var codeCheck = []
+  document.body.addEventListener('keydown', function(event)
+  {
+    codeCheck.push(parseInt(event.detail || event.which))
+
+    if (codeCheck.toString() === code.toString()) {
+      alert('Congratulations!')
+    }
+
+  })
 }
+init()
