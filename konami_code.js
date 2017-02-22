2f092c42eb31238c870a11742993a595cf9990a2
Bryon Martinez <martinez.bryon@gmail.com>
2017-02-22 04:11:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0
+
+  function triggerKeyDown(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length - 1) {
+        alert("You Rock Buddy!!!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+
+  const check = document.body
+
+  check.addEventListener("keydown", triggerKeyDown)
 }
