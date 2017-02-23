50f7fb5884ac03bb08f88816fb1b03997a085bce
Jake Buccarelli <jtb1137@gmail.com>
2017-02-23 18:32:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
+
 function init() {
-  // your code here
+  var body = document.querySelector('body');
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0
+    }
+  })
 }
