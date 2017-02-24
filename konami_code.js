5123a19d06cfdc7873985f14a2e60fcd88ab5d58
Pablo Lee <lee.pablo.e@gmail.com>
2017-02-24 03:57:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
+const body = document.body;
+
 
 function init() {
-  // your code here
+  body.addEventListener('keydown', konamiCode)
+}
+
+function konamiCode(e){
+  const key = parseInt(e.detail || e.which)
+
+  if(key === code[index]){
+    index++
+    if(index === code.length){
+      alert("YOU DID IT!")
+      index = 0;
+    }
+  }else{
+    index = 0;
+  }
 }
