7571153682e410d6cc8d45dbd57841b854894590
Kyle Vigorito <kyle.vigorito@gmail.com>
2017-02-27 14:41:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // your code here
+  var i = 0
+  document.body.addEventListener('keydown', function(e) {
+    var keyPressed = parseInt(e.detail || e.which)
+    if (keyPressed === code[i]){
+      i++
+      
+      if (i === code.length - 1){
+        alert('All characters unlocked!');
+        i = 0
+      }
+    }
+  else {
+    i = 0
+  }
+} )
 }
+init();
