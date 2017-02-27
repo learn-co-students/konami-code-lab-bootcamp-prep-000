8973cfe4743ea1b76757ccdf66d3d7149ff3d829
Sarju Mulmi <mulmi.sarju@gmail.com>
2017-02-27 01:08:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0;
+  function onKeyDownHandler(e){
+    var key = parseInt(e.which || e.detail);
+    if(key===code[index]){
+      index++;
+      if(index===code.length-1){
+        alert('CODE CRACKED!!!');
+        index=0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
