e8529b69aeb6e24c4b793e16e5d343db103a28cd
Amanda Chen <amandachen13@gmail.com>
2017-03-01 03:44:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', keydownHandler);
+
+  var index = 0;
+
+  function keydownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++
+      if (index === code.length) {
+        alert('CONGRATULATIONS!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+
+  }
 }
