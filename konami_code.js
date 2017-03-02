4855dac56c61f9b063d8497032f60ba8ef54c610
Jorge Soto <jorgemsm19@gmail.com>
2017-03-02 22:42:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0;
+
+  document.body.addEventListener("keydown", function(event){
+      const key = parseInt(event.detail || event.which)
+
+      if (key == code[index]) {
+          index++
+
+          if (index == code.length) {
+              alert("F U, KONAMI!");
+              index = 0;
+          }
+      } else {
+          index = 0;
+      }
+  })
 }
