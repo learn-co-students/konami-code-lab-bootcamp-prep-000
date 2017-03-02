17b01e2cf0072db0ed174fb1543274357bc9c95c
Ma. Corazon Charmaine Chammag <charmchammag@gmail.com>
2017-03-02 02:24:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var body = document.querySelector('body')
+  body.addEventListener('keydown', onKeyDownHandler)
+}
+
+function onKeyDownHandler(e) {
+  var key = parseInt(e.detail || e.which)
+
+  for(var i = 0; i < code.length; i++) {
+    if (key === code[i]) {
+      alert("YOU DID IT!")
+    }
+  }
 }
