57b007e8b5d23b0953cbd28633f2629011a5297a
Graham Squires <graham.squires3@gmail.com>
2017-03-07 18:51:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+var index = 0
+
+// unattached event handler
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+
+  if (key === code[index]) {
+
+    if (index === code.length - 1) {
+      alert("YOU DID IT!")
+      index = 0
+    } else {
+      index++
+    }
+  } else {
+    index = 0
+  }
+}
+
+// function init() {
+//   document.body.addEventListener('keydown', function (e){
+//     alert('Congrats!')
+//   })
+//   // your code here
+// }
+
+
+function init(){
+  document.body.addEventListener('keydown', onKeyDownHandler)
 }
