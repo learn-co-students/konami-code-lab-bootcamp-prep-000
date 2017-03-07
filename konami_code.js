7cf1513914a42edde9bfd1c880f46a1713dee7e6
Andrew Bonner <andrewbonner92@gmail.com>
2017-03-07 14:43:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   var index = 0
 
-  const input = document.querySelector('body')
-
-  input.addEventListener('keydown', function keyDownEasterEgg(e) {
+  document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which)
 
     if (key === code[index]) {
