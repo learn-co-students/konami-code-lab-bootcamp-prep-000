6ccdf2b363b352f7a4ac047959a87736a74c1d7f
Bryan Hou <bryan.hou94@gmail.com>
2017-03-09 02:04:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,6 @@
 var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 var index=0;
+
 function init() {
   document.body.addEventListener('keydown',function(e) {
     var key = parseInt(e.detail || e.which);
