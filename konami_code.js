257954f925b10a86fa1e0da388908f9f0593066f
Bryan Hou <bryan.hou94@gmail.com>
2017-03-09 02:04:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,14 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var index=0;
 function init() {
-  // your code here
+  document.body.addEventListener('keydown',function(e) {
+    var key = parseInt(e.detail || e.which);
+    if (key === code[index]) {debugger;
+      index++;
+      if (index === code.length) alert("easter egg!");
+    }else {
+      index = 0;
+    }
+  }
+  )
 }
