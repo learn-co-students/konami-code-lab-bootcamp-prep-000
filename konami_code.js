c9244654fbd14e80f89a122ef6de4e82cc0ed6a3
Monika Williams <monika.m.williams@gmail.com>
2017-03-09 05:27:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+
+  document.addEventListener('keydown', onKeyDownHandler);
+
+
+  var index = 0;
+
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+  	index++;
+
+      if (index === code.length) {
+        alert("WAY TO GO!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
