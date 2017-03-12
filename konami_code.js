c4e2d31f1c0d1c10d3d777c2456e1d93ac88e621
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2017-03-12 19:08:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
+
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function (event){
+  const key = parseInt(event.detail || event.which);
+  if (key ===code[index]) {
+    index++
+    if ( index === code.length -1){
+      alert('Congratulations!')
+      index = 0;
+    }
+  }
+  else {
+    index = 0
+  }
+})
 }
