da8cc88104a8562086bc5fbaac7de6005d4319c7
Heather Mason <heatem@gmail.com>
2017-03-13 06:43:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  //const main = document.querySelector('body');
+  var index = 0;
+  document.body.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Woo hoo! Winner!");
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
+init();
