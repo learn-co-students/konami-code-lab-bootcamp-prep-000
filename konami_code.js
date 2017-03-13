504e7ebc41bc0ab534fa3ae0a6bdada23eebbdfb
Eddie Rossiter <eddie.rossiter@gmail.com>
2017-03-13 19:17:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+//gets the body where the event listener will go
+const body = document.querySelector('body')
+//this will count through the code
+var index = 0
+
 function init() {
-  // your code here
+  body.addEventListener('keydown', function(e) {
+    //check to see if keydown is equal to current index in code
+    if(parseInt(e.which || e.detail) === code[index]) {
+      index++
+      //find out if we've reached the end of the code
+      if(index === code.length) {
+        window.alert('Congratulations!')
+        //reset code
+        index = 0
+      }
+    } else { //if it's not the write keydown, start back at 0
+      index = 0
+    }
+  })
 }
+
+//init()
