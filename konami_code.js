cff8c25d9e53b7bab7fa975302b9f28063e22111
Tracy Copeland <ayerik@hotmail.com>
2017-03-15 01:14:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var i = 0;
+  document.body.addEventListener("keydown", function(e) {
+    var key = parseInt(e.which || e.detail);
+
+    if (key === code[i]) {
+      i++;
+      console.log(code[i]);
+      console.log(i);
+
+      if (i === 9) {
+        alert("Easter egg activated!!");
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  });
 }
+
+document.onload = init;
