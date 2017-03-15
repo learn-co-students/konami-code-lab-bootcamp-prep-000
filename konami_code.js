9b89dacf67d0686956692d1558aab041ea313287
Tracy Copeland <ayerik@hotmail.com>
2017-03-15 01:29:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,4 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-const body = document.getElementByTagName("body");
 
 var index = 0;
 
@@ -16,11 +15,10 @@ function init() {
       if (i === 9) {
         alert("Easter egg activated!!");
         i = 0;
+        console.log("success!");
       }
     } else {
       i = 0;
     }
   });
 }
-
-document.onload = init;
