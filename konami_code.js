b93ad19795e690da5bb78bfa3c4b95c1c697cb99
Abdul Rahman Quwatli <aznq2005@yahoo.com>
2017-03-16 01:17:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index=0;
 
 function init() {
-  // your code here
+
+  document.body.addEventListener('')
+}
+
+// unattached event handler
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+
+  if (key === alphabet[index]) {
+    index++
+
+    if (index === alphabet.length - 1) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
