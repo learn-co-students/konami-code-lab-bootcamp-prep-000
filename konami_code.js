c4b21cacd30a4be6b1f40ce693b7c57eeb811629
Brian Chou <bchou515@gmail.com>
2017-03-21 17:34:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var body = document.body
+  body.addEventListener('keydown',onKeyDownHandler)
+}
+
+var index = 0
+
+function onKeyDownHandler(e) {
+  const key = Number.parseInt(e.detail || e.which)
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length - 1) {
+      alert("YOU ENTERED THE KOONAMI CODE")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
