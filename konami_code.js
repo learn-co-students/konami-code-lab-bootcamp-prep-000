b494c70962cb43f996961bd3392a12700c254337
Hou In Choi <hichoi17@outlook.com>
2017-03-22 17:23:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index=0
+  document.body.addEventListener('keydown',function(e){
+        
+      //console.log(parseInt(e.detail))
+      const key= parseInt(e.detail||e.which)
+    
+      
+      if(key===code[index]){
+        index++
+          if(index===code.length-1){
+            alert("don't use cheat")
+            index=0
+          }
+      }else{
+        index=0
+      }
+  })
 }
