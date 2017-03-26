3fb0ad5a9da15be9d1b348693c0c8f233e9ea982
Daniel Justus <daniel.justus32@gmail.com>
2017-03-26 21:22:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var body = document.body
+
+
+
 function init() {
-  // your code here
+  var i = 0
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+      debugger
+      if (key === code[i]) {
+        i++
+      if (i === code.length - 1) {
+        alert('YOU DID IT!!!')
+        i = 0
+      }
+    } else {
+      i = 0
+    }
+  })
 }
