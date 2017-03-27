7e113bea15e612677589925f4e882aa4912d5d9a
Tyler Gotto <tylergotto17@gmail.com>
2017-03-27 07:55:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var i = 0;
+function init(){
+    document.body.addEventListener('keydown', total);
+  }
 
-function init() {
-  // your code here
-}
+function total(e){
+          var keyNumber = parseInt( e.detail || e.which );
+
+          if ( keyNumber == code[i] ) {
+            i++;
+            if (i === code.length - 1) {
+              alert("YOU DID IT!");
+            }
+          } else {
+            i = 0;
+          }
+        }
