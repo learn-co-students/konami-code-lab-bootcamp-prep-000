30513157a4ffe897fca47613d55f3d03e8cd11dc
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2017-03-27 02:56:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', listenForCode);
+}
+
+let codeIndex = 0;
+
+function listenForCode(e) {
+  // HACK: it seems jsdom (what our tests/mocha uses to host the DOM) puts 
+  //    the key in e.detail, which is not where the spec says it should go:
+  //    https://developer.mozilla.org/en-US/docs/Web/API/UIEvent/detail
+  //    > For all other UIEvent objects, UIEvent.detail is always zero.
+  let key = e.detail || e.which;
+
+  // HACK: use == and not ===, so string/number comparisons will be coerced
+  if (key == code[codeIndex]) {
+    codeIndex++;
+    if (codeIndex === code.length - 1) {
+      alert('you did it');
+    }
+  }
+  else {
+    codeIndex = 0;
+  }
 }
