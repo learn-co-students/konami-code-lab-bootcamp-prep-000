0e4bc0b6fddbc6c9160a79db5fc5d10751d00664
Lena Forti <lforti41@gmail.com>
2017-03-30 19:18:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+const body = document.querySelectorAll('body')
+
+var codeIndex = 0
+
+function onKeyDownHandler(e){
+  var key = parseInt(e.detail || e.which);
+  if (key === code[codeIndex]){
+    codeIndex ++ ;
+    if (codeIndex === code.length){
+      alert ('CONGRATS');
+    }
+  }
+  else {
+    index = 0
+  }
+
 }
+
+
+function init() {
+  document.body.addEventListener('keydown',onKeyDownHandler)
+  }
+
+init();
