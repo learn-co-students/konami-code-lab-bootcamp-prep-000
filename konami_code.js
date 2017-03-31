4e4c14b6bc8e7bf3c1a8bc6edaa51d876ef1a999
Cheng Rong <chronger1115@gmail.com>
2017-03-31 01:19:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+var index = 0
+
+function z(e) {
+  const key = parseInt(e.detail)
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length-1) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
+
+
+
+function init(){
+  for (let i = 0, l = code.length; i < l; i++) {
+    document.body.addEventListener('keydown',z)
+  }
 }
