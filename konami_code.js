2b9a9ca7131974229dd03157abf2ba9301629b2b
Nicholas D'Amico <nickalan82@icloud.com>
2017-04-01 14:15:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.getElementsByTagName('body')[0]
+
 function init() {
   // your code here
+
+  var index = 0;
+
+  body.addEventListener('keydown', function(e) {
+
+    const key = parseInt(e.detail || e.which)
+    console.log(key)
+    if (key === code[index]) {
+      index++
+      console.log('match');
+
+      if (index === code.length - 1) {
+        alert('You broke the Konami Code!')
+        index = 0
+      }
+    }
+    else {
+      index = 0
+    }
+
+  })
+
+
 }
+
+init();
