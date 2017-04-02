e34e1332fe7edf3513bac7fd44899cc3e830b6a6
Rahim Spencer <spencer.rahim@gmail.com>
2017-04-02 02:04:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,40 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+/*
 function init() {
   // your code here
+  var index = 0
+
+  if (window.addEventListener) {
+  document.body.addEventListener("keydown", function(e) {
+    if (e.keyCode === code[index]){
+      index++;
+    }
+
+    if(index === code.length - 1){
+      alert('You typed the Konami Code')
+      index = 0
+    } else {
+      index = 0
+    })
+    }
+  }
+}
+*/
+
+
+function init() {
+  // your code here
+  var index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+      if (key === code[index]) {
+        index++
+      }
+      if (index === code.length - 1) {
+        alert('You entered the right code!')
+        index = 0
+      } 
+  } )
 }
