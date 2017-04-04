4c823af3741b71fabef9c590fae83f8dcfe901b4
Yveline Say <sayyveline@gmail.com>
2017-04-04 08:23:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var i=0;
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", function(e){
+    e.preventDefault();
+    const key = parseInt(e.detail || e.which);
+    if (key === code[i]) {
+    i++;
+      if (i === code.length-1) {
+        alert("Congratulations! You entered the Konami code!");
+      }
+    }
+  })
 }
