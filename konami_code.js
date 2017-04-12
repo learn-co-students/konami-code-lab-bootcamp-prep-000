a7df9c2b0e70d72768fb627f1e6e9dd7a6e8e673
Anson Pontynen <aponty@gmail.com>
2017-04-12 02:57:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // your code here
+  const keyhitlogger=[]
+  document.body.addEventListener('keydown', function (event){
+    keyhitlogger.push(parseInt(event.detail||event.which))
+    if(code.length!==keyhitlogger.length){
+      return false
+    }
+    for(i=0;i<keyhitlogger.length;i++){
+      if(code[i]!==keyhitlogger[i])
+          return false
+    }
+    alert('Yay congrats!')
+  })
 }
+init()
