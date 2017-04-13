a9d86c38a210ae8f08e38862c9c24ca3b853e88e
Juan Diaz <juandiaz9114@gmail.com>
2017-04-13 21:46:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
   // your code here
   var i = 0
   const body = document.querySelector('body')
 
-  body.addEventListener('keydown', fucntion(k){
+  body.addEventListener('keydown', function(k){
     const key  = parseInt(k.detail || k.which)
     if (key === code[i]) {
       i++
     if (i === code.length - 1) {
-      alert('Good Shit!!')
+      alert('Yeah!')
       i = 0}
     }
     else {
@@ -19,26 +18,3 @@ function init() {
   } )
 
 }
-
-
-
-/*const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  var index = 0
-
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail)
-
-    if (code[index] === key) {
-      index++
-
-      if (index === code.length - 1) {
-        alert('hurray!')
-
-        index = 0
-      }
-    } else {
-      index = 0
-    }
-  })*/
