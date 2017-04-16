c4a2e4691a459c83317def9ecee9885b626fa45c
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-16 01:36:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
+//konami-code-lab-bootcamp-prep-000
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  const body = document.querySelector('body');
+  //onKeyDownHandler(e)
+  var index = 0
+
+  body.addEventListener( 'keydown' , function(e) {
+    var key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+    }
+    else {
+      index = 0;
+    }
+    if (index === code.length) {
+      alert("YOU DID IT!");
+      index = 0;
+    }
+  });
 }
