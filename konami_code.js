2b48b5c89b6220f4ed1a53eb8f98bfbfab0c298f
Richard Steinbach <steinbach.dick@gmail.com>
2017-04-19 16:59:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var index = 0
+init()
 function init() {
   // your code here
+
+document.querySelector("body").addEventListener("keydown", kd)
 }
+function kd(e){
+
+    const key = parseInt(e.detail || e.which)
+
+      if (key === code[index]) {
+
+        index++
+
+        if (index === code.length){
+        
+          alert("Nice work!")
+          index = 0
+        }
+      }
+      else {index = 0}
+    }
