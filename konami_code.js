09129e36c942d7885871ed22f468edb33df6a35a
Mikka Pineda <mikkanthrope@gmail.com>
2017-04-20 02:00:47 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,35 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [49, 50];
 
-function init() {
-  // your code here
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+var index = 0;
+
+// add event listener
+
+document.body.addEventListener('keydown', init, false); 
+
+// define init()
+
+function init(e) {
+
+  const key = parseInt(e.detail || e.which);
+
+// log what keys were pressed
+
+  console.log(e.which);
+  
+// alert when special code is entered
+
+  if (key === code[index]) {
+    index++;
+
+      if (index === code.length-1) {
+          window.alert('YOU WON!')
+          index = 0;
+      } 
+
+      else {
+          index = 0;
+      }
+  }
 }
