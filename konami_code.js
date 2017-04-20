278a72730ca0432f723548b5788f037f7eccc8f8
Anish Kumar Shaw <anishshaw100@gmail.com>
2017-04-20 13:45:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-var index = 0
-
-function init(e) {
-  const key = parseInt(e.detail || e.which)
-  if (key === code[index]){
-    index++
-    if (index === code.length - 1){
-      alert("YOU DID IT!")
-      index = 0
-    }
-    else {
-      index = 0
+function init() {
+  var index = 0
+  function eventFunc(e){
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]){
+      index++
+      if (index === code.length - 1){
+        alert("YOU DID IT!")
+        index = 0
+      }
+      else {
+        index = 0
+      }
     }
   }
+  document.body.addEventListener('keydown',eventFunc)
 }
-document.body.addEventListener('keydown',init)
