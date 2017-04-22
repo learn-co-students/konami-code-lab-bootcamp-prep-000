1f40b75798b4d8065b0fb4b62cd19dafefa4c7f0
Alexandra Wright <superbiscuit@gmail.com>
2017-04-22 17:33:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,3 +3,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // your code here
 }
+
+idx = 0
+
+document.body.addEventListener('keydown',
+  function(e) {
+    key = parseInt(e.which || e.detail)
+  if (key === code[idx]) {
+    if (code[idx] === 65){
+      alert("YOU DID THE THING")
+    }
+    console.log(key)
+  idx++
+  }
+  else{
+    idx = 0
+  }
+  }
+)
