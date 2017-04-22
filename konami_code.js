61484fd6138ecfe31aa4079f9b954e687bd5b088
Benjamin Coleman <hello@benjamincoleman.me>
2017-04-22 17:11:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
 
 function init() {
   // your code here
-}
+  document.body.addEventListener('keydown', function(e){
+      const key = parseInt(e.detail || e.which);
+      if (key === code[index]){
+        index ++
+        if (index === code.length){
+          alert('b a b a select start');
+          index = 0;
+        }
+      }
+      else {
+        index = 0;
+      }
+    });
+};
+
+//init();
