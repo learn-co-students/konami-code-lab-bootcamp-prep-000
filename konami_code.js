5b8d61183ed1a49e63ed37d17172e12e13681df9
Jin Cheng <jincheng88@outlook.com>
2017-04-25 17:23:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+{
+let index = 0;
 
-function init() {
-  // your code here
+ function init() {
+// your code here
+ const input = document.body.addEventListener('keydown', (aKey) => {
+ const key = parseInt( aKey.detail || aKey.which );
+ console.log(key);
+  if (index === code.length - 1) {
+     alert("congrats");
+     index = 0;
+   }
+  if (key === code[index]) {
+      index++;
+    } else { index = 0; }
+  })
 }
+ }
