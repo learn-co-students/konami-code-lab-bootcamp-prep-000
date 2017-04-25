82aa22dd2761124dd1d2939f1e41e9acd7fd75cd
Natalie Dean <ndean87@gmail.com>
2017-04-25 21:31:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', callback);
+}
+
+function callback(e){
+  var key = parseInt(e.which || e.detail)
+  for(var i = 0; i <= code.length; i++){
+    if(key === code[i]){
+      console.log("not yet");
+      index++
+        if(index === code.length - 1){
+          alert("You cracked the code!!!");
+        }
+    } else {
+      code[index];
+    }
+  }
 }
