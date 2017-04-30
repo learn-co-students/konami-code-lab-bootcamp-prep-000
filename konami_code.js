00dea88513c4a180ede27f141c71f4522f0c5016
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2017-04-30 10:23:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  let body = document.querySelector('body')
+  body.addEventListener('keydown', function(e) {
+    for (let i=0;code.length>i;i++){
+      if (parseInt(e.detail) === code[i]){
+        alert('You did it!')
+      }
+    }
+})
 }
