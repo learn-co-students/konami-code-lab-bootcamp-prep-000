534e448f3c1d3ae1200ab2cec6f60514447417d1
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-04 14:59:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', konami)
 }
+
+var index = 0
+function konami (e) {
+  const key = parseInt(e.detail || e.which)
+  if (key === code[index]) {
+    index++
+    if (index === code.length) {
+      alert("YOU DID IT!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
+init()
