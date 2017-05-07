3b2e4c2d294c08406af009381be23d2957bbef72
Alex Katalkin <alexey.katalkin@gmail.com>
2017-05-07 02:33:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+
+
+function init (){
+var index = 0
+
+document.body.addEventListener('keydown', function(e) {
   // your code here
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length - 1) {
+        alert("KANAMI!!!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+})
 }
