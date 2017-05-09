8b3ca394dfade3a18b6fc2a54f49d2be50a3e47b
Abraham Rudy <aberudy@gmail.com>
2017-05-09 22:22:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
-}
+var index = 0
+
+function init(){
+
+  document.body.addEventListener('keydown', function(e) {
+  const key = parseInt(e.which);
+  if (key === code[index]) {
+  index++
+    if (index === code.length) {
+    alert('congratulatory message')
+    index = 0
+  }
+  } else {
+    index = 0
+  }
+  })
+  }
