cff9b1da78cfdf771ae019ffc578690da8bfb65c
Vincent Hoffman <vince_hoffman@hotmail.com>
2017-05-13 05:51:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0;
+
+  const input = document.querySelector('body');
+  input.addEventListener("keydown", onKeyDownHandler);
+
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("YOU DID IT!");
+        index = 0;
+      }
+    }else {
+      index = 0;
+    }
+  }
 }
