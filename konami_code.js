ced475220c69b7ee551e9f6e8b43a4b88c68a1a2
Eduardo Nicastro <edu.nicastro@outlook.com>
2017-05-18 05:32:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var i = 0
+
 function init() {
-  // your code here
+
+  document.body.addEventListener("keydown", onKeyDownHandler)
+
+  function onKeyDownHandler(e){
+
+    const key = parseInt(e.which || e.detail)
+
+    if (key === code[i]) {
+      i++
+
+      if (i === code.length - 1){
+        alert("YOU DID IT!")
+        i = 0
+      }
+    }
+    else {
+      i = 0
+    }
+  }
+
 }
