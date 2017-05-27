15042a73b5ad02d5c5155e23112afddd4093a45a
Vivian Kulumba <vkulumba@gmail.com>
2017-05-27 00:39:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ function init() {
       index++;
 
       if (index === code.length) {
-        alert("KONAMI CODE INSERT!");
+        alert("KONAMI CODE ROCKS!!!");
         index = 0;
       }
     } else index = 0;
