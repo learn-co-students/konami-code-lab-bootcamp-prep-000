5dc432f66d5463ed29df239f5c31edabe535d302
Vivian Kulumba <vkulumba@gmail.com>
2017-05-27 00:38:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("KONAMI CODE INSERT!");
+        index = 0;
+      }
+    } else index = 0;
+  });
+
+
 }
