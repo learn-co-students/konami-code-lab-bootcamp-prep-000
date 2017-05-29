158adc4cb63c5628b61554b5be8bd62e05b1d6e2
Robert Laws <roblaws@me.com>
2017-05-29 13:28:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
 
 function init() {
-  // your code here
+  var page = document.body
+  page.addEventListener('keydown', function(e) {
+    if(index == 0) {
+      alert('info')
+      index++
+    } else {
+      
+    }
+    // const key = parseInt(e.detail || e.which)
+    // if (key === code[index]) {
+    //   index++
+    //   if (index === code.length) {
+    //     alert("You got the code!")
+    //     index = 0
+    //   }
+    // } else {
+    //   index = 0
+    // }
+  })
 }
+
+init()
