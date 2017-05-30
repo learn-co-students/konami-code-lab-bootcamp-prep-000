8d3688e7898ce9d00716e2a05e423265ca793c30
Sid Batra <thesidbatra@hotmail.com>
2017-05-30 18:42:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0
+
 function init() {
-  // your code here
+  const body = document.body;
+  body.addEventListener('keydown', onKeyDownHandler);
 }
+
+function onKeyDownHandler(event) {
+  const key = parseInt(event.detail || event.which);
+  if (key === code[index]) {
+    index++;
+    if (index === code.length - 1) {
+      alert("Congratulatory Message!")
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+// function handleStuff(event) {
+//   for (var i = 0; i < code.length; i++) {
+//     if (!onKeyDownHandler(event, i)) {
+//       return null;
+//     }
+//   }
+//   alert("Congratulatory message!");
+// }
