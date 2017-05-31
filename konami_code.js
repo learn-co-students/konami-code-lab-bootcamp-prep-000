3c3cce3ab08427be85ca658342477c4eac3f4323
Hunter Kleindl <hkleindl@gmail.com>
2017-05-31 21:14:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+var index = 0;
+
+function init(e) {
+  const input = document.body.addEventListener('keydown', onKeyDownHandler);
+  }
+
+function onKeyDownHandler(event) {
+  const key = parseInt(event.detail || e.which)
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length - 1) {
+      alert('you did it')
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
