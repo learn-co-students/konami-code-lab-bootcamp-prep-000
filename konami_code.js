9eb220b00ec75114c4501f7ec1a7a211fbf51465
Benjamin Geyer <brgeyer49@gmail.com>
2017-06-01 19:53:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let ind = 0
+let key = 0
+
+document.body.addEventListener('keydown', function(e) {
+  key = parseInt(e.detail || e.which)
+  init()
+})
+
 function init() {
   // your code here
+  if(key===code[ind]){
+    ind++
+  }
+  else{
+    ind = 0
+  }
+  if(ind===code.length){
+    alert()
+  }
+}
+
+function alert(){
+  return console.log("my name is jeff")
 }
