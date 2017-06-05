7b227661a4200453e06f7842f913d2874ec0dac2
Elaine Lai <elainejlai@gmail.com>
2017-06-05 20:53:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,15 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const kCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+// event listener
+const e = document.body
+e.addEventListener('keydown', init){
+  alert('keydown down')
+}
+
+// keep track of index outside of the event handler
+var index = 0
+
+// unattached event handler
 function init() {
-  // your code here
+
 }
