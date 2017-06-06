8f6f0d78dd0908448e1064c66742850f07d7dece
Elaine Lai <elainejlai@gmail.com>
2017-06-06 00:46:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,22 @@
+// I'm looking for this
 const kCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-// event listener
-const e = document.body
-e.addEventListener('keydown', init){
-  alert('keydown down')
-}
-
 // keep track of index outside of the event handler
 var index = 0
 
 // unattached event handler
 function init() {
+  const key = parseInt(e.detail || e.which)
+  if (key === kCode[index]) {
+    index++
+      if (index === kCode.length) {
+        alert("YOU DID IT! KONAMI SUCCESS")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
 
-}
+// event listener, only calls init when the event happens
+const e = document.querySelector('body')
+e.addEventListener('keydown', init)
