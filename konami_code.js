515acbb08091e595ad6aa06792b3098f79891ede
Yiming Jiang <agorify@gmail.com>
2017-06-07 04:33:59 +0000

    DONE

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
 
 function init() {
   // your code here
+  const input = document.querySelector('body');
+  // input.addEventListener('keydown', function(e));
+  input.addEventListener('keydown', surprise);
+}
+
+function surprise(e) {
+  const key = parseInt(e.detail | e.which);
+
+  if(key === code[index]) {
+    console.log("Watch out!");
+    index++;
+
+    if (index === code.length) {
+      alert("POP");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+
 }
