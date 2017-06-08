bd05331ec1e7da1a9a6343dae165486842134df0
Matthew Prenzler <mattprenz@yahoo.com>
2017-06-08 13:40:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var progress = [];
+
+init();
 
 function init() {
-  // your code here
+  reset(progress)
+  document.addEventListener('keydown', function(event){
+
+      if (event.keyCode === progress[0]){
+        progress.shift();
+      }
+      else{progress = reset(progress)}
+      if (progress.length === 0){alert("KONAMI!");progress = reset(progress)}
+});
+
+}
+function reset(prog){
+  prog = [];
+  for (item in code){
+    prog.push(code[item])
+  }
+  return prog
 }
