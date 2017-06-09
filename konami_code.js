ab127b713d018b9f6eebf47029f7ff783475442f
Justin Manalad <jstn.mnld@gmail.com>
2017-06-09 17:12:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+var codeComplete=0
+
 function init() {
-  // your code here
+
+  document.body.addEventListener('keydown', function(e){
+
+    const key = parseInt(e.detail|| e.which);
+
+    console.log(e.which); //outputs what key has been pressed
+
+    if (key === code[codeComplete]){
+      codeComplete++;
+      if(codeComplete === code.length){
+        alert("30 Extra Lives!");
+        codeComplete=0;
+      }
+
+    } else{
+      codeComplete=0;
+    }
+  })
+
 }
