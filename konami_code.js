f00fe71d40f464358c3022b1865a34d0f62988c8
Andrew James <ajames20@gmail.com>
2017-06-09 14:25:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
 
 function init() {
-  // your code here
+  const input = document.querySelector('body');
+  input.addEventListener('keydown', konamiHandler);
 }
+
+function konamiHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  let banner = document.querySelector('body');
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      banner.innerHTML = '<h1">KOMAMIIIII</h1>';
+      alert('KONAMI');
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+init();
