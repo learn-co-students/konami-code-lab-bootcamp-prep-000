492dcaee4d54dc3d170e658ac0bc8c351a65639b
Tyler Durden <blokejohnsmith@tutamail.com>
2017-06-16 04:21:47 +0530

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var count = 0;
+
+  function eventHandler(e) {
+    const key = parseInt(e.which||e.detail);
+    if (key===code[count]){
+      count++;
+      if (count===code.length-1){
+        alert('YOU DID IT!');
+        count = 0;
+      }
+    }
+    else {
+      count = 0;
+    }
+  }
+  
+  document.body.addEventListener('keydown', eventHandler)
+
 }
+init();
