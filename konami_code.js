32f9ca7864753c6d334a1f20dd57b1e2c045d04a
chris u <cu364@nyu.edu>
2017-06-22 01:58:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
+
+
+function alert(string) {
+  console.log(string);
+}
 
 function init() {
-  // your code here
+  var body = document.body
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+    index++
+
+      if (index === code.length) {
+      alert("Hero Mode")
+      index = 0
+      }
+    }
+    else {
+      index = 0
+    }
+  })
 }
