cda2e459f2bfa5ebaa5a31c7f206d0a9fc656ad1
Kyle Kinnear <kyle.kinnear@gmail.com>
2017-06-22 17:33:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const body = document.body
+var index = 0
 
-function init() {
-  // your code here
-}
+body.addEventListener('keydown', function init(e) {
+  var key = parseInt(e.detail || e.which)
+
+  if (key === code[index]) {
+    index++
+
+      if (index === code.length) {
+        alert("God mode enabled!")
+        index = 0
+      }
+      }
+    else {
+      index = 0
+    }
+  })
+
+function init() {}
