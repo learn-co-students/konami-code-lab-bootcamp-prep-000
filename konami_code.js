ffebf6ca91e2f971dc59882ec7f8cd90eb1344fa
michelle lin <lin.michelle.m@gmail.com>
2017-06-28 16:03:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
+init()
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', onKeyDownHandler)
+}
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+  if(key === code[index]) {
+    index ++
+
+    if(index === code.length) {
+      alert("Congratulations!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
