5cb68d787df2a84207ccd45ea19864bda0985037
github username <github email address>
2017-06-29 16:21:35 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,18 +3,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0
-  document.body.addEventListener('keydown', (event) => {
+  document.body.addEventListener('keydown', function(event) {
+    debugger
     let key = parseInt(event.detail || event.which)
 
     if (key === code[index]) {
-      index++
 
       if (index === code.length - 1) {
         alert("YOU DID IT!")
         index = 0
-      }
-    } else {
+      }index++
+    }
+     else {
       index = 0
     }
   })
 }
+
+init()
