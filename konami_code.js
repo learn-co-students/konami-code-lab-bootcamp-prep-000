4c68c1a003c643f253e68e4454a17a5ee787401b
Matthew Crooks <mfcrooks@gmail.com>
2017-06-29 04:49:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  var index = 0
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail)
+
+    if (code[index] === key) {
+      alert('hurray!')
+      console.log('hurray!')
+      index = 0
+    }
+    index = 0
+  })
 }
