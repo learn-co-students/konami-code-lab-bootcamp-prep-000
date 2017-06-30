6ff1128dbf11272fb25d03a816a68b59caf5b61d
James Kim <kim.jamesw@gmail.com>
2017-06-30 06:45:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,4 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  var index=0;
+
+  function onKeyDownHandler(e){
+    const key=parseInt(e.detail||e.which)
+
+    if (key===code[index]){
+      index++
+
+      if (index===code.length){
+        alert("secret unlocked")
+        index=0
+      }
+    }else {
+      index=0
+    }
+  }
+
+  document.addEventListener('keydown', onKeyDownHandler)
+
 }
