ecad2dc5bd21691c06a06702b7e253dbda22c21a
Kevin Huang <yellow.khaos@gmail.com>
2017-06-30 04:55:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // your code here
+index = 0
+
+function init(){
+  const input = document.querySelector('body')
+  input.addEventListener('keydown', keyDownHandle)
 }
+
+function keyDownHandle(e) {
+  const key = parseInt(e.detail || e.which)
+
+  if (key === code[index]){
+    index++
+    if (index === code.length - 1){
+      alert("Surprise!")
+    }
+  } else {
+    index = 0
+  }
+}
+
+
+
+
