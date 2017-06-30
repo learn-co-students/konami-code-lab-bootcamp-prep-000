c004516ce7a2c016f60770775da52b0235fa7ae6
Bret Burau <burhooo@gmail.com>
2017-06-30 12:12:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
 
 function init() {
-  // your code here
+  document.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which);
+    if(key === code[index]) {
+      index++;
+      if(index === code.length) {
+        alert("YOU DID IT!");
+        index = 0;
+      }
+    } else index = 0;
+  })
 }
