407000d8273b2c22143b7c4fd1a29764aaaea714
Daniel Shiling <dshiling21@gmail.com>
2017-07-01 14:26:23 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let idx = 0
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', keydownHandler)
+}
+
+function keydownHandler(e) {
+  const key = parseInt(e.detail || e.which)
+  if (key == code[idx]) {
+    idx++
+    if (idx == code.length - 1) {
+      alert("You entered the Konami Code!")
+      idx = 0
+    }
+  } else {
+    idx = 0
+  }
 }
