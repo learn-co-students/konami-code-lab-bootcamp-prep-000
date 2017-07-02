ea134821c55cde3a9af622c14a6971791d57b1d2
Chris Miller <chris.miller8823@gmail.com>
2017-07-02 03:54:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  //event listener to document.body
+    //check for keydown as an event
+  var index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    //if user eters every code correctly
+      //alert() a nice message
+    var userKey = parseInt(e.which || e.detail);
+
+    if (userKey === code[index]) {
+      index++;
+
+      if (index === code.length - 1) {
+        alert('Congratulations! You have activated the KONAMI code!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
