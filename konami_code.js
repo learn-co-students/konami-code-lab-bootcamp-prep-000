c4934d18f79be7d83ff85312680f851113c7730b
Daniel Toma <danieltoma@ubalumni.com>
2017-07-03 00:27:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ var index = 0
 
 function init() {
   // your code here
-  const buddy = window.document.getElementsByTagName('body')
+  const buddy = window.document.getElementsByTagName('body')[0]
   buddy.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which)
 
