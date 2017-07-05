3c06d17f36bc6a452fe8bbabf8a8636da161dc07
Jonathan Magolnick <jonathanmagolnick@gmail.com>
2017-07-05 15:33:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var i = 0
+
 function init() {
-  // your code here
+  var body = document.body
+
+  body.addEventListener('keydown', function(e){
+    for(let i = 0; i < code.length; i++){
+      if(parseInt(e.detail || e.which) === code[i]) {
+        alert("YOU DID IT!")
+      }
+    }
+  })
 }
+
+init()
