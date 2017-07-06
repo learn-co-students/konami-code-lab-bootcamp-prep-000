298980008ab1f9a49c5a0aa2f9aeaa240c62685a
Sophia L <soph915@gmail.com>
2017-07-06 23:14:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
-}
+   // your code here
+  const body = document.querySelector('body')
+  let index = 0
+
+  body.addEventListener('keydown', function(e) {
+    let key = (parseInt(e.detail) || parseInt(e.which))
+
+    if (key === code[index]) {
+      index++
+      if (index === code.length) {
+        alert("You did it!!! ")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
+ }
