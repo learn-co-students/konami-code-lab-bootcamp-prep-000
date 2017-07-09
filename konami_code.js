3b581da791d7454573f6204945a15bbe65bec702
Matt Crowe <crowe.matthew5@gmail.com>
2017-07-09 22:03:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
 
-function init() {
-  // Write your JavaScript code inside the init() function
+const body = document.querySelector('body');
+
+function init(){
+  body.addEventListener('keydown', function(e){
+    const key = parseInt(e.which || e.detail);
+
+    if(key === code[index]){
+      index++;
+      if(index = code.length){
+        alert('Hurray!');
+        index=0;
+      }
+    }
+    else{
+      index=0;}
+  });
 
 }
