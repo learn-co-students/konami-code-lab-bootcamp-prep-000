6910c0e0b46254a8ead46354256bc2a196679f21
Christina Pan <christina.pl.pan@gmail.com>
2017-07-11 19:11:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const body = document.body
+
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
-}
+  // your code here
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+    if (key === code[index]) {
+      index++;
+        if (index === code.length) {
+          alert("You got it!");
+          index = 0;
+        }
+    } else {
+      index = 0;
+    }
+  })
+};
