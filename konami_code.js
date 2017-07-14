51458645cebf35181758de5c42f76aac98300dac
Kariny Neves Parreira de Vasconcelos Vasconcelos <karinyparreira@hotmail.com>
2017-07-14 19:49:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//(function() {
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+  function init()
+  {
+    let index = 0;
+    const body = document.querySelector('body')
+    body.addEventListener('keydown', function(e) {
+      //console.log(e.which)
+      const a = parseInt(e.detail || e.which);
 
-}
+      if (a === code[index])
+      {
+        index++;
+        if (index === code.length)
+        {
+          alert("Yeah! You pressed all ten of the keys in the correct order!");
+          index = 0;
+        }
+      }
+      else
+      {
+        index = 0;
+      }
+    })
+  }
+  //init();
+//})();
