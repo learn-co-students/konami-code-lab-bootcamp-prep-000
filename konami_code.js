505068f14c81dd7e36d4d145a1f075fa87259f8f
Nick Cianci <n.cianci3@gmail.com>
2017-07-19 00:04:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let index = 0;
 
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
