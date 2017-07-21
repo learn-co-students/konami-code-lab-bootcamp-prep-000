c1de6ddedb6fd4195e8264c75e4a11b140473aaf
Eric Lau <cireual510@gmail.com>
2017-07-21 22:04:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+/*
 function init() {
   var index = 0
-  document.body.addEventListener('keydown', combo)
+  document.body.addEventListener('keydown', combo)// can also write it as below document.body.addEventListener('keydown', function combo e() {
   function combo(e) {
     const key = parseInt(e.detail || e.which)
       if (key === code[index]) {
@@ -17,4 +17,22 @@ function init() {
           index =0
         }
   }
+}
+*/
+function init() {
+  let index = 0
+  document.body.addEventListener('keydown', function combo(e){
+    var key = parseInt(e.detail|| e.which);
+    if (key === code[index]){
+      index++
+
+      if (index === code.length) {
+        alert('you gotti')
+        index =0
+      }
+    }
+    else {
+      index = 0
+    }
+  })
 }
