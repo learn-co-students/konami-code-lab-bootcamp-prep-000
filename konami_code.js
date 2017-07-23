312294a1eb18873644696b40182eaa7219bdc2d5
reid sherman <reid.sherman@gmail.com>
2017-07-23 19:11:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // Write your JavaScript code inside the init() function
-
+let index = 0;
+function onKeyDownHandler(e) {
+  //console.log("here")
+  const key = parseInt(e.detail || e.which);
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      window.alert("Hurray!");
+      index = 0;
+    }
+  }
+  else {
+    index = 0;
+  }
 }
+function init() {
+  document.addEventListener('keydown', function(e) {
+    onKeyDownHandler(e);
+  })
+}
+//init();
