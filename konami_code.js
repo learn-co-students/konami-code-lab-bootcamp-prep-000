b67774f72b67c0cc8b7e51293cfbdf88a4930ac6
Archana Burra <burra.archana@gmail.com>
2017-07-25 02:39:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   // Write your JavaScript code inside the init() function
+
+  let index = 0;
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      console.log(e.which);
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+
+  var body = document.querySelector('body');
+  body.addEventListener('keydown', onKeyDownHandler);
+
 
 }
