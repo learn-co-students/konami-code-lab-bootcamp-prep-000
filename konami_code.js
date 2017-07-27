4dae7f4a961c42359aff4f8907d8150964bbebb1
Michelle Meyer <mlm8wd6448@gmail.com>
2017-07-27 20:21:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  document.body.addEventListener('keydown', function(event) {
+    const key = parseInt(event.which|| event.detail);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
+init()
