6596e6bb104031442da0e45e9530abe12d96b05e
Max Ward Ward <pmaxwellward@gmail.com>
2017-07-27 03:43:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// (function() { /* all code goes here */})();
 
-function init() {
-  // Write your JavaScript code inside the init() function
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const body = document.querySelector(`body`);
+index = 0;
 
+
+function init()
+{
+
+body.addEventListener(`keydown`, function triggerKeyDown(e) {
+
+  console.log(parseInt(e.detail || e.which));
+  var key = parseInt(e.detail || e.which);
+
+  if (key === code[index]){
+    index++;
+    if (index === code.length){
+      alert(`Code accepted!`);
+    }
+  }
+
+  else {
+    index = 0;
+  }
+})
 }
