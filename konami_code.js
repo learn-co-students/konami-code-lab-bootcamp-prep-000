f954f02420246732ea3d5649fc4822c4bf50491e
Mohammad Khan <sparrowangels@gmail.com>
2017-07-29 03:20:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   document.addEventListener("keydown", function(e){
-    if(code[index] === e.which){ index++; //console.log("index is " + index)
+    let key = parseInt(e.detail || e.which); console.log(`key is ${key}`)
+
+    // if(code[index] === e.which){ index++; //console.log("index is " + index)
+    if(code[index] === key){ index++; //console.log("index is " + index)
       }
     else {index = 0; //console.log("index reset " + index)
     }
