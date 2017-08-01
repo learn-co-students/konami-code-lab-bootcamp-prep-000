ec17b3e95989ed68499c420737e448765b459040
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-01 01:08:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++
+
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    console.log('wrong');
+    index = 0;
+  }
+}
+
 function init() {
-  // Write your JavaScript code inside the init() function
+  const body = document.getElementsByTagName('body')[0];
+  body.addEventListener('keydown', onKeyDownHandler);
 
 }
