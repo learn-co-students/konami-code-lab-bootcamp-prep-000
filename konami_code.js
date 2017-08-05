8c3e7b9a57b833be1994206b753c1acbc05a2fed
Adam Gold <golddragonxyz@yahoo.com>
2017-08-05 16:57:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  console.log('started')
+  let index = 0
 
+  function keyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log(key)
+    if (key === code[index]) {
+      index++
+      console.log(index)
+      if (index === code.length) {
+        alert('You cracked the code!!!')
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+
+  const main = document.body
+  main.addEventListener('keydown', keyDownHandler)
 }
