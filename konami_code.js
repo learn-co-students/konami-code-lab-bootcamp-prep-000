a3fe17c8e518837548cc276fecc2e7e76875ae37
Bryan Clark <bryanhclark@gmail.com>
2017-08-07 23:05:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+
+
+function init(){
+  var index = 0;
+
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.which || e.detail);
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Congrats");
+        index = 0;
+
+      }
+    } else {
+      index = 0;
+
+      }
+    });
 }
