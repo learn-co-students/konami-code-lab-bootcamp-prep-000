0127a953e47c4d23fb84c7f6746aec1a4c328b11
hailey endres <hailey.endres@gmail.com>
2017-08-09 20:30:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+var index= 0
+
+function init() {
+  document.body.addEventListener('keydown', function(e) {
+    console.log("hi")
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]) {
+      index++
+      console.log(index)
+      if (index === code.length-1) {
+        alert('Fuck yeah!')
+        index = 0
+      }
+    } else {
+      index= 0
+    }
+  })
 }
