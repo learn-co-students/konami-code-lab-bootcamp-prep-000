9e2eb60045dee55206a40ccff7da6221e46e14bf
cunhasb <cunhasb@aol.com>
2017-08-09 02:49:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index =0;
 function init() {
   // Write your JavaScript code inside the init() function
+document.body.addEventListener('keydown',function(e){
+  const key = parseInt(e.which || e.location);
+  //console.log(`key is = ${key}`)
+  if (key === code[index]){
+      index++;
+      //console.log (`code key is = ${code[index]}`)
 
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    }else{
+      index = 0;
+    }
+  })
 }
