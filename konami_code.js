637b0d6c35ea58dbda0557715f751490d138e17d
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2017-08-10 17:15:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index=0
+document.body.addEventListener('keydown', onKeyDownHandler)
+function init()
+{
 
-function init() {
-  // Write your JavaScript code inside the init() function
+}
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(key);
+  if (key === alphabet[index]) {
+    index++;
 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
