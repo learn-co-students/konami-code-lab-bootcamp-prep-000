bb10f3671e6477c3856028680393c3a5b790613c
Josh Stillman <joshstillman@gmail.com>
2017-08-10 16:23:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var index = 0
 
+
+
+function init(sneil) {
+  if(sneil === undefined) {
+    console.log("whoops")
+    return
+  }
+
+  const keyz = parseInt(sneil.location /* sneil.which || sneil.keyCode || sneil.key || sneil.char || sneil.charCode*/);
+  console.log(keyz)
+
+  if (keyz === code[index]) {
+    index++
+    if (index === code.length) {
+      alert('Konami')
+      index = 0
+    }
+  } else {
+    index = 0
+  }
 }
+
+const input = document.body
+
+input.addEventListener('keydown', init)
