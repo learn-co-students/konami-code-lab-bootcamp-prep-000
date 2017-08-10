23ee9d2d9e9d561e1591b29753bf59c0940f66bd
Josh Stillman <joshstillman@gmail.com>
2017-08-10 16:48:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,14 +3,13 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
 
-
-function init(sneil) {
-  if(sneil === undefined) {
+function initz(sneil) {
+/*  if(sneil === undefined) {
     console.log("whoops")
     return
-  }
+  } */
 
-  const keyz = parseInt(sneil.location /* sneil.which || sneil.keyCode || sneil.key || sneil.char || sneil.charCode*/);
+  const keyz = parseInt(sneil.location);
   console.log(keyz)
 
   if (keyz === code[index]) {
@@ -24,6 +23,8 @@ function init(sneil) {
   }
 }
 
-const input = document.body
+function init(){
+  const input = document.body
 
-input.addEventListener('keydown', init)
+  input.addEventListener('keydown', initz)
+}
