7cfe2c417ddb68d2c96f62f3f63c09f7c6fb07e9
Brittany Hartmire <bkhartmire@gmail.com>
2017-08-10 03:29:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
+  document.body.addEventListener('keydown', function(event) {
+  const key = parseInt(event.which || event.detail);
 
+
+  // Write your JavaScript code inside the init() function
+    if (code[index] === key) {
+      index++
+
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+      else {
+        index = 0;
+      }
+    }
+  });
 }
+init();
