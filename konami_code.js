f9bcb2a321f908a9f5a32eeb84adceef2cadc17d
LG-STEM <gatesinfo@gmail.com>
2017-08-15 17:21:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,21 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  // const keydown = document.createEvent('Event')
-  //
-  // keydown.initEvent(
-  //   'keydown',
-  //   true,
-  //   true,
-  //   window,
-  //   which,
-  //   which,
-  //   0,
-  //   null,
-  //   null
-  // )
-
-
   var index = 0;
 
   document.body.addEventListener('keydown', function(e) {
