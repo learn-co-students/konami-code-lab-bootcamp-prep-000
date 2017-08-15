297f6d1bad3c0e96678cb28dd71d6242010ffc43
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-15 20:57:47 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
-}
+function init(e) {
+  let body = document.body;
+  body.addEventListener('keydown', function(event){
+    const key = parseInt(event.detail || event.which);
+
+    if (key === code[index]) {
+         index++;
+
+         if (index === code.length) {
+           alert("Hurray!");
+  		     index = 0;
+          }
+        }
+    else {
+          index = 0;
+        }
+      })
+    }
