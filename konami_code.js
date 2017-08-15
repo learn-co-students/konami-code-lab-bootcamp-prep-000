0acb3d0a88755035836b0c9dea59813b0318d4cb
LG-STEM <gatesinfo@gmail.com>
2017-08-15 16:04:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const input = document.querySelector('body')
+  const input = document.body.querySelector('input')
 
   var index = 0;
 
@@ -15,11 +15,12 @@ function init() {
 
       if (index === code.length) {
         alert("Hurray!");
-
         index = 0;
       } else {
         index = 0;
       }
+    } else {
+      alert("Wrong code.");
     }
   }
 }
