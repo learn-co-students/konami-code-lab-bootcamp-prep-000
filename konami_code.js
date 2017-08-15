f8564c6b0f154335803e7dc5591ef6a69d830799
LG-STEM <gatesinfo@gmail.com>
2017-08-15 17:11:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,11 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const input = document.body.querySelector('input')
+  const keyboardEvent = document.createEvent("KeyboardEvent")
+
+  keyboardEvent.initKeyboardEvent(
+    'keydown',
+    true,
+    true,
+    window,
+    which,
+    which,
+    0,
+    null,
+    null
+  )
+
 
   var index = 0;
 
-  input.addEventListener('keydown', function(e) {
+  document.body.addEventListener('keydown', function(e) {
 
     const key = parseInt(e.detail || e.which);
 
@@ -19,8 +32,6 @@ function init() {
       } else {
         index = 0;
       }
-    } else {
-      alert("Wrong code.");
     }
   }
 }
