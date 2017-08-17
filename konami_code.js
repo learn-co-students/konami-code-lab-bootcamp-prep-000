2b71dfe6790d3102c0d9b7332cf089462e08160a
Song W <ace1905@gmail.com>
2017-08-17 19:36:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,41 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var hist=[];
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  //log key
+  function logg(f){
+    hist.push(parseInt(f.detail || f.which))
+    if (hist.length>code.length){
+      hist.shift();
+    }
+    //console.log(`${hist.join(', ')}`)
+  }
+  var i=0;
+  function equals(a,b){
+    //console.log(`alengthis${a.length}, blengthis${b.length}`)
+    if(a.length != b.length){
+      //console.log('unequal length')
+      return false
+    } else{
+      for (i=0; i< a.length; i++){
+        if (a[i]!=b[i]){
+          //console.log('false')
+          return false
+        }
+        //console.log('true')
+        return true
+      }
+    }
+  }
+  //check with konami code
+  function check(d){
+    alert.close
+    logg(d)
+    if(equals(code, hist)){
+      //console.log('congrats!')
+      alert('Congrats!')
+    }
+  }
+  //attach eventListener
+  bb=document.querySelector('body')
+  bb.addEventListener('keydown',check)
 }
