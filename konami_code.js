012164c03560ac1945a109660f229cfb1cf3626a
MARIA HRNDZ <maria.hrndz14@gmail.com>
2017-08-17 04:01:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
 function init() {
   // Write your JavaScript code inside the init() function
 
 let index = 0;
+var e = e;
 
 function KeyDown(e)
-  { const key = parseInt(e.detail || e.which);
+  { const key = parseInt(e.detail || e.which || e.location);
+
+    console.log('fyi: '+ e.detail);
+     expect.createSpy();
     if (key === code[index])
     { index++;
-      if (index === code.length)
-         { alert("OkDokie!");   index = 0;   }
+
+        if (index === code.length)
+         {   alert("OkDokie!");
+         expect.createSpy();
+           index = 0;   }
+
+         //window.alert = expect.createSpy();
       }
     else
            { index = 0;  }
