c256d1b4d9adc839f963e776eaf8c9a7b68d84a3
Jon Edgeworth Edgeworth <jredgeworth@gmail.com>
2017-08-18 02:22:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const pool = []
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let count = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
 
+    if (code[count] === key) {
+      count++;
+
+      if (count === code.length) {
+        alert("Almost done!");
+        count = 0;
+      }
+    } else {
+      count = 0;
+    }
+  });
 }
