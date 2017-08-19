8dab09b83a8a2222ca94dd5ba2907be9ac4c7243
omikolaj <oscar222@sbcglobal.net>
2017-08-19 19:26:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,4 +3,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
+  document.body.addEventListener('keydown', function(e){
+    var keyStroke = parseInt(e.which || e.detail)
+    var counter = 0;
+    if (counter === code.length){
+      console.log("Congrats! You have entered the correct code")
+    }
+    for (var i = 0; i < code.length; i++) {
+      if (keyStroke === code[i]) {
+          counter++;
+          alert(e);
+      }
+      else {
+        counter = 0;
+      }
+    }
+
+  })
+
 }
