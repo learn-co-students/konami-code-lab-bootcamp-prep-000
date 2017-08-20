ec2f4ced57fbf64e4009febc8ca2565cb9d22f5c
PeterBadal <peter.badal@gmail.com>
2017-08-20 20:51:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+// body.addEventListener('click', function(event) {
+//   alert('Hooray, you don\'t suck so bad')
+// }) //TEMPLATE
 
+let index = 0;
+
+function init() {
+
+  document.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+
+   if (key === code[index]) {
+     index++;
+
+   if (index === code.length) {
+     alert("Hooray, you don\'t suck so bad!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+}
+})
 }
