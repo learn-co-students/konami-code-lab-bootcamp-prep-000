ba9c53551a5546f83e8e04ce5a09061b870ea891
Francis Walsh <fow5040@yahoo.com>
2017-08-21 15:52:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown',keyListen);
+  document.body.contra = new Checker(code);
+}
 
+function keyListen(e){
+  document.body.contra.checkKey(e.detail || e.which);
+}
+
+function Checker(array){
+  var index=0, arr=array;
+  return {
+    checkKey:function(num){
+        if(parseInt(num)===arr[index]){
+          index++;
+        }
+        else{
+          index=0;
+        }
+        if(index===(arr.length-1)){
+          alert("+30 lives");
+        }
+    }
+  }
 }
