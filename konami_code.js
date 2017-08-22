2f5b8d1c68fc23046e7ba13024a84a181884b67a
Joshua Wasserman Wasserman <wassermj@union.edu>
2017-08-22 21:26:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,30 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0; // index of code
 
+  function onKeyDownHandler(e){
+
+    console.log("e.location > " + e.location)
+    const key = parseInt(e.which || e.location );
+    //debugger
+    console.log(" key > "+ key)
+    console.log(code[index])
+    if (key === code[index]){
+      index++;
+      console.log("index :" + index)
+
+      if (index === code.length){
+        //console.log("Konami Code entered!")
+        window.alert("Konami Code entered!");
+        index = 0;
+      }
+    }
+
+    else{
+      index = 0
+    }
+  }
+
+  document.body.addEventListener('keydown', onKeyDownHandler)
 }
