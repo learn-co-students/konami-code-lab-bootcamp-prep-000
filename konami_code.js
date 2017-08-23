c2c94943b4b318cc69cb8b4c7481d84ed2a00e6a
W M <wmendoz7@calstatela.edu>
2017-08-23 14:52:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,28 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let i = 0;
 function onKeyDownHandler(e) {
+
   const key = parseInt(e.detail || e.which);
-  return console.log(e.which());
-  for(let i = 0; i < code.length; i++){
-
-    if (key === code[i]) {
-      i++;
-
-      if (i === code.length) {
-
-          alert("Congratulatory Message");
-          i = 0;
-      }
+  if (key === code[i]) {
+    i++;
+    if (i === code.length) {
+        alert("Congratulatory Message");
+        i = 0;
     }
-    else {
-      i = 0;
-    }
+  }
+  else {
+    i = 0;
   }
 }
 function init() {
-  // document.body.addEventListener('keydown', function(event){
-  // onKeyDownHandler(event);
-  // })
-
   document.body.addEventListener('keydown',onKeyDownHandler);
-  )
 }
