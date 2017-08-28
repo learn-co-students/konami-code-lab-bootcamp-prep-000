d1a9bdb981165a9d73b5c8527b7c98fcf3a7b70a
Chris Roberts <chrisrobertspdx@gmail.com>
2017-08-28 17:51:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
 function init() {
   // Write your JavaScript code inside the init() function
-
+  console.log("initializing");
+  document.body.addEventListener("keydown",function(e) {
+	const key = parseInt(e.detail || e.which);
+	if (code[index] ===  key) {
+		index++
+		if (index === code.length) {
+		alert("EASTER EGG!!!!! Root access granted.");
+		index = 0;
+	}
+	}
+	else {
+		index = 0;
+		console.log("reset")
+	}
+  })
 }
