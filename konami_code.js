349c7f4e1f26d00406d3b31569ab5190dffef136
1Programmer1 <a86749137@gmail.com>
2017-08-29 23:06:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65] //The keystrokes are up, up, down, down, left, right, left, right, B, A, Enter.
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  code;
+  let index = 0;
+  document.body.addEventListener("keydown", function(e){
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("You did it!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
