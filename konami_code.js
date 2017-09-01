7eb92d790ac7291cf0290109d2c8c1e815e8f7ed
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-01 15:01:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+document.addEventListener('keydown', onKeyDownHandler);
 
+let index=0;
+
+function onKeyDownHandler(e) {
+  const key= parseInt(e.location);
+
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      alert('Congratulations!');
+      index=0;
+    }
+  } else {
+    index=0;
+  }
+}
 }
