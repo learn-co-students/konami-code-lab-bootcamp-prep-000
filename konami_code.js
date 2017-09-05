23a76f453dab6ea3c1ecee8a3c5457286d11ab6a
Julie Singh <jsingh@alumni.iu.edu>
2017-09-05 18:25:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
   // Write your JavaScript code inside the init() function
+function init() {
+var i = 0
+document.body.addEventListener("keydown", function(e){
+var key = parseInt(e.which||e.detail||e.location)
+ 
+        if (key === code[i]) {
+            i++;
+            if (i === code.length) {
+                alert("Congratulations!!");
+                i = 0;
+            }
+        } else {
+            index = 0
+        }
+});
 
 }
