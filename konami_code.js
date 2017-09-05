628618018052b372e804edb311d19db04b873ac1
Daniel Lewis <danbrianlewis@gmail.com>
2017-09-05 14:39:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0
+
+function activation(){
+  alert("Konami!")
+}
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  var main = document.body
+  main.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which)
+    var keyNeeded = code[index]
+    if (key == keyNeeded){
+      index++
+      if (index+1 == code.length){
+        activation()
+      }
+    }else{
+      index = 0
+    }
+  })
 }
