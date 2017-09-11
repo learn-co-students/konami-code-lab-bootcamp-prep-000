0a318c7895ab549ccba0fc54b9de63e27be18723
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-11 16:58:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+let index = 0;
+let entered = [];
+
+function init() {
+  document.body.addEventListener("keydown", function(e) {
+    var keyed = parseInt(e.which|| e.detail);
+
+    if(keyed === code[index]) {
+      entered.push(keyed);
+      index++;
+      if(entered.length === code.length) {
+        alert("Congrats!");
+        index = 0;
+        entered = [];
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
