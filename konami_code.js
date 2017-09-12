80cc868c4f9c765860d5c460836bfac358ec5a97
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-09-12 03:19:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var index = 0
 
+const keyDown = document.querySelector('body')
+
+function init() {
+  keyDown.addEventListener('keydown', function(e) {
+    var key = parseInt(e.which || e.detail)
+    if (code[index] === key) {
+      index++
+      if (index === code.length) {
+        alert('Congradulations!')
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  })
 }
