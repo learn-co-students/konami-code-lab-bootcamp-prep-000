e6c98daedbe1c7b3fe02aa5767632c95378f2819
Lory Zhang <zhang.rouyun@gmail.com>
2017-09-14 00:45:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+document.body.addEventListener('keydown',onKeyDownHandler)
+var index=0;
+function onKeyDownHandler(keydown){
+var key=parseInt(keydown.detail||keydown.which||keydown.location);
+if(key===code[index]){
+  index++;
+  if (index===code.length){
+  window.alert('Congrats!');
+  index=0;
+    }
+  }
+    else{
+      index=0;
+    }
+  }
 }
