5dcab41cb45bf1cb61ad47805498907a8e77e117
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-14 00:10:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  var index = 0
+  document.querySelector('body').addEventListener('keydown', function(e){
+    let key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('Konami!')
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
