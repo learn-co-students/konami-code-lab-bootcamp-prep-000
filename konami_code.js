2ba5801d771dca2931947fd1fc634a376a69b16c
Rebecca Kaplan <beccatess@gmail.com>
2017-09-14 18:54:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,30 +1,25 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-var i = 0;
-
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-}
-
-
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-  document.body.addEventListener('keydown', function KeyboardEvent(KeyboardEvent.charCode) {
-    var keyboard = [];
-    for (var i = 0; i < code.length; i++) {
-        if(KeyboardEvent.charCode === code[i]){
-          keyboard.push(KeyboardEvent.charCode)
-        } else {
-            keyboard = [];
-          }
-        if(keyboard === code){
-          alert('Congratulations!')
-          }
-        }
+  var i = 0;
+
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[i]) {
+      i++;
+
+      if (i === code.length) {
+        alert("Hurray!");
+
+        i = 0;
       }
-    )
-  }
+    } else {
+      i = 0;
+    }
+  })
+}
 
 console.log(init());
