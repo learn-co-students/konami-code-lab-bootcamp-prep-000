8433265ccc903012856fd14d99b67f5c9ef0e6f1
Hazelin Ngan <hazelin.ngan@gmail.com>
2017-09-16 13:26:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let i=0;
 
+function init(e) {
+  const key = parseInt(e.detail || e.which);
+  if (key === code[i]){
+    i++;
+    if (i === code.length){
+      alert("Congratulations!");
+      i = 0;
+    }
+  }
+  else {
+    i = 0;
+  }
 }
