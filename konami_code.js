a169d6d46f0d0fe7bd8275861b87ce9088c110d3
Seth Barden <sethbarden@gmail.com>
2017-09-16 12:54:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+/*My code works in the browser. It is difficult to see what the tests are testing for in terms of..
+  the structure of the function. */
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  let index = 0
+  document.body.addEventListener('keydown', function(e) {
+    if (parseInt(e.which || e.detail) === code[index]) {
+    index ++ ;
+    if (index === code.length) {
+      alert("You unlocked the code!")
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+ })
 }
