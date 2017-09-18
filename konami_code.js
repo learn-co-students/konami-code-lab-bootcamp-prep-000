357b86ab2cad997e0cd05e1846a1f66a94454d57
Davin Dillon <davindillon@gmail.com>
2017-09-18 01:44:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   // Write your JavaScript code inside the init() function
 document.body.addEventListener('keydown', function (e){
     const key = parseInt(e.which || e.which);
-    if (key === code[index]) {
+    while (key === code[index]) {
       //console.log(key);
         index++;
       if (index === code.length) {
