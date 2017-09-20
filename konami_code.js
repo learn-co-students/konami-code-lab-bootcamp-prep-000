8d47d3bc40c2cb1b3b2326d438202fd3873c97ce
DANIEL CHAN <dchan513@msn.com>
2017-09-20 02:34:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//up, up, down down, left, right left right B A
+
 
 function init() {
   // Write your JavaScript code inside the init() function
+
+  let index =0;
+  const test = document.querySelector('body')
+  test.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    //console.log(key)
+
+    if(key === code[index]){
+      index++;
+
+      if(index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 
 }
