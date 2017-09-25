8ea49ba307c9c793a54895631b228cd5efdd0e6c
Chris Cardle <cdcardle@gmail.com>
2017-09-25 15:15:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const body = document.querySelector(`body`);
+  let l = code.length;
+  let i = 0;
 
+  body.addEventListener(`keydown`, function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[i]) {
+    i++;
+      if (i === l) {
+      alert(`Secret Unlocked!`);
+      i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  })
 }
+
+init();
