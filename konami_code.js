e3045c2104446b6e3cd3dacaede8e10e301739f6
Joshua Carlson <carlson.joshuaph@gmail.com>
2017-09-28 17:51:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(key);
+  if (key === code[index]) {
+    index++;
 
-function init() {
-  // Write your JavaScript code inside the init() function
+    if (index === code.length) {
+      alert("Hurray!");
 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+function init() {
+  document.body.addEventListener('keydown', onKeyDownHandler)
+}
+
+init();
