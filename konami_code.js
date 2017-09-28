866b895685086218bf2ce053e14b0e9280deaf10
Armando <salixonline@gmail.com>
2017-09-28 22:07:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const body = document.querySelector('body');
+  var sequence = [];
 
+  body.addEventListener('keydown', function(e) {
+    if (sequence.length === 10) {
+      sequence.shift();
+    }
+    sequence.push(parseInt(e.detail || e.which));
+    if (sequence.length < 10) {
+      return false;
+    }
+    for (var i = 0; i < code.length; i++) {
+      if (sequence[i] !== code[i]) {
+        return false;
+      }
+    }
+    alert("Konami code entered! 😀\n⬆️⬆️⬇️⬇️⬅️➡️⬅️➡️🅱️🅰️");
+  });
 }
