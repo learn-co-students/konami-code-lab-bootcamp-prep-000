c2a01b7ad94b02af26d08d22a7b455ec7ce20728
Sandy Yeung <sandynism@gmail.com>
2017-09-30 18:46:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-
 var index = 0
 
-function onKeyDownHandler(e) {
-  document.body.addEventListening('keydown', function(e) {
+function init() {
+
+  document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
