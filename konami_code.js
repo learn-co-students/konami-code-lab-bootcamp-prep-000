f833124118aec41ea1fdedff7c55d6b595abb327
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-02 22:36:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
+  const body = document.body
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+   	console.log(key);
+	  if (key === code[index]) {
+      index++;
 
+      if (index === code.length) {
+          alert("Hurray!");
+          index = 0;
+        }
+    } else {
+      index = 0;
+    }
+  })
 }
