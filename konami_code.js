477a43439ff9129f1e5b15ad9c7bf9e4a7b80ae1
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-04 19:46:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,31 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  const body = document.querySelectorAll('body')
+// Keep track of index outside of the event handler.
+let index = 0
 
-  body.addEventListener('keydown', function(e) {
-    if (e.which === code) {
-      alert('CONGRATULATIONS!!!')
-    }
-  }
+//When you're testing the code out in the browser, be sure to call init() to attach the event listener and set everything up
+function init() {
+  const body = document.body
+
+  body.addEventListener('keydown', onKeyDownHandler(e))
 }
 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 
 /*
 const main = document.getElementById('main')
