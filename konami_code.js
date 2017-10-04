8f1b0dfc75da094d7c85f7e14a1207158f3f59f4
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-04 16:42:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const body = document.querySelectorAll('body')
 
+  body.addEventListener('keydown', function(e) {
+    if (e.which === code) {
+      alert('CONGRATULATIONS!!!')
+    }
+  }
 }
+
+
+/*
+const main = document.getElementById('main')
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
+
+const input = document.querySelector('input')
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+*/
