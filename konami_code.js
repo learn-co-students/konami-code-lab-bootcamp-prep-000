021dbc60a46d9301d8287b0b8044516e81b0a9cf
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 06:55:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  const input = document.getElementsByTagName("BODY")[0];
+
+  input.addEventListener('keydown', function(e) {
+    testCode(e.detail || e.which)
+  })
+
+}
+
+function testCode(key) {
+  key = parseInt(key)
+  if(key == code[index]) {
+    index++;
+
+    if(index == code.length) {
+      alert("Hurray!")
+      index = 0;
+    }
+
+  } else {
+    index = 0;
+  }
 }
