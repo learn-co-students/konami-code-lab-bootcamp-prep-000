07661d9162edda4682c70b17c26c51e8104b8a8e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-08 05:28:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-var index =0
+
 function init()
 {
   // Write your JavaScript code inside the init() function
+  var index =0
   document.body.addEventListener('keydown', function(e) {
     console.log(e.detail)
     console.log(e.which)
@@ -25,4 +26,4 @@ function init()
 }
 })
 }
-init()
+//init()
