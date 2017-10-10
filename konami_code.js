cb2ee490773a71d72a746aef426842dbd44259ce
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-10 18:21:42 -0400

    Update konami_code.js
    added comments

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,22 @@
+// Konami key codes
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
+// Keep track of index outside of the event handler
 let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
     document.body.addEventListener('keydown', function(e) {
+      // const to hold key code on keydown event
       const key = parseInt(e.detail || e.which);
-
+      // if the key is the same as the code[index] value, go to the next code index
       if(key === code[index]) {
         index++;
-
+        // if index is equal to the code.length value, alert "Success" and reset index to 0
         if(index === code.length) {
           alert("Konami Code Success!");
-
           index = 0;
         }
+        // if a key out of sequence or a key that isn't part of the Konami code is pressed set index to 0
       } else {
         index = 0;
       }
