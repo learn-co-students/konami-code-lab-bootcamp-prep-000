7d1afe8f3e09d6f3e217ebc6a9b2af287f267159
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-10 18:23:41 -0400

    Update konami_code.js
    added comment

--- konami_code.js
+++ konami_code.js
@@ -5,6 +5,7 @@ let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
+    // event listener for keydown events
     document.body.addEventListener('keydown', function(e) {
       // const to hold key code on keydown event
       const key = parseInt(e.detail || e.which);
