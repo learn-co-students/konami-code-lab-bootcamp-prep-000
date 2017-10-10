3f14ae0f3301082ae265b0124c453cb3cb8c5966
Emika Ijuin <emikaijuin@gmail.com>
2017-10-10 04:44:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,35 @@
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+//
+// function init() {
+//   let index = 0;
+//   document.body.addEventListener('keydown', function(e){
+//     if (e.which === code[index]) {
+//       index++
+//     }
+//     if (e.which != code[index]) {
+//       index = 0
+//     }
+//     if (index === code.length) {
+//       window.alert('Hurray!')
+//     }
+//   })
+// }
+
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which)
+    if (key === code[index]) {
+      index++
+    }
+    else if (key != code[index]) {
+      index = 0
+    }
+    if (index === code.length) {
+      window.alert('Hurray!')
+    }
+  })
 }
