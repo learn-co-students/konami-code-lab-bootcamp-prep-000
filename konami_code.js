865eb2436fe43b4c0bde87aafc4bd8fa895065b2
xxDOOMbox <shawwian@gmail.com>
2017-10-11 17:23:34 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init() {
-  // your code here
-}
+let index = 0; 
+
+function init() { 
+  document.body.addEventListener('keydown', function (e) {
+    let key = parseInt(e.detail);
+   
+    if (key === code[index]) {
+      index++;
+   
+      if (index === code.length - 1) {
+        alert("KAAABOOOOOOOSH!!!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+
+};
+
+
+
