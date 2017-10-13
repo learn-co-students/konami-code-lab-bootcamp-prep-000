6a06eda0261f24237b2e1fce88350f8647a83e18
Aslan1st <aslan1st@mac.com>
2017-10-13 06:16:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var correct = []
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+document.body.addEventListener('keydown', keyDownHandler)
+
+
+function init(){
+
+}
+
+function keyDownHandler(e){
+  const key = parseInt(e.detail||e.location||e.which)
+  correct.push(key)
+
+  let index = correct.length - 1
+
+if (correct[index] != code[index]) {
+     correct = [];
+}
+if ((code.length == correct.length) && code.every(function(element, index) {
+    return element === correct[index];
+}))
+{
+alert('You did it!')
+correct = []
+  }
 }
