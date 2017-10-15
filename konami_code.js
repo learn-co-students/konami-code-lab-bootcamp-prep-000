da9d364bb6aa3493882e6df97b5cce8c02d1c6b3
Andrew Siplas <andrew@asiplas.net>
2017-10-15 00:43:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var inputBuf = []
+  var match
+  document.body.addEventListener('keydown', function(e) {
+    if (inputBuf.length >= code.length) {
+      inputBuf.shift()
+    }
+    inputBuf.push(e.detail || e.which)
 
+    match = true
+    for (let c in code) {
+      if (code[c] !== parseInt(inputBuf[c], 10)) {
+        match = false
+      }
+    }
+
+    if (match === true) {
+      alert('＼(≧▽≦)／ [573] ٩(◕‿◕)۶ [573] ＼(≧▽≦)／')
+    }
+  })
 }
