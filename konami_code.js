d669bf6aeb80b507880d6bbd72eda0645d722d43
Christopher Diep <diep.christopher@gmail.com>
2017-10-17 03:36:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,32 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
+
+// function init() {
+//   document.body.addEventListener('keydown',alert(`Hurray!`)) // Write your JavaScript code inside the init() function
+// }
+
+const body = document.querySelector('body')
+
+function keydownHandler() {
+  body.addEventListener('keydown',function(e) {
+
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
+}
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  keydownHandler()
 }
