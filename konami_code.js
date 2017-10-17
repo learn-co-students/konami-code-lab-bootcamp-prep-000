8efa0aafc6f6a0d568968822c42cb122d5298932
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-17 12:05:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,31 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// 38 == up-arrow
+// 40 == down-arrow
+// 37 == left-arrow
+// 39 == right-arrow
+// 66 == B
+// 64 == A
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var body = document.querySelector('body')
+  var arr = []
 
+  body.addEventListener('keydown', function (e) {
+    e.preventDefault()
+    var keyCode = e.keyCode || e.location
+    arr.push(parseInt(keyCode))
+    if (arr.length == 10) {
+      for (var i = 0; i < 10; i++) {
+        if (arr[i] !== code[i]) {
+          arr = []
+          break
+        }
+        else if((arr[i] === code[i]) && i == 9) {
+          alert("WE DID IT")
+          arr = []
+        }
+      }
+    }
+  })
 }
+init()
