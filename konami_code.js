9e300c3e73c9d5ab8fae538db2857c06c8f87085
Jacob Miller <jake0miller@gmail.com>
2017-10-18 07:21:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var comp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const main = document.body
+  main.addEventListener('keydown', onKeyDownHandler)
+}
 
+init()
+
+function onKeyDownHandler2(e){
+  const key = parseInt(e.which || e.detail)
+  console.log(e.which || e.detail)
+
+  if (key === code[index]){
+    index++
+    if (index === code.length){
+      alert('Congratualtions!')
+      index = 0
+    }
+  } else{
+    index = 0
+  }
 }
