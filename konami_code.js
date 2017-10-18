c177a8e14e5f9cf3aa353f90d0bf4e4ef24e4d15
Jacob Miller <jake0miller@gmail.com>
2017-10-18 09:26:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,5 @@
+<<<<<<< HEAD
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-var comp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 let index = 0
 
 function init() {
@@ -11,6 +11,21 @@ main.addEventListener('keydown', onKeyDownHandler)
 function onKeyDownHandler(e){
   const key = parseInt(e.which || e.detail)
   console.log(e.which || e.detail)
+=======
+const code = [38] //[38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var comp = [0]//[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let index = 0
+
+function init() {
+  const main = document.querySelector('body')
+
+  main.addEventListener('keydown', function(e) {
+  })
+}
+
+function onKeyDownHandler(e){
+  const key = parseInt(e.which || e.detail)
+>>>>>>> f44a66a0c3130882a8aac4a03a7e7000a53d7c14
 
   if (key === code[index]){
     index++
@@ -22,3 +37,16 @@ function onKeyDownHandler(e){
     index = 0
   }
 }
+<<<<<<< HEAD
+=======
+
+  //comp.push(parseInt(e.which || e.detail))
+  //comp.shift()
+  //if (code === comp){
+    //console.log(e.which || e.detail)
+    //alert('Congratulations!')
+  //}
+//}
+
+init()
+>>>>>>> f44a66a0c3130882a8aac4a03a7e7000a53d7c14
