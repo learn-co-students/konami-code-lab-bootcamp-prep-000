753164b6312436acbaf011543372b609224fba36
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-19 03:06:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let i = 0
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which)
 
+    if (key === code[i]){
+      i++
+      if (i === code.length){
+        alert("yay~!")
+        i=0
+      }
+    } else {
+      i=0
+    }
+  })
+  // Write your JavaScript code inside the init() function
 }
