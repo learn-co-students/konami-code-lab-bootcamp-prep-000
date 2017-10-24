5f8387c597708d3cab412dfc7cc08c1158a13605
briecoyle <brie.coyle@gmail.com>
2017-10-24 18:36:40 -0400

    Revert to starting lab status

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
-  var index = 0
+  // Write your JavaScript code inside the init() function
 
-  document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which)
-
-    if (key === code[index]) {
-      index++
-
-      if (index === code.length - 1) {
-        alert("YOU DID IT!")
-        index = 0
-      }
-    } else {
-      index = 0
-    }
-  })
 }
