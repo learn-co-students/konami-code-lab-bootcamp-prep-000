089fa22fe41fe0032994eb8effe1be49309d3b8f
Andrew Hong <hongandrew16@gmail.com>
2017-10-24 08:00:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -16,25 +16,3 @@ function init() {
     }
   });
 }
-
-// const body = document.body.addEventListener('keydown', init);
-// let entered = [];
-//
-// function init(e) {
-//   if(entered.length===10) {
-//     entered.shift();
-//   }
-//   let key = parseInt(e.which || e.detail);
-//   entered.push(key);
-//   console.log(entered);
-//   let check = true;
-//   for(let i=0;i<code.length;i++) {
-//     if(code[i] != entered[i]) {
-//         check = false;
-//     }
-//   }
-//   if(check===true) {
-//     alert("Yeeee");
-//   }
-//
-// };
