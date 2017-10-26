81523beda5f71d4bb33ed5752b041d5d1174d7da
Domnik Mack <domnikmack@gmail.com>
2017-10-26 20:16:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let index = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.getElementsByTagName('body')[0].addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      console.log('correct');
+      index++;
+      if (index === code.length) {
+        alert('Congrats!');
+        index = 0;
+      }
+    } else {
+	console.log('false');
+    index = 0;
+    }
+  });
 }
