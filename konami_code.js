5d6019a5c4910b969800f67cd9e53ef58b82cf20
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-27 01:00:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init(e) {
-  document.body.addEventListener('keydown', (e) => {
+  document.body.addEventListener('keydown', function(e) => {
   const key = parseInt(e.detail || e.which);
 
       if (key === code[index]) {
