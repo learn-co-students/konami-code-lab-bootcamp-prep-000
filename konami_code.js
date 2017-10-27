cadea8c709a7e17011e71f53230664d25a93a983
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-27 14:19:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(event) {
+    onKeyDownHandler(event);
+  });
+}
 
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert('Konami code successful!');
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
