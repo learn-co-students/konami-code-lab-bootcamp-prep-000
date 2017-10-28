b3f9fcf995745765d68f717c91d56189568bad92
Shahin Motia <chromepanthers@gmail.com>
2017-10-28 18:15:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  document.body.addEventListener('keydown', function(event) {
+    const keyStroke = parseInt(event.detail || event.which);
+
+    if (keyStroke === code[index]) {
+      console.log(keyStroke);
+      index++;
+      console.log(index)
+      if (index === code.length) {
+        alert("27 Extra Lives!");
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  })
 }
