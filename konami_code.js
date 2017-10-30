3d9455362a9e213f3cac9132ff2d9fab72e8c923
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-30 22:54:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,18 +3,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 init();
 
 function init() {
-
+var i=0;
   document.addEventListener('keydown', (e)=>{
-    console.log(e.key);
-    // for(i=0;i<e.key.length; i++){
-    //   if(e.key == code[i]){
-    //     i++;
-    //   }else{
-    //     i--;
-    //   }
-    // }
+    if(i == code.length-1){ //if reach end of array
+      alert("congratulatory message"); //alert
+      console.log("Congrats");
+      i=0;
+    }
+    console.log("index "   + i);
+    console.log("e.which " e.which);
+    console.log("code[i] " + code[i]);
+    console.log("e.which || e.detail " +e.which || e.detail);
+
+    console.log((e.which || e.detail) === code[i]);
+      if((e.which || e.detail) === code[i]){ //if key val is equal to array
+        i++;
+      }else {
+        i=0;
+      }
 
   });
-  // Write your JavaScript code inside the init() function
-
 }
