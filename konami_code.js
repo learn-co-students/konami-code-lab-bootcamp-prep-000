33279bd72767804849909b38f827298695cfc57f
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-30 21:41:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+init();
+
 function init() {
+
+  document.addEventListener('keydown', (e)=>{
+    console.log(e.key);
+    // for(i=0;i<e.key.length; i++){
+    //   if(e.key == code[i]){
+    //     i++;
+    //   }else{
+    //     i--;
+    //   }
+    // }
+
+  });
   // Write your JavaScript code inside the init() function
 
 }
