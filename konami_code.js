211d2eb1a123886c5038f74c1066f992cad7ab51
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-30 23:09:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -4,19 +4,20 @@ init();
 
 function init() {
 var i=0;
-  document.addEventListener('keydown', (e)=>{
+  document.body.addEventListener('keydown', (e)=>{
     if(i == code.length-1){ //if reach end of array
-      alert("congratulatory message"); //alert
-      console.log("Congrats");
-      i=0;
+      if(parseInt(e.which || e.detail) === code[i]){ //if key val is equal to array
+      window.alert("congratulatory message"); //alert
+      }
+    i=0;
     }
     console.log("index "   + i);
-    console.log("e.which " e.which);
+    console.log("e.which " + e.which);
     console.log("code[i] " + code[i]);
-    console.log("e.which || e.detail " +e.which || e.detail);
+    console.log("e.which || e.detail " + (e.which || e.detail));
 
-    console.log((e.which || e.detail) === code[i]);
-      if((e.which || e.detail) === code[i]){ //if key val is equal to array
+    console.log(parseInt(e.which || e.detail) === code[i]);
+      if(parseInt(e.which || e.detail) === code[i]){ //if key val is equal to array
         i++;
       }else {
         i=0;
