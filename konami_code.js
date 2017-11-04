9d11490ccdcc6f1ab2c58a4f8510dbacae3e022a
Francisco Tamay <tamayfrancisco95@yahoo.com>
2017-11-04 22:35:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  var index = 0;
+  document.body.addEventListener('keydown', function(e){
+    let key = parseInt(e.detail || e.which);
+    if (key === code[index]){
+        index++;
+        if (index === code.length-1){
+          window.alert('Hurray');
+          index = 0;
+        }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+init();
