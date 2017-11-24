bbf813cdc2aedd5ee5d3bf66ca6b0474f1d94bd9
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-24 03:19:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  console.log('initialized')
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (code[index] === key) {
+      index++
+      console.log(index)
+    } else {
+      index = 0
+    }
+
+    if(index === code.length) {
+       alert('Congratultions! You entered the Konami Code.');
+    }
+
+  } );
 
 }
