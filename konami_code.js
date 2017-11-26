6251455c86695f32c90e135da6ebf4e305103f86
Brian <deltoro.brian@gmail.com>
2017-11-26 18:05:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+//listen for key presses
+//store them in an array
+//compare to code
+//do the alerts or not
 function init() {
   // Write your JavaScript code inside the init() function
 
+  // document.body.addEventListener('keydown', function(e) {
+  //   console.log(e.which);
+  // })
+  // function hey(e){
+  //   e.innerHTML = "hey";
+  // }
+  // var clicky = document.getElementById('theP');
+  // clicky.addEventListener('keydown',hey)
+
 }
+
+init();
