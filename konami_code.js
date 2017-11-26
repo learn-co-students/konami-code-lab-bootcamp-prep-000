41fdb2acb434930acec481d7e77c195c9c968d11
Devon Langendoerfer <425devon@gmail.com>
2017-11-26 04:41:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(){
+  document.body.addEventListener('keydown', function(e){
+  const key = parseInt(e.key || e.detail);
 
+     if (key === code[index]) {
+       index++;
+
+        if (index === code.length) {
+          alert("You have unleashed an awesome secret!");
+       }
+     } else {
+       console.log('no dice!')
+        index = 0;
+      }
+    });
 }
