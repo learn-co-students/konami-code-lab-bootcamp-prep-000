63b2cda02f8b71d2ca8f692801fe38a11fe6c1a2
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-27 20:36:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,35 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+/*function init() {
+  let index = 0;
+  function onKeyDownHandler(i){
+    const key = parseInt(i.which);
+      if (key === code[index]) {
+          index++;
+          if (index === code.length) {
+            alert("Congratulations fine warrior. You have cracked the konami code.");
 
+            index = 0;
+          }
+      } else {
+        index = 0;
+      }
+    }
+  document.body.addEventListener('keydown', onKeyDownHandler(i))
 }
+*/
+function init(){
+  let index = 0;
+  document.body.addEventListener('keydown', function(event){
+    const key = parseInt(event.detail);
+    if (key===code[index]){
+      index++;
+      if (index === code.length) {
+        alert("Congratulations fine warrior. You have cracked the konami code.");
+        index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    })
+  }
