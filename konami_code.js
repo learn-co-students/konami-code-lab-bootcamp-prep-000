3863c3adbe523d330391b37d5539fde7b0669cf0
Karen Zhao <karen.zhao18@gmail.com>
2017-11-28 19:59:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,42 +1,88 @@
-// // const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// // const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 // // let index = 0;
 // //
 // // function init(e) {
-// //   // Write your JavaScript code inside the init() function
 // //   const key = parseInt(e.detail || e.which);
-// //   console.log(e.detail);
-// //   console.log(e.which);
 // //   if (key === code[index]) {
 // //     index++;
-// //
 // //     if (index === code.length) {
 // //       alert('Hurray!');
-// //
 // //       index = 0;
 // //     }
 // //   } else {
 // //     index = 0;
 // //   }
 // // }
-// //
 // // init();
+//
+// // var keys = [];
+// // const code = [38,38,40,40,37,39,37,39,66,65];
 // //
+// // document.addEventListener('keydown', (e) => {
+// //     keys.push(e.keyCode);
+// //     keys.splice(-code.length-1, keys.length-code.length);
+// //     if (keys == code) {
+// //         alert('hurray');
+// //         keys = [];
+// //     }
+// //     else {
+// //       keys = [];
+// //     }
+// // }
 //
-// if (window.addEventListener) {
-//     var keys = [],
-//         konami = "38,38,40,40,37,39,37,39,66,65";
+// // function init() {
+// //   // document.body.style.backgroundImage = "url('images/cheatBackground.png')";
+// //   //
+// //   // var audio = new Audio('audio/pling.mp3');
+// //   // audio.play();
+// //   alert("cheats activated");
+// // }
 //
-//     window.addEventListener("keydown", function init(e){
-//
-//         keys.push(e.keyCode);
-//
-//         if (keys.toString().indexOf(konami) >= 0) {
-//
-//             alert('This is where the magic would happen');
-//
-//             keys = [];
-//         };
-//     }, true);
+// var allowedKeys = {
+//   37: 'left',
+//   38: 'up',
+//   39: 'right',
+//   40: 'down',
+//   65: 'a',
+//   66: 'b'
 // };
 //
-// init();
+// var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+// var konamiCodePosition = 0;
+//
+// function onKeyDownHandler(e) {
+//   var key = allowedKeys[e.keyCode];
+//   var requiredKey = konamiCode[konamiCodePosition];
+//   if (key == requiredKey) {
+//     konamiCodePosition++;
+//     if (konamiCodePosition == konamiCode.length) {
+//       alert("cheats activated");
+//       konamiCodePosition = 0;
+//     }
+//   } else {
+//     konamiCodePosition = 0;
+//   }
+// }
+//
+// window.addEventListener('keydown', onKeyDownHandler());
+
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
+
+// This is the function that would be invoked by the event listener.
+function init(event) {
+  const key = parseInt(event.detail || event.keyCode || event.which);
+
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+init();
