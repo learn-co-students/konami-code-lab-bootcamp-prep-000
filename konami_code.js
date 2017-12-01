3d23744127fdce5ff9145b3667f444d75b93f0ef
David J Towster <djt4xe@virginia.edu>
2017-12-01 07:39:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0
 
+function init() {
+  document.body.addEventListener('keydown', function(event) {
+    //alert('yo');
+    //console.log(parseInt(event.detail || event.which));
+    //console.log(code.length);
+    //console.log(index);
+    var key = parseInt(event.detail || event.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        //console.log("should be saying hurray");
+        index = 0;
+        alert('Hurray!');
+      }
+    }
+    else {
+      index = 0;
+    }
+    //console.log(index);
+  });
 }
