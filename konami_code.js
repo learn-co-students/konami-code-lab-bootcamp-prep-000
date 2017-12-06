ba4dc7ad556835c8f682c61b776c9c46d4778579
J.D. Bean <jonathandbean@gmail.com>
2017-12-06 20:21:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+const body = document.body
+
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  body.addEventListener('keydown', function test(e) {
+    const key = parseInt(e.detail || e.which);
+    //*console.log(e.which);*//
+    if (key === code[index]) {
+      index++;
 
+      if (index === code.length) {
+        alert("Way to go; you have been granted infinite lives!");
+
+        index = 0;
+      }
+
+    } else {
+      index = 0;
+    }
+  })
 }
