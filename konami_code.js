97966565d9a99803dbe58060d77efaa927d0082d
Michael Farber <farbohydrate@gmail.com>
2017-12-08 22:45:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var index = 0;
 
+function init() {
+  // your code here
+  document.addEventListener('keydown', function(e) {
+  const key = parseInt(e.which || e.detail);
+  console.log(key);
+  if(key === code[index]){
+    if(index === code.length - 1){
+      alert("You did it!");
+    }
+    index++;
+  } else {
+    index = 0;
+  }
+})
 }
