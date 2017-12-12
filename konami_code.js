2305ef56d1cba8fbdfb3347620dde150f05661d8
sanober riaz <sanober@sanober.me>
2017-12-12 05:12:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+  var index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
-  var index = 0;
+
   document.body.addEventListener('keydown', function(e){
     const key = parseInt(e.which || e.detail);
 
