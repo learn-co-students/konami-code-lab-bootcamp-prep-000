48e5236337040c42765379634a8177975ba9fdfa
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-12 01:25:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,47 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(){
+  const keyboardEvent = document.createEvent("KeyboardEvent")
 
-}
+  const body = document.querySelector('body')
+
+  let index = 0
+  document.body.addEventListener('keydown', function(e){
+
+    if (parseInt(e.which || e.detail) === code[index]){
+        index++
+
+        if (index ===code.length){
+          alert("YAY!")
+          console.log("Got it")
+        }
+      }
+     else{
+      index = 0
+    }})}
+
+    init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tasks//
+//create event listener for keyboard//
+//create a function that listens for a specific event //
+    //if that occurs, enter a function that listens for a second event//
+      //and so on//
+        //create a function so that if code is completed, an alert with message gets printed//
+//if code is not entered properly, listener events always restart//
