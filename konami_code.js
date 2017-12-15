5c26698ce3e15dd5689accf5c3237aa9b369ab20
Erin Witmer <ecwitmer@gmail.com>
2017-12-15 19:23:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,26 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  let array = [];
-  document.onkeydown = function(e) {
+  document.body.addEventListener('keydown', function(e) {
     let expected = code[index]
-    let actual = e.keyCode
+    let actual = parseInt(e.which || e.detail);
     if (expected === actual) {
-      array.push(actual)
       index++
-      console.log(array)
-      if (array.length === code.length) {
+      if (index === code.length) {
         alert('you won')
-        console.log('you won')
         index = 0;
-        array = [];
       }
     } else {
       index = 0;
-      array = [];
-      console.log(array)
     }
-  }
+  })
 }
 
 init()
