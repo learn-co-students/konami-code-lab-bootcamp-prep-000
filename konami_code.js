8d4e748e3a44f087b6f23401bc5bed0756a96a41
Beth Schofield <bethmschofield@gmail.com>
2017-12-20 23:06:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const input = document.querySelector('body')
+
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
+
+  input.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which);
+    console.log(e.which)
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 
 }
+
+init();
