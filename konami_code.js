5692b9bdb1cddc448dad3ae9eb2b9c261db7bf5d
jencekade <gaephuoc@gmail.com>
2017-12-27 09:21:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,69 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+function init() {
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (key === code[code.length-1]) {
+        alert(`This is the Konami Code!`);
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+}
+init();
+
+/* commented out
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  document.body.addEventListener('keydown', function(e){
+    let key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      let key = parseInt(e.detail || e.which);
+      if(key === code[index]){
+        index++;
+        let key = parseInt(e.detail || e.which);
+        if (key === code[index]) {
+          index++;
+          let key = parseInt(e.detail || e.which);
+          if (key === code[index]) {
+            index++;
+            let key = parseInt(e.detail || e.which);
+            if (key === code[index]) {
+              index++;
+              let key = parseInt(e.detail || e.which);
+              if (key === code[index]) {
+                index++;
+                let key = parseInt(e.detail || e.which);
+                if (key === code[index]) {
+                  index++;
+                  let key = parseInt(e.detail || e.which);
+                  if (key === code[index]) {
+                    index++;
+                    let key = parseInt(e.detail || e.which);
+                    if (key === code[index]) {
+                      index++;
+                      let key = parseInt(e.detail || e.which);
+                      if (key === code[index]) {
+                        alert(`Konami Code.`);
+                      }
+                    } else {index = 0;}
+                  } else {index = 0;}
+                } else {index = 0;}
+              } else {index = 0;}
+            } else {index = 0;}
+          } else {index = 0;}
+        } else {index = 0;}
+      } else {index = 0;}
+    } else {index = 0;}
+  }
 }
+init();
+*/
