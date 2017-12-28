a3dd686b164762fe02d69f1b9882f1fe1b3a9e83
michael lynn <michael_lynn@comcast.net>
2017-12-28 21:35:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,22 +1,27 @@
-// check to make sure that the browser can handle window.addEventListener
-if (window.addEventListener) {
-    // create the keys and konami variables
-    
-    var keys = [],
-        konami = "38,38,40,40,37,39,37,39,66,65";
-    
-    // bind the keydown event to the Konami function
-    window.addEventListener("keydown", function(e){
-        // push the keycode to the 'keys' array
-        keys.push(e.keyCode);
-        
-        // and check to see if the user has entered the Konami code
-        if (keys.toString().indexOf(konami) >= 0) {
-            // do something such as:
-            // alert('Konami');
-            
-            // and finally clean up the keys array
-            keys = [];
-        };
-    }, true);
-};
+function init(){
+  
+// Key codes for A, B, and C keys.
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+ 
+// Keep track of index outside of the event handler.
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === code.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+document.body.addEventListener("keydown",onKeyDownHandler)
+}
+
