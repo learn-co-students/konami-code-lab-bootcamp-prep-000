579a3e605ada51041c961fab44da52f11ec4a6e5
winter mutt <sejongsan@hotmail.com>
2017-12-30 22:34:24 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+  let index = 0;
+  const body = document.querySelector('body');
+  function onKeyDownHandler(e){
+    const key = parseInt(e.which || e.detail);
+    if(key === code[index]){
+      index++;
+      if(index === code.length){
+        alert("Konami!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+
+  body.addEventListener('keydown', function(e){
+      onKeyDownHandler(e);
+  });
 }
