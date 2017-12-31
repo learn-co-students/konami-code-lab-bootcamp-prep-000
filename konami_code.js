9dd084b26baa982d444f5345b6550db648a545b0
Micah Shute <micah.shute@gmail.com>
2017-12-31 03:12:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+function codeTester(e){
+  const key = isNaN(parseInt(e.which || e.detail)) ? e.which : parseInt(e.which || e.detail);
+  console.log(key);
+  if(key === code[codeFollower]){
+    if(codeFollower === code.length - 1){
+      alert('Cheat Entered.');
+      codeFollower = 0;
+      return;
+    }
+    codeFollower++;
+  }else{
+    codeFollower = 0;
+  }
+  
+  console.log(codeFollower);
+  
+}
+
+var codeFollower = 0;
+
+init()
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  
+  const cheatListener = document.addEventListener('keydown', codeTester);
+  
 }
