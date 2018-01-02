38c93d5f49dc5db130f6c7c9c1e8b63d195f94ca
Ed Wright <ew_wrightiii@live.com>
2018-01-02 06:06:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+let index = 0;
+function downKeyHandler(e){
+  const key = parseInt(e.detail || e.location);
+  console.log(e.detail);
+  if(key === code[index]){
+    index++;
+    if(index == code.length){
+      alert("Congratulations!");
+      index = 0;
+    }
+  }else{
+    index = 0;
+  }
 }
+
+function init() {
+  const input = document.querySelector('body');
+  input.addEventListener('keydown', downKeyHandler);
+}
+init();
