598c3399f85cb811f64106878fb0f51ff4c9b627
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-07 08:21:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,27 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  var index = 0;
 
+  var konami = document.querySelector('body');
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+      if (index === (code.length)) {
+        window.alert('Congratulations!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+konami.addEventListener('keydown', onKeyDownHandler);
+
+console.log('Init success');
 }
+
+init();
