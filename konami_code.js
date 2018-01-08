e26fdb5a200db66e6ccba56b9772e6d6373ec905
Edward Norton <edwardnorton@icloud.com>
2018-01-08 23:12:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
 
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
