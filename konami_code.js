b97260e68fc7551c6ac007e71c896cdf39cfc422
Ariel Feingold <feingold.ariel@gmail.com>
2018-01-08 19:47:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const body = document.querySelector(`body`);
+let index = 0;
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(e){
+	body.addEventListener(`keydown`, function(e){
 
+		const key = parseInt(e.detail || e.which); 
+				if(key === code[index]){
+					index++;
+					console.log(`${key} correct`);
+				  if(index === code.length){
+				  	alert(`You Made It`);
+				  	index = 0;
+				  	}
+				  } else{
+				  	console.log(`${key} wrong`);
+				  	index = 0;
+				  }
+		});
 }
