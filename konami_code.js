e2fa33f3872dd295285b36ebf85bbf53a09e6617
desmond beramendi <desmond7187@gmail.com>
2018-01-09 03:43:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+var indx = 0; 
+document.body.addEventListener('keydown', function(evnt) {
+  const key = parseInt(evnt.which || evnt.detail)
+  if(code[indx] === key) {
+    indx++
+    if(indx === code.length) {
+      alert("Hurray MOTHERFUCKER!")
+      indx = 0 
+    }
+  } else {
+    indx = 0 
+  }
+
+})
+
 
 }
