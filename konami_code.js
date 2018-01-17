c20f2e8bcc0d1de155c7514b3a291481f7632083
Marvin Clerge <clergemarvin@gmail.com>
2018-01-17 08:58:39 -0500

    Done

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', handleKonamiCode);
+  let input = 0;
+  function handleKonamiCode(event) {
+    const key = parseInt(event.which || event.detail);
+    if (key === code[input]) {
+      input++;
+      if (input === code.length) {
+        alert('Woo');
+      }
+    } else {
+      input = 0;
+    }
+  }
 }
