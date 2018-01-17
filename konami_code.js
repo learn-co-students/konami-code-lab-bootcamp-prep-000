cabc11030ae0daad37143d1c5304592f65978e5f
kendocode <git@kendocoding.com>
2018-01-16 19:53:56 -0600

    konami code

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var index = 0
 
+
+document.body.addEventListener('keydown', init);
+
+function init(e) {
+  const key = e.keyCode;
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+
+// function init(e) {
+//   alert(e.char);
+// }
+
+// function properties(e) {
+//   alert( `target is ${e.target}\ntype is ${e.type}\nchar is ${e.char}\nkey is ${e.key}
+//           \ncode is ${e.code}\ncharCode is ${e.charCode}\nkeyCode is ${e.keyCode}\nwhich is ${e.which}
+//          \nlocation is ${e.location}`)}
+
+
