89195777f8d79aee8a54a96fd8b37ab27b644899
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-19 20:26:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init() {
-  // Write your JavaScript code inside the init() function
-
+let index = 0;
+function init(kon) {
+  const key = parseInt(kon.detail || kon.which);
+  
+  if (key === code[index]) {
+    index++;
+  }
+  if (index === code.length) {
+    alert ('Congrats, you did it!!');
+    index = 0;
+  } else { 
+    index = 0;
+  }
 }
+
