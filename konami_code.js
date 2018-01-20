944338cf23dd126f43621f7f0a77766fb48c285c
Dawn Ann Beattie <heartandhandstraining@yahoo.com>
2018-01-20 23:45:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
-}
+    let index = 0;
+    document.body.addEventListener('keydown', onKeyDownHandler(e) {
+        var key = parseInt(e.detail || e.which);
+        if (key === code[index]) {
+            index++;
+            if (index === code.length) {
+                window.alert(`Congratulations! You got the entire ordered Konami Code!`);
+                index = 0;
+              }  
+        } else {
+            index = 0;
+        }
+      })
+    }
