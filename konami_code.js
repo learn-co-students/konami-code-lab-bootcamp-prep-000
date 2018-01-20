f5c034e4bc984b7a061617eb97514f0f02f7e7b4
Laurents Mohr <laurents.mohr@whu.edu>
2018-01-20 15:53:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,27 +1,23 @@
 const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-
+function init() {
 
   // Keep track of index outside of the event handler.
   let index = 0;
 
-  // This is the function that would be invoked by the event listener.
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
 
-    if (key === alphabet[index]) {
+    if (code[index] === key) {
       index++;
 
-      if (index === alphabet.length) {
-        alert("Hurray!");
+      if (index === code.length) {
+        alert('Hurray!');
 
         index = 0;
       }
     } else {
       index = 0;
     }
-  }
-
-const input = document.querySelector('body')
-
-input.addEventListener('keydown', onKeyDownHandler);
+  });
+}
