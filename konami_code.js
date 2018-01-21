09e9a8715b9a25fa424e936bbc5ad58280a70fc2
Zachary Ciatteo <zciatteo96@gmail.com>
2018-01-21 18:49:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let i = 0;
+  
+  
+  document.body.addEventListener('keydown', function(event){
+  
+    var key = parseInt(event.detail || event.which);
+    
+    if(key === code[i]){
+      i++;
+    
+    if(i === code.length){
+      alert("You got the right code");
+      i = 0;
+      } 
+    } else {
+      i = 0;
+    }
+  });
 
 }
