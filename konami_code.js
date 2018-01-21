12e2ccd01ef823767dbe9af1f091bbab02d52a48
Monica Tamaru Jones <mntamaru@gmail.com>
2018-01-21 22:27:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
   // Write your JavaScript code inside the init() function
-
+ 
+  var index = 0
+  
+  document.body.addEventListener('keydown', function(e) {
+    
+    var check = parseInt(e.detail || e.which)
+    if (check == code[index]){
+      index ++ 
+      if (index == code.length) {
+        alert("Konami Code Master!")
+          
+        index = 0
+      }
+    }
+      
+    else { index = 0 }
+    }
+  )
 }
