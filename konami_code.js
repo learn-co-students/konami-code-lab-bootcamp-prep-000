d81a530f50f7a0e3330ded83d62ced04776bd119
Zach Vary <zach.vary@gmail.com>
2018-01-25 17:33:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,19 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
 
-function init() {
-  // Write your JavaScript code inside the init() function
-  function konamiCodeEntered(e) {
-    const key = parseInt(e.detail || e.which);
+body = document.querySelector('body')
+
+function konamiCodeEntered(e) {
+        const key = parseInt(e.detail || e.which);
       if (key === code[index]) {
-        index++;
-        if (index === code.length) {
+          index++;
+      if (index === code.length) {
           alert("Nice Job!");
         }
       } else {
         index = 0;
-      };
-    }
-    const body = document.querySelector('body');
-    document.body.addEventListener('keydown', konamiCodeEntered);
+      }
 }
+
+body.addEventListener('keydown', konamiCodeEntered)
