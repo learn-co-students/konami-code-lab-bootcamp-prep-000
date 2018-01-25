6526df0c5d88b8b455ae7d7a656de2e7610288a8
Harim Choi <harim1206@gmail.com>
2018-01-25 19:04:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,26 +2,29 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // your code here
+  console.log(code)
   let index = 0;
   document.body.addEventListener('keydown',function(e){
+    console.log(`e.location: ${e.location}`);
 
-    if(e.which===code[index]){
+    if(e.location===code[index]){
       index++;
       console.log(`index: ${index}`)
+
+      if(index===code.length){
+        window.alert("Congratulations!");
+        index = 0;
+        console.log(`index: ${index}`)
+      }
     }else{
       i = 0;
     }
-    console.log(e.which);
 
-    if(index===10){
-      alert("Congratulations!");
-      index = 0;
-    }
+
+
+
 
   })
-
-
-
 }
 
 init()
