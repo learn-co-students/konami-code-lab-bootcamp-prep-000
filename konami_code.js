7c84019226f457a068d61eaecae2a1117c40c7a5
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-01-25 07:11:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,24 +2,24 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
-  
   const body = document.getElementsByTagName('body')[0];
  
   body.addEventListener('keydown', function(event) {
     const key = parseInt(event.detail || event.which);
     console.log(key);
   
-  if (key === code[index]) {
-    index++;
- 
-    if (index === code.length) {
-      alert("Congratulations! You've successfully keyed the Konami Code!!");
- 
-      index = 0;
-    }
-  } else {
+    if (key === code[index]) {
+      index++;
+    
+      if (index === code.length) {
+        alert("Congratulations! You've successfully keyed the Konami Code!!");
+      
+        index = 0;
+      }
+      
+    } else {
     index = 0;
-  }
-  
+    }
+    
   });
 }
