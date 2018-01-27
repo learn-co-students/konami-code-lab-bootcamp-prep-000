350556a36a512ba579791dd1061de969d2a021ac
Eterna <one.eternity@gmail.com>
2018-01-27 08:03:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0
-//var key = []
-//document.addEventListener('keydown', function(e) {
-//key.push(e.which)}
 
 function init() {
-
-  document.addEventListener('keydown', function(e) {
+  var body = document.querySelector('body')
+  body.addEventListener('keydown', function(e) {
 
     const key = parseInt(e.detail || e.which);
 
@@ -21,9 +18,11 @@ function init() {
     }
     else {
       index = 0;}
-
   })
-
 }
 
-init()
+init();
+
+//var key = []
+//document.addEventListener('keydown', function(e) {
+//key.push(e.which)}
