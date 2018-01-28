723b96a061bbcd430a834f606a29613a18a43911
Greg Stimson <stimsong@yahoo.com>
2018-01-28 23:40:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,29 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-const body = document.getElementById('mocha');
-
-body.addEventListener('keydown', onKeyDownHandler)
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
   let index = 0;
+//  var e = document.querySelector("div.mocha input [e='Konami']");
 
-  function onKeyDownHandler(e) {    
-    const key = parseInt(e.detail || e.which);
-    
-    if (key === code[index]) {
+  document.body.addEventListener('keydown', function(e) {
+//    function onKeyDownHandler(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
       index++;
-      
+
       if (index === code.length) {
-        alert("Hurray!");
+        alert('Hurray!');
+
         index = 0;
       }
     } else {
       index = 0;
     }
-  }
-//  for ( let i = 0; i < code.length; i++ ) {
-//  code[i].addEventListener('keydown', onKeyDownHandler);
-//  }
-
+//    }
+  });
 }
