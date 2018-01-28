adfdbabae2e718bc7ae00f887d93f4ba444f18ab
Greg Stimson <stimsong@yahoo.com>
2018-01-28 16:32:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,14 +1,13 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const body = document.getElementById('mocha');
+
+body.addEventListener('keydown', onKeyDownHandler)
 
 function init() {
   // Write your JavaScript code inside the init() function
 
   let index = 0;
-  
-  code.addEventListener('keydown', function(onKeyDownHandler) {
-    
-  });
-  
+
   function onKeyDownHandler(e) {    
     const key = parseInt(e.detail || e.which);
     
@@ -23,5 +22,8 @@ function init() {
       index = 0;
     }
   }
+//  for ( let i = 0; i < code.length; i++ ) {
+//  code[i].addEventListener('keydown', onKeyDownHandler);
+//  }
 
 }
