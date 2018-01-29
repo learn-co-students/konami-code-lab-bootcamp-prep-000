0d670266bbcd93a45fc4fa2f8b9e56fd05456925
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-29 19:13:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-let index = 0;
-function init(kon) {
-  const key = parseInt(kon.detail || kon.which);
+function init() {
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+    if (code[index] === key) {
+      index++;
+      if (index === code.length) {
+        alert('Hurray!');
+        index = 0;
+      }
+    } else {
+      index = 0;
   
-  if (key === code[index]) {
-    index++;
-  }
-  if (index === code.length) {
-    alert ('Congrats, you did it!!');
-    index = 0;
-  } else { 
-    index = 0;
-  }
+    }
+  });
 }
-
