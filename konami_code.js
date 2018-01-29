6ea99ba913eeebad893376bcbb08d11736c314f4
Greg Stimson <stimsong@yahoo.com>
2018-01-29 16:37:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
+let index = 0;
+
 function init() {
-  let index = 0;
 //  var e = document.querySelector("div.mocha input [e='Konami']");
 
   document.body.addEventListener('keydown', function(e) {
