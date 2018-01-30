a5ca7ab1998b9a47b47d9edea684e61103e07559
Arthur Carson <carson.bob.arthur@gmail.com>
2018-01-30 00:41:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+let index = 0;
+document.addEventListener('keydown', function(event) {
 
+const key = parseInt(event.detail || event.which);
+if (key === code[index]) {
+  alert('Horray!')
+  index++
+} else {
+index = 0;
+}
+})
 }
