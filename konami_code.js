3516f1b1da9b7d75f708fab5406dce2c87a331c6
Daniel Brazel <dbrazel4@gmail.com>
2018-01-31 22:19:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.which || e.detail);
+    
+    console.log(key)
+    
+    if (key === code[index]) {
+      index++;
+      console.log(index)
+      if (index === code.length) {
+        
+        alert("Congratulations, Konami Code entered!");
+        
+        index = 0;
+      
+      }
+      
+    } else {
+        index = 0;
+      
+    }
+  })
 
 }
