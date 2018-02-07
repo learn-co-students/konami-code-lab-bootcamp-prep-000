6ce64f88a3faadbd203b5804938c110b952b7cfb
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-07 00:13:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
 function init() {
-  
+  const key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("God Mode Enabled");
+
+      index = 0
+    }
+  } else {
+    index = 0;
+  }
 }
