645fa9b8c0ea3d89da94b120ceffe16a3355e402
arye dov eidelman <aryedov.e@gmail.com>
2018-02-07 00:38:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+var index = 0;
+  var body = document.querySelector('body');
+  body.addEventListener('keydown', function onKeyDownHandler(event) {
+    var e = event;
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        console.log(e);
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    console.log(`key:${key}`);
+    console.log(`e:${e}`);
+    console.log(`index:${index}`);
+  })
 }
+init()
