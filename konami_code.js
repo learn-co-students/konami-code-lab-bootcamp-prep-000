3ed6d324c3e9833d719cac226f783a289827260d
Kevin Delvo <kevinbdelvo@gmail.com>
2018-02-07 00:17:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,11 @@
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+})
+
+
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
