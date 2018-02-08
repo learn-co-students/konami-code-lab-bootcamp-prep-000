37ce34011f6a0a5d01f3538acbdf6c0160bf65c7
Santosh SN <nasturtus@gmail.com>
2018-02-08 11:42:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.addEventListener('keydown', function(e) {
+    var trueCount = 0;
+    var position = 0;
+    var keydownCount = 0;
+    if (e.detail === code[position]) {
+      keydownCount++
+      trueCount++
+      position++
+      if (keydownCount === trueCount) {
+        alert ('Congratulations' );
+      }
+    } else {
+      trueCount = 0;
+      keydownCount = 0;
+      position = 0;
+    }
+  })
 }
