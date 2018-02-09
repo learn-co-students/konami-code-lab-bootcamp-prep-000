e5806a1668c58e7cf52bdc0fc0e3c1983c53bac3
Brandon Pretelt <brandonpretelt93@gmail.com>
2018-02-09 07:02:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+  
+  if (key === code[index]) {
+    index += 1;
+    
+    if (index === code.length) {
+      alert('Hurray!');
+    }
+    
+  }
+}
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
