aa418a0cb278003823daff9bd5a23a74f88631d7
Kevin Li <kevinjcli@hotmail.com>
2018-02-11 04:06:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const konamiDetector = document.body;
+  let konamiProgressIndex = 0;
+  
+  konamiDetector.addEventListener ("keydown", function(e) {
+    const keyDetector = parseInt(e.which || e.key || e.detail || e.location);
+    if (keyDetector === code[konamiProgressIndex]) {
+      konamiProgressIndex++;
+      if (konamiProgressIndex === code.length) {
+        alert("Congratulations!");
+      }
+    } else {
+      konamiProgressIndex = 0;
+    }
+  });
 }
