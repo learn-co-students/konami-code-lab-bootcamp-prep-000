62a81fd0e2b62aae4af909f4a5da6d117739b531
carlitos321 <ca.cabrera321@gmail.com>
2018-02-13 21:58:50 +0000

    work in progress

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.querySelector('body')
+
 function init() {
-  // Write your JavaScript code inside the init() function
+  body.addEventListener('keydown', function(e) {
+    // if user types the code in order, then send x alert
+    const key = parseInt(e.detail || e.which);
+
+    for (let i = 0, l = code.length; i < l; i++) {
+      console.log(code[i])
+    }
+  } )
 
 }
