1ff7b1651581e11bc6b422335e303f9aad8d1aac
carlitos321 <ca.cabrera321@gmail.com>
2018-02-13 22:00:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-const body = document.querySelector('body')
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  body.addEventListener('keydown', function(e) {
-    // if user types the code in order, then send x alert
-    const key = parseInt(e.detail || e.which);
+  let index = 0;
 
-    for (let i = 0, l = code.length; i < l; i++) {
-      console.log(code[i])
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
     }
-  } )
-
+  });
 }
