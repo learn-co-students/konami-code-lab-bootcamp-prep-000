5cbc8d9923a81671ecde7f551cead108993f26b4
Alberto Carreras <acarrerasc@gmail.com>
2018-02-14 19:51:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,33 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+	 
+	document.body.addEventListener('keydown', onKeyDownHandler) 	
+ 
+}
+
+var index = 0;
+
+function onKeyDownHandler(e) {
+	
+	var key = parseInt(e.detail || e.which);
+
+	console.log(key);
+	console.log(index);
+
+	if(key === code[index]) {
+
+		index++;
+
+		if(index === code.length) {
+			
+			alert("Konami Code!");
+			index = 0;
+		
+		} } else {
+
+			index = 0;
+		
+		}
 
 }
