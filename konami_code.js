ee0ec27f19997a2ea38a21f231333c0a0c9d89cb
Bryan Bell <bbell1333@gmail.com>
2018-02-15 15:46:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,34 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]; //up up down down left right left right b a
+
+
+
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const body = document.querySelector('body');
+  let index = 0;
+  body.addEventListener('keydown', function onKeyDownHandler(e){
+    
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code[index]){
+      index++;
+      if (index === code.length){
+        alert("Hurray!");
+        
+        index = 0;
+      }
+    }else{
+      index = 0;
+    }
+  });
 }
+
+
+
+
+
+
+
+
+
