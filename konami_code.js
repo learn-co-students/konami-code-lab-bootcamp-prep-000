55f177613001c86d601bb0a74274cf44b5400a43
Troy Lawrence <redirect@gmail.com>
2018-02-15 23:23:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let body = document.querySelector('body');
 
+function init() {
+  body.addEventListener('keydown', function())
 }
