2b9d1e28285026f8c7515382c6c77864936fb1e0
Troy Lawrence <redirect@gmail.com>
2018-02-16 01:32:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let body = document.querySelector('body');
+let index = 0;
 
 function init() {
-  body.addEventListener('keydown', function())
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.which || e.detail);
+    
+    if(key === code[index]){
+     index++;
+    
+      if (index === code.length) {
+         alert('Congratulations!!!');
+         
+         index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    
+  })
 }
+
+
