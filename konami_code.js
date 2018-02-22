ef1518385c37ae5f29546c98e5d6d177acff28eb
butch cosio <butchcosio@yahoo.com>
2018-02-22 02:54:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const alphabet = [65, 66, 67];
+  let index= 0;
 
+  function onKeyDownHandler(e) {
+      const key = parseInt(e.which || e.detail);
+   //   console.log(key)
+      if (key === code[index]) {
+        index++;
+ 
+        if ( index=== code.length) {
+          alert("Hurray!");
+ 
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+  }
+  document.addEventListener('keydown', onKeyDownHandler)
 }
+
+init()
+
+
