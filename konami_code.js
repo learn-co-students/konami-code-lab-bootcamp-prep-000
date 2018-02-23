72f0704efd45d344b9a90051d26544ab14e4d604
Hanuman Chu <chuhanuman@gmail.com>
2018-02-23 14:47:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // Write your JavaScript code inside the init() function
-
+var index = 0;
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === code[index]) {
+    index++;
+ 
+    if (index === code.length) {
+      alert("Yay!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+function init() {
+  var body = document.querySelector('body');
+  body.addEventListener('keydown', function(event) {onKeyDownHandler(event)});
+}
+init();
