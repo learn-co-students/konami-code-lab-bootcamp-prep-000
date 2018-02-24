235ac4b55956958a52b05899e6f56cb7bad58b1a
Vanessa Toro <vanetoro84@gmail.com>
2018-02-24 21:22:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
 function init() {
-var body =  document.getElementsByName('body').addEventListener('keydown');
-alert(body) ;
+  var index = 0;
+
+  document.body.addEventListener('keydown', function(event){
+    var key = parseInt(event.which || event.detail);
+
+  if(code[index] === key){
+    index++;
+  } else{
+    index = 0;
+  }
+  
+  if(index === code.length){
+    alert('Hurray')}
+  });
 }
+
+
+init();
