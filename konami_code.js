d53195e1523511537bc639174becff7f5aea251a
Ricardo Ebbers Carneiro Leão <recl@cin.ufpe.br>
2018-02-24 22:53:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // Write your JavaScript code inside the init() function
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let index = 0;
+function init(e) {
+  const input = document.querySelector('body');
+  input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+  const key = parseInt(e.detail || e.location || e.which);
+  if (key === code[index] ) {
+    index++;
+    if (index === code.length) {
+      alert('Congrats, you pressed the right keys');
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+  });
 }
