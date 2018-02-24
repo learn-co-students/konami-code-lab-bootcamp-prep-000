6cb2077cc16ec7331ba207b3db0d0e01b0e28d14
Pitta <mailez4all@gmail.com>
2018-02-24 10:32:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,34 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function init(e) {
+
+  const input = document.querySelector('body')
+
+  input.addEventListener('keydown', function(e) {
+  console.log(e.which)
+  const key = parseInt(e.detail || e.location || e.which);
+
+  if (key === code[index] ) {
+
+    index++;
+
+    if (index === code.length) {
+
+    alert('Congrats, you pressed the right keys');
+
+    index = 0;
+
+    }
+
+  } else {
+
+    index = 0;
+
+  }
+
+  })
 }
+
+  // Write your JavaScript code inside the init() function
