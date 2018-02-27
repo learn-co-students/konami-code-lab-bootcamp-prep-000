5aef29e67175c3e2f6b3e470af865f607a414728
Kelsey Fecho <kfecho30@gmail.com>
2018-02-27 03:37:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-let index = 0;
 
-function init(e) {
+function init() {
   // Write your JavaScript code inside the init() function
- const key = parseInt(e.detail || e.which)
+  let index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler);
 
-if (key === code[index]){
-  index ++;
-  if(index === code.length){
-    alert('WOO!');
-    index = 0;
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[index]){
+      index ++;
+      if(index === code.length){
+        alert('WOO!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   }
-} else {
-  index = 0;
 }
-}
-
-document.body.addEventListener('keydown', init);
