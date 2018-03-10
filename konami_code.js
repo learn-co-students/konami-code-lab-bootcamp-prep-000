a6e017d0780bdd09813e50e92eb31b493d63a3c1
maxsmouha@gmail.com <maxsmouha@gmail.com>
2018-03-10 21:15:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]  //store konami code as array in constant
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  var index = 0;
+
+  document.body.addEventListener('keydown', function(e) { //add event listener for keydown, callback
+
+  const key = parseInt(e.detail || e.which || e.location);
+
+  if (key === code[index]) {  //if required key is pressed
+    index++;                //move to the next key
+
+        if (index === code.length) {   //if length of code is input
+          alert("Hurray!");            //alert hurray
+          index = 0;                    //set index back to 0
+          }
+
+  } else {
+    index = 0;                      //if not entered, reset index to 0
+  }
+});
 }
+
+init();
