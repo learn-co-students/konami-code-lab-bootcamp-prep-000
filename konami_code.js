0b9d99c115126b145b6eb5e2dc0053be3495105a
Allan Glasier <allan.glasier@outlook.com>
2018-03-13 18:56:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const body = document.querySelector("body")
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  body.addEventListener("keydown", function(e) {
+    const key = parseInt(e.detail || e.which || e.location)
 
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert ("Yay, Konami Code!")
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
+
+init()
