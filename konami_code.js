ce90368531908a23b2cd7dbd242e9fdc8fb8c67b
abra2abra <abra2abra@mailinator.com>
2018-03-13 10:14:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
+let index = 0;
+function init(e) {
   // Write your JavaScript code inside the init() function
-
 }
+
+var listener = document.body;
+listener.addEventListener('click', function init(e) {
+  // Write your JavaScript code inside the init() function
+  
+  
+  if (key.which == code[index]) {
+    index++; }
+ 
+    if (index == code.length) {
+      alert("something");
+ 
+      index = 0;
+  } else {
+    index = 0;
+  }
+
+});
