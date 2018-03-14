abb9c467d7601e05b3346e4d12a8054b45cf8cb1
Jarom Roney <byutrueblue@gmail.com>
2018-03-14 23:08:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,18 +2,26 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0
 
-document.body.addEventListener('keydown',init)
+function init() {
+ document.body.addEventListener('keydown',listen)
+}
 
-function init(e) {
- const key = parseInt(e.keyCode)
+function listen(e){
+  const key = parseInt(e.location||e.which)
+  
   if (key === code[index]){
     index++
+    
+    if (index === code.length){
+    alert('You have unlocked the secrets of the web. Use your power wisely')
+    index = 0
+    }
   }
   else {
     index = 0
   }
-  if (index === code.length){
-    alert('You have unlocked the secrets of the web. Use your power wisely')
-    index = 0
- }
+  
+
 }
+
+
