7b37ea45cad32202e42bb3682db93ccf76c05121
William Chabla <w_chabla@hotmail.com>
2018-03-15 19:34:57 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Konami Code Lab
+! # Konami Code Lab
 
 ## Objectives
 1. Practice using event listeners.
--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+//let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  
+  let index = 0;
+  
+  let body = document.querySelector('body');
+  body.addEventListener('keyDown', checkCode);
+  
+  function checkCode(e){
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code[i]) {
+      index++;
+      
+      if (key === code.length - 1) {
+        alert("code compelte");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
+
