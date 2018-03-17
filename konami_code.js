51e99ba245bb3b72e00d5a90c3bbd6e9d58c22ad
jconway4 <jconwa4@illinois.edu>
2018-03-17 03:23:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,26 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-  
-
-  
+ 
 function init() {
-  let main = document.querySelector('body');
   let i = 0;
-  let codeChecker = [];
-  
-  main.addEventListener('keydown', function(e) {
+
+  function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
-    if (key === code[i]) {
+      if (key === code[i]) {
       i++;
-      codeChecker.push(key);
-      
-      if (codeChecker === code) {
-        alert("Hurray!");
-        i = 0;
-        codeChecker = [];
+        if (i === code.length) {
+          alert("Congratulations!");
+          i = 0;
+          }
+        } else {
+          i = 0;
+        }
       }
-    } else {
-      i = 0;
-      codeChecker = [];
-    }
-  });
+      
+  for (let i=0;i<code.length;i++){
+    document.body.addEventListener('keydown', onKeyDownHandler);
+  }
 }
