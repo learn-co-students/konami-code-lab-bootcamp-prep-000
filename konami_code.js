f9251ef50539df8f4269074780abee6ddb64e6c3
jconway4 <jconwa4@illinois.edu>
2018-03-17 02:46:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,31 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  
 
+  
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  
+  const main = document.getElementById('mocha')
+  
+  main.addEventListener('keydown', function(e) {
+    
+    function onKeyDownHandler(e) {
+      const key = parseInt(e.detail || e.which);
+      var checkCode = [];
+      i = 0;
+      
+      while (checkCode != code) {
+        if (key === code[i]) {
+          checkCode.push(key);
+        } else {
+          i = 0;
+          checkCode = [];
+        }
+      }
+        
+      alert("Hurray!");
+      i = 0;
+      checkCode = 0;
+    
+    }
+  });  
 }
