92be0b67a761b63a20e726b4abcaa7d5e0c31d3e
Hollie Lambert <hollielulambert@gmail.com>
2018-03-27 21:06:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  document.addEventListener('keydown', function(e) {
+    onKeyDownHandler(e);
+  })
+
+}
+
+var index = 0;
+
+function onKeyDownHandler(e) {
+  var key = parseInt(e.detail || e.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert('KONAMI CODE!!!')
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
