da60d699be68e91f7eebb1ea6cbd2937619fe98b
John Rodriguez <jcr2125@gmail.com>
2018-03-28 02:24:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  
+  let index = 0;
+  
+  document.addEventListener("keydown", onKeyDownHandler, false);
 
+  function onKeyDownHandler(e) {
+    
+    const key = parseInt(e.detail || e.which);
+   
+    if (key === code[index]) {
+      index++;
+   
+      if (index === code.length - 1) {
+        
+        alert("Konami Code FTW!!!!!!");
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
