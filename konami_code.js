0d12d45f4ad9d54fe1f74d9b260b2367d405d07f
Monica Seaman <monica.l.seaman@gmail.com>
2018-03-30 20:16:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', codeListener);
+}
 
+function codeListener(e) {
+  console.log(e.which);
+  var key = parseInt(e.detail || e.which);
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      alert("You know the super-secret password!!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
