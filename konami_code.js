4adb0aa3c1733475ddfa90d85fe523b6945f8141
John Gratton <jgratton@gmail.com>
2018-03-30 01:47:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
 
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }, false);
 }
