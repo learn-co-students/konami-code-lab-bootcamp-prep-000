6d74f8a21594a4b666aea722afd75b953ea23a4a
Adam Mrachek <amrachek@outlook.com>
2018-03-31 19:26:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-
-function init() {
+const codeListener = document.querySelector('body');
+codeListener.addEventListener('keydown', function init(e) {
   // Write your JavaScript code inside the init() function
-  const codeListener = document.querySelector('body');
   var codeKeeper = [];
-  codeListener.addEventListener('keydown', function(e) {
-    codeKeeper.push(e.which);
-    if (codeKeeper.length > 10) {
-      codeKeeper.shift();
-    }
-    console.log(codeKeeper);
-    if (JSON.stringify(codeKeeper) == JSON.stringify(code)) {
-      alert("Congratulations! You've won $1,000,000!")
+  codeKeeper.push(e.which);
+  if (codeKeeper.length > 10) {
+    codeKeeper.shift();
   }
-  })
+  console.log(codeKeeper);
+  if (JSON.stringify(codeKeeper) == JSON.stringify(code)) {
+    alert("Congratulations! You've won $1,000,000!")
+    return;
+  }
 }
 init();
