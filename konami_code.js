b2dc5540fa63025597ec430128ba3f1e8ddb3607
Luke Minnich <squareman2357@gmail.com>
2018-04-01 16:02:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let body = document.body
+  body.addEventListener('keydown',onKeyDownHandler)
+  
+  let i = 0;
+  
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    
+    if (key === code[i]){
+      i++;
+      
+      if (i === code.length){
+        alert("Congratulations!")
+      }
+    } else {
+      i = 0;
+    }
+  }
 }
