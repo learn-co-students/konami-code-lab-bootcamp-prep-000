6960f033ae34eb440d7d8e3a2acb8dd6c8b93134
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-04-02 01:21:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
+
+   document.body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length - 1) {
+
+        index = 0;
+        alert("Congrats!");
+      }
+
+  } else {
+    index = 0;
+  }
+
+
+  });
 
 }
