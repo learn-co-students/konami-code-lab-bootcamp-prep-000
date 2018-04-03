e7caff84f917b5fe1cf784c338d736e1898c8cf6
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-04-03 19:25:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+ var index = 0
+  document.body.addEventListener('keydown', function(events){
+    var key = parseInt(events.detail || events.which);
+   if (key === code[index]){
+     index++;
+    
+     if(index === code.length){
+       
+       alert("Hurray!");
+       index = 0;
+     }
+    
+    } else {
+     index = 0;
+   }
+ });
 }
