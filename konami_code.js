f5a843a18df134195ae3d51517ca76c7446d5473
Christopher <christopherccm@msn.com>
2018-04-03 17:31:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -3,18 +3,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  const body = document.body.querySelector('body')
-  const key = parseInt(keyPress.detail || keyPress.which);
+  document.body.addEventListener('keydown', event => {
+    const keyPress = parseInt(event.detail || event.which);
 
-  if (key === code[index]) {
+  if (keyPress === code[index]) {
     index++;
+    console.log(index);
 
     if (index === code.length) {
       alert("You've cracked the code!");
-
       index = 0;
     }
   } else {
     index = 0;
+    console.log(index);
   }
+  })
 }
