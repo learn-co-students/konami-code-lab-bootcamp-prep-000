267643d3a9fc318f02734fa847afbd2ffdbfd875
Christopher <christopherccm@msn.com>
2018-04-03 16:22:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function init() {
+  const body = document.body.querySelector('body')
+  const key = parseInt(keyPress.detail || keyPress.which);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("You've cracked the code!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
