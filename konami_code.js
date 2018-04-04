a3eab8d1f1d0321862a0844c49882fef30a22f34
tommejas@gmail.com <tommejas@gmail.com>
2018-04-04 22:06:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,17 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  var body = document.querySelector("body");
+  var current = 0;
+  body.addEventListener("keydown", function(event) {
+    if (parseInt(event.detail) === code[current]) {
+      current = current + 1
+      if (current === code.length) {
+        alert("finished");
+      }
+    } else {
+      current = 0;
+    }
+  })
 }
+init();
