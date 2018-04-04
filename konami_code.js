9cd350cb8cb846969990a51509c18d06b27a3d58
Ronald DeBeras <rdeberas@gmail.com>
2018-04-04 12:43:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,13 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const alphabet = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0;
 function init() {
 
    document.body.addEventListener('keydown', function(e){
     var key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
+    if (key === alphabet[index]) {
       index++;
-      if (index === code.length - 1) {
+      if (index === alphabet.length - 1) {
 
         index = 0;
         alert("Dreamteam!");
