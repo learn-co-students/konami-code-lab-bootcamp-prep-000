2d2c3bd25f8e481cf977eb4690dd2267efeda0e8
Michael Chapman <ecssiah@gmail.com>
2018-04-06 08:31:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
+let index = 0
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function onKeyDownHandler(e) {
+  const key = parseInt(e.location)
+  
+  if (key === code[index]) {
+    index++
+    
+    if (index === code.length) {
+      alert("Konami Code Access!")
+      
+      index = 0
+    } 
+  } else {
+    index = 0
+  }
+}
 
+function init() {
+  document.body.addEventListener('keydown', onKeyDownHandler, true)
 }
