0bca0d221e3fffa9c4c1333d91597feea0652570
Jackie First <jackiejeep@gmail.com>
2018-04-06 18:16:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+        index++;
+    } 
+    if (index === code.length-1) {
+      window.alert("Hurray!");
+      index = 0;
+    }
+    else {
+      index = 0; 
+      }
+    });
+  } 
 
-}
+
+
