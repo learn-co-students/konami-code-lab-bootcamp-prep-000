c0b678a6111a49707715c5515dc56fa3fe5f60bf
Anthony <zeitlin1@gmail.com>
2018-04-08 17:34:24 -0400

    refactor code

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,24 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-            // up up down down left right left right b a
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+// up up down down left right left right b a
 
 var keycode = [];
-const bod = document.body
 
-bod.addEventListener('keydown', function(e) {
-  console.log(e.which);
-  init(e.which)
-})
+const main = document.getElementById('main');
 
-function init(entry) {
-  keycode.push(entry)
-  if (keycode.length > code.length) {
-    keycode.shift()
-  } if (keycode.toString() === code.toString()) {
-      window.alert('You Did it!')
+main.addEventListener('keydown', function(e) {
+  init(e.which);
+  // console.log(e.detail);
+  // console.log(e.which);
+});
+
+function init(argument) {
+  keycode.push(argument);
+
+  console.log(keycode);
+
+  if (keycode.toString() === code.toString()) {
+    alert('Konami Code Has been Activated!');
+  } else if (keycode.length > code.length) {
+    keycode.shift();
   }
 }
