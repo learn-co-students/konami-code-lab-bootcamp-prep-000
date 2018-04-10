90a15185723d9bf93219bd227e863b7f74d11f2f
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 12:49:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  //Keep track of index outside of the event handler.
+  let index = 0;
+  
+  const body=document.body;
+  
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    
+    if (key === code [index]) {
+      index++
+      
+      if (index === code.length) {
+        window.alert("You cracked the code!");
+        
+        index = 0;
+      }
+    } else {
+      index - 0;
+    }
+  })
+  
+  
+  
+  
 
 }
