cb29f4b6be345df478108a1f83d8c4518c7e7208
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-12 00:02:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var input = document.body
 
-function init() {
-  // Write your JavaScript code inside the init() function
-
+function init(){
+   input.addEventListener('keydown', function onKeyDownHandler(input) {
+  const key = parseInt(input.detail || input.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+)}
