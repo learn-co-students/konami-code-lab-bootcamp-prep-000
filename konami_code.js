2264c9d158c88d6df1d2a1a2831a773c0068a946
Michael Hansen <michaelandhansen@gmail.com>
2018-04-14 18:09:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  const main = document.getElementsByTagName("body")[0];
+  
+  main.addEventListener("keydown", keyHandler); 
+  
+  function keyHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key == code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('Praise Kojima!');
+        index = 0;
+      } 
+    } else {
+      index = 0;
+    }
+  }
 }
