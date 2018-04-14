c526e353afc5f97fff2c5485dc79c485f2527041
Kwaku J <gyazi@live.com>
2018-04-14 22:54:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0
+
 function init() {
   // Write your JavaScript code inside the init() function
-
+  var bod = document.querySelector('body')
+  bod.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which)
+    
+    if (key === code[index]){
+      index++;
+      
+      if (index === code.length){
+        alert("Hurray")
+        index = 0
+      }
+      else{
+        index = 0
+      }
+    }
+ })
 }
