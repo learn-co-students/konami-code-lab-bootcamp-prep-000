655e5d82065a4b46cba2bdd1f1115626c152b37e
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-15 15:47:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
 
 function init() {
-function onKeyDownHandler(e) {
-  let index = 0;
-  const key = parseInt(e.detail || e.which)
- if (key === alphabet[index]) {
+ 
+document.body.addEventListener("keydown", function(e) {
+      const key = parseInt(e.detail || e.which) 
+ 
+  if (key === code[index]) {
     index++;
-    if (index === alphabet.length) {
-      console.log(alert("Hurray!"));
-            index = 0;
+     
+    if (index === code.length) {
+    alert("Hurray!")
     }
   } else {
     index = 0;
+    }
   }
-}
-}
+)}
