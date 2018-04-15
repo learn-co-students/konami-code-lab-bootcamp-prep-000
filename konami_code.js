975d14be469e441a17496ba16d1cce3f31b14945
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-15 03:00:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+function init() {
+function onKeyDownHandler(e) {
+  let index = 0;
+  const key = parseInt(e.detail || e.which)
+ if (key === alphabet[index]) {
+    index++;
+    if (index === alphabet.length) {
+      console.log(alert("Hurray!"));
+            index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 }
