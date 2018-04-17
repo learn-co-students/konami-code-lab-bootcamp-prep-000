7356418e7e1bed95499ae0cea091aaf8d77842d0
Anthony C <acherone92@gmail.com>
2018-04-17 13:40:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown',onKeyDownHandler);
+let index = 0;
 
+function onKeyDownHandler(e){
+  console.log(`Index ${index}\nDetail: ${e.detail}\nWhich: ${e.which}\nLocation: ${e.location}`);
+  const key = parseInt(e.detail);
+    if (key==code[index]){
+      index++;
+      if (index ==code.length){
+        alert("You found the easter egg!");
+      }
+  }
+}
 }
