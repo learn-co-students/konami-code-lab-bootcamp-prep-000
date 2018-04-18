6d968f8e2bbef0f9c3e7ed14933a22045d618d6f
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-04-18 21:37:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+let index = 0
+
+document.body.addEventListener('keydown', function (f){
+  const key = parseInt(f.detail || f.which)
+ 
+  if (key === code[index]) {
+      index++;
+        if (index === code.length) {
+           alert();
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
+
 
 }
