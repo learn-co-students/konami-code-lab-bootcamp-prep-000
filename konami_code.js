24016c0f38ea8389d375ec9fc59274eca82a132f
Jigar Polra <jigarpolra@live.co.uk>
2018-04-23 13:00:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
+
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  var index = 0;
+  const div =  document.querySelector('body');
 
+  div.addEventListener('keydown', function(e) {
+  var key = parseInt(e.key || e.detail);
+
+  if(code[index] == key)
+  {
+    index++;
+    console.log("A key pressed.");
+  } else {
+      index = 0;
+  }
+  if(index == code.length)
+  {
+    console.log("All keys pressed.");
+    window.alert("Hurray!");
+  }
+  });
+  // Write your JavaScript code inside the init() function
 }
