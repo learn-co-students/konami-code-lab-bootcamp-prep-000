fe4fe884e60e769eec64d1c99b2dfb476fc4b663
Nick Bradley <nick.bradley.193@pm.me>
2018-04-25 20:14:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+
 function init() {
   // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  
+  document.body.addEventListener('keydown', function(event) {
+    const key = parseInt(event.detail || event.which);
+    if (key === code[index]) {
+      index ++;
+      
+      if (index === code.length) {
+        alert("Wow... you played some Konami games. Good for you?");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
+  
 }
+
