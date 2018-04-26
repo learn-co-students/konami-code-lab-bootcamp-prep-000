c8c93bfafef74686d655839ba0a67e4636e64971
wtscott@gmail.com <wtscott@gmail.com>
2018-04-26 20:28:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var index = 0;
 
+function Konami(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log("detail" + e.detail);
+  if(key === code[index]){
+    index++;
+    if(index === code.length) {
+      alert('Konami Code!!!@');
+      index = 0;
+    }
+  }
+  else
+  { index = 0; }
 }
+ 
+function init() {
+  const bod = document.getElementsByTagName('body')[0];
+  bod.addEventListener('keydown', Konami) ;
+}
+
+init();
+
+
