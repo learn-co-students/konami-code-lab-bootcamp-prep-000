58aa2758fa54d16aaf151fd8b3b74f6c8cb1a6ed
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-26 14:14:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', function(e){
+    const ky = parseInt(e.detail || e.which);
+      for(let i=0; i< code.length; i++){
+        if(ky === code[i]){
+          alert("Congrats boy");
+        }
+        else{
+          console.log("Oops");
+        }
+      }
+  });
 }
