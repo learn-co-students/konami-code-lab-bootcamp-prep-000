c69d555a4a02b53a8a85f7b1837362576e1f74ef
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-29 20:11:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
-
+function init(e) {
+  
+ let i = 0;
+  document.body.addEventListener('keydown', function(e){
+  const key = parseInt(e.detail || e.which);
+  console.log(key);
+  if (key === code[i]){
+    i++;
+    if(i === code.length){
+      window.alert("Hurray!");
+      i = 0;
+    }
+  }else{
+    i = 0;
+  }
+});
 }
