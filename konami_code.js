394f017c60ba5faeac47123e0361d4d34d3b5527
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-30 23:53:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  var index = 0
+  var i = 0
   document.body.addEventListener('keydown',function(e) {
     const key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
-      index++;
-      if (index === code.length) {
+    if (key === code[i]) {
+      i++;
+      if (i === code.length) {
         alert('CONGRATS!');
         index = 0;
       }
