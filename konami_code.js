f44281a576019f5d6f66586635a84621ed506a54
Joseph Milliken <joseph97milliken@gmail.com>
2018-05-02 19:16:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = Number(e.detail || e.which);
+    if (key !== code[index]) return index = 0;
+    
+    index++;
+    if (index >= code.length) alert('Hurray!');
+  });
 }
--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ describe('index', () => {
   const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
   function triggerKeyDown(which) {
-    const keyboardEvent = document.createEvent("KeyboardEvent")
+    const keyboardEvent = document.createEvent("keyboardEvent")
 
     keyboardEvent.initKeyboardEvent(
       'keydown',
