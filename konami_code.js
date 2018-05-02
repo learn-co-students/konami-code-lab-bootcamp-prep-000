45853cfaeb3216f4a02a9da29518e1fdc42ed9c6
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-02 16:37:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
-
+document.body.addEventListener("keydown",onKeyDownHandler)
+function onKeyDownHandler(e){
+  const input=parseInt(e.detail||e.which);
+  if (input===code[index]){
+    index++;
+    if (index===code.length){
+      alert("Thank You for Purchasing This Game!!!");
+      index=0;
+    }
+  }else{
+    index=0;
+  }
+}
 }
