fd15b4cda7a0813e400ceab37e2cb4117aad633f
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-03 19:32:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,17 @@
-
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let step = 0;
 
 function init() {
-  document.body.addEventListener("keydown", press => {
+  const body = document.getElementsByTagName("BODY")[0];
+
+  body.addEventListener("keydown", press => {
     const button = parseInt(press.which);
 
     if (button == code[step]) {
       step++;
       if (step == 10) {
-        alert("Konami Code entered!");
+        window.alert("Konami Code entered!");
         step = 0;
       }
     } else {
