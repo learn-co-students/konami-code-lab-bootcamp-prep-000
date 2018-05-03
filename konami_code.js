db3e282043df6322a5fdeaebfe04db996d5b59de
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-03 02:26:29 -0400

    saving most up to date version in case the github merge conflict is related to why the test just will not work despite working in multiple browsers on different machines

--- konami_code.js
+++ konami_code.js
@@ -1,34 +1,38 @@
 
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
   myCode = [
-    "ArrowUp",
-    "ArrowUp",
-    "ArrowDown",
-    "ArrowDown",
-    "ArrowLeft",
-    "ArrowRight",
-    "ArrowLeft",
-    "ArrowRight",
-    "b",
-    "a"
+    `ArrowUp`,
+    `ArrowUp`,
+    `ArrowDown`,
+    `ArrowDown`,
+    `ArrowLeft`,
+    `ArrowRight`,
+    `ArrowLeft`,
+    `ArrowRight`,
+    `b`,
+    `a`
   ];
 let step = 0;
 
 function init() {
   document.body.addEventListener(`keydown`, function(press) {
-    let button = press.key,
-        sign = parseInt(press.keyCode || press.which);
+    const button = press.key,
+          sign = parseInt(press.which || press.keyCode);
 
-    if (button === myCode[step] || sign === code[step]) {
+    if (sign === code[step] || button === myCode[step]) {
       step++;
-      if (button === "a") {
-        alert("Konami Code entered!");
+
+      if (step > 9) {
+        window.alert(`Konami Code entered!`);
+        // alert(`Konami Code entered!`);
         step = 0;
       }
+
     } else {
       step = 0;
     }
+
   });
 }
 
-init();
+// init();
