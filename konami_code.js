8ca22ba2730239509f919863cb288864033ed340
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-03 02:57:32 -0400

    ran through prettier

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let step = 0;
 
 function init() {
-  document.body.addEventListener('keydown', (press) => {
+  document.body.addEventListener("keydown", press => {
     const button = parseInt(press.which);
 
     if (button == code[step]) {
