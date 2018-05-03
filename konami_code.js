91ce5262eb37cd94319c39f9edd559769522965c
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-03 01:41:02 -0400

    finished just needs tests ran from learn app

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,33 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+  myCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
+let step = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener(`keydown`, function(press) {
+    let button = press.key;
 
+    if (button === myCode[step]) {
+      step++;
+      if (button === "a") {
+        alert("Konami Code entered!");
+        step = 0;
+      }
+    } else {
+      step = 0;
+    }
+  });
 }
+
+init();
