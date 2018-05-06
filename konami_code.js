0fcd9826c1f38a10904d7c651a5400fbef3b1977
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-05-06 17:27:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+const konamiKeys = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'KeyB', 'KeyA'];
+
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  let index = 0;
+  
+ document.body.addEventListener('keydown', function onKeyDownHandler(event){
+   const pressedKey = `${event.code}` ;
+   const keyPressed = parseInt(event.which || event.detail);
+   
+   
+   if (pressedKey === konamiKeys[index] || keyPressed === konamiCode[index]) {
+     index++;
+     if (index === konamiKeys.length || index === konamiCode.length) {
+       alert("Congratulations, you did it!");
+       index = 0;
+     }
+   } else {
+     index = 0;
+   }
+ });
+ 
+ 
 }
