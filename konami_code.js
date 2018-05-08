0fa52c96cbe3100c5a674aee9bb8e3759c63e3d4
Asaf Davidov <adavidov@conncoll.edu>
2018-05-08 23:12:04 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,37 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0;
 
 function init() {
-  // your code here
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', onKeyDownHandler);
 }
+
+function onKeyDownHandler(e){
+    
+    if (e === undefined){
+    }
+    else{
+      const key = e.key;
+      if (key === codes[index]){
+        index++;
+        if (index === codes.length){
+          alert("Hurray!");
+          console.log("Hurray");
+          index = 0;
+        }
+      }
+      else{
+        index = 0;
+      }
+    }
+  }
+
+
+
+
+
+
+
+
