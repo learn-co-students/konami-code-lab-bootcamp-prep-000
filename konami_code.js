07ba943954a174cc39c1b2bf96230f8ace8b9fa7
Eric Frommelt <ericfrommelt@gmail.com>
2018-05-08 12:50:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
 
+  document.body.addEventListener('keydown', checkKeyDown)
+
+  function checkKeyDown(e) {
+    const key = e.key;
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert('Good job!');
+      }
+        index = 0;
+    } else {
+      index = 0;
+    }
+
+    console.log('key pressed');
+    console.log(index);
+  }
 }
