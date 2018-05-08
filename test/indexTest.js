57f9629eceecf3acae945fae8124d9f5dc16edc0
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-05-07 20:38:19 -0400

    solution and tests fixed

--- test/indexTest.js
+++ test/indexTest.js
@@ -22,23 +22,28 @@ describe("index.js", () => {
     document.body.dispatchEvent(keyboardEvent);
   }
 
+  init()
+
   describe("Konami code", () => {
+
+    const spy = sinon.stub(window, "alert");
+
     it("triggers an alert if the right code is entered", () => {
-      init();
 
       for (let i = 0, l = codes.length; i < l; i++) {
         triggerKeyDown(codes[i]);
       }
-
-      expect(spy.calledWith("Hooray!")).to.be.ok;
+      expect(spy.called).to.equal(true)
+      expect(spy.callCount).to.equal(1)
     });
 
     it("does not trigger an alert if the wrong code is entered", () => {
-      init();
+      spy.reset()
 
-      for (let i = 0, l = codes.length; i < l; i++) {
-        triggerKeyDown(i);
+      for (let i = 0; i < codes.length - 1; i++) {
+        triggerKeyDown(codes[i])
       }
+      triggerKeyDown("ArrowUp")
 
       expect(spy.notCalled).to.equal(true);
     });
