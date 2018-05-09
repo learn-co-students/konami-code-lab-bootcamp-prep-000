1733b436422eae0b28d0706b3e449416b55cf688
Morgan Edmonds <edmonds.morgan@gmail.com>
2018-05-09 23:45:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,26 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  // set an initial value for the index to track against
+  let index = 0;
+  
+  document.body.addEventListener('keydown', function(event) {
+    // set the key in the event
+    const key = event.key
+    
+    // if that key is equal to the key at the current index (0 to start), increase the index by 1
+    if (key === codes[index]) {
+      index++
+      
+      // if the user gets the index value up to equal the last in the array (codes), show alert and reset the index
+      if (index === codes.length) {
+        alert('Hurray!')
+        index = 0 }
+    
+    // otherwise, reset the index    
+    } else { index = 0 }
+  })
 }
+    
+    
+   
