ed27108c4da6d9e5e6b4a22c8744d2e11d7caffc
Jess H <jessdev83@gmail.com>
2018-05-11 17:20:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+let index = 0;
+
+ document.querySelector('body').addEventListener('keydown', init);
+
+function init(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+    
+    if (index === codes.length) {
+      alert('Hurray!');
+      
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
