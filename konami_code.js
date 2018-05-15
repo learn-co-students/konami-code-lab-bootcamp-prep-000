bdfa7d1d399289a8ccb1322386243ffb05f1e29b
Luke Worley <luke.worley@colorado.edu>
2018-05-15 00:20:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,24 @@ const codes = [
   "b",
   "a"
 ];
-
+var i=0
 function init() {
   // your code here
+  console.log("hello")
+  document.querySelector("body").addEventListener("keydown", function(e) {return checkcode(e)})
+  console.log()
+}
+
+function checkcode(e){
+  console.log(e.key)
+  if (e.key == codes[i]){
+    i++
+    console.log(i)
+  }
+  else{
+    i=0
+  }
+  if (i==10){
+    alert("konami code engaged.... lets party baby") 
+  }
 }
