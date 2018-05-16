24053a280ebe5bb1056f19767cf2d421d7e74e47
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-16 01:22:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+//const codeWhich = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+   // debugger;
+    const key = e.key;
+    if (key === codes[index]) {
+      console.log(`did ${codes[index]}`);
+      index++;
+      if (index === codes.length) {
+        console.log(`should be congrats`);
+       // debugger;
+        window.alert('Congratulations!');
+        console.log('aftercongrats');
+        index = 0;
+      }
+    } else {
+      index = 0;
+      console.log(`failed ${key}`);
+    }
+  })
 }
