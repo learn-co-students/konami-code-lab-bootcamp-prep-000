3e753f224381f9f6aaf2e753ffe29b2a3475ad3f
Ankur Puri <apuri@iit.edu>
2018-05-17 22:46:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if(key === codes[index]) {
+      index++;
+      
+      if(index === codes.length) {
+        alert("Congratulations! You found an easter egg!");
+        index = 0;
+      }
+    } else {
+        index = 0;
+      }
+  }
+  
+  const body = document.body;
+  body.addEventListener('keydown', onKeyDownHandler);
+  
 }
