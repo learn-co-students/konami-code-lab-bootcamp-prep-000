f2e97fae0f0dd17a9639ead71a239dd50f3003a8
Trevor Russell <truss.42@gmail.com>
2018-05-18 22:35:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,30 +1,30 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
+var index = 0;
+function init() {
 
-let index = 0;
+  const body = document.querySelector('body')
 
-var body = document.querySelector('body')
-body.addEventListener('keydown', init)
-
-function init(e) {
-  var key = e.key;
-  if(key === codes[index]) {
-    index++;
-    if(index === codes.length) {
-      alert('Yipee!');
-      index = 0;
-    } else {
-      index = 0;
-    }
+  body.addEventListener("keydown", code)
+  function code(event){
+      if(event.key === codes[index]) {
+        index++;
+        if(index === codes.length) {
+          alert('Yipee!');
+          index = 0;
+        }
+      } else {
+          index = 0;
+        }
   }
 }
