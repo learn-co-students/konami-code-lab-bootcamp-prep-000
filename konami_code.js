7aee265254c72bbed804273dba74f7a594be0f5b
Steven Crouse <sdcrouse91@gmail.com>
2018-05-19 22:02:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,32 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  
+  const bodyListener = document.body;
+  
+  bodyListener.addEventListener('keydown', konamiCode);
+  
+}
+
+function konamiCode(e) {
+  const typedKey = e.key;
+  
+  console.log(typedKey);
+  
+  if (typedKey === codes[index]){
+    index++;
+    
+    if(index === codes.length) {
+      alert('Congratulations! You entered the Konami Code! Now you have 30 extra lives (just kidding).');
+      
+      index = 0;
+    }
+  }
+  else {
+    index = 0;
+  }
 }
