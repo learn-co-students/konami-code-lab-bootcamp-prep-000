589d955d34341e7dac9b515feab765f667e0a30b
Sav Younts <savyounts@gmail.com>
2018-05-20 19:40:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+//let array = []
 function init() {
-  // your code here
+  index=0
+  document.body.addEventListener('keydown', function(e){
+    var key = e.key
+    if (codes[index] === key){
+      index++;
+    }else{
+      index=0
+    }
+     
+     if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+    //console.log(e.key)
+  /*  for(var i=0; i<e.length; i++)
+        array.push(e.which)
+         if (array === codes){
+          alert ('Yay!');
+          index = 0;
+         }*/
+})
 }
