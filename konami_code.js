1cfcaf66e322b8abecf24d5788556b348d8fcd61
Nathan Hill <nateohill@gmail.com>
2018-05-21 16:22:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,43 @@ const codes = [
   "a"
 ];
 
+// function cheeter(e) {
+//   let key = e;
+    
+//     if(key === codes[index]) {
+//       index++;
+//     }
+    
+//     if(index === codes.length) {
+//       alert("CHEETER!!!");
+//       index = 0;
+//     } else {
+//       index = 0;
+//     }
+//   }
+
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener('keyup', (e) => {
+    let key = e.key;
+    console.log(index);
+    if(key === codes[index]) {
+      console.log(index, codes.length - 1);
+      if(index === codes.length - 1) {
+      cheeter();
+      index = 0;
+      }
+      if(key !== codes[index]) {
+        index = 0;
+      }
+      index++;
+      
+      
+    }
+    
+    });
+    function cheeter() {
+      alert("CHEETER!!!");
+      console.log('cheeter');
+    }
 }
