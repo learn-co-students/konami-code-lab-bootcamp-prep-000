4c57e8d9651f33d6197a059f87fc61f50b7000e8
Roxana <roxanafetea@yahoo.com>
2018-05-23 03:15:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -17,19 +17,18 @@ function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(event) {
    const key = event.key;
-  for(let i=0;i<codes.length;i++){
-  if (key === codes[i]) {
-     index++;
-  
-   if (index === codes.length) {
-        alert("Hurray!");
-        index = 0;
-      
-   }} 
-   else {
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
     index = 0;
   }
-  }   
   });
  
 }
