cb07d0e67beb7f8ea7de784cb27dd39665e265ed
Mahad Najam <mahad.najam15@gmail.com>
2018-05-23 07:20:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,26 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var body = document.querySelector("body");
+  body.addEventListener("keydown", konami); 
 }
+
+var index = 0;
+
+function konami(x){
+  const key = x.key;
+  console.log(key);
+  if (key === codes[index]){
+    index++;
+
+    if (index === codes.length){
+      alert("Konami Code Successful");
+      index = 0;
+    }
+  }
+  else {
+    index = 0;
+  }
+}
+
+init();
