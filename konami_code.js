1233e6e08b8ab66d66ab02a5c1348ff93b78bece
Beatrice Mendoza <mendoza@nyu.edu>
2018-05-24 21:41:03 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+    }
+    
+    if (index === codes.length) {
+      alert('Hurray!'); 
+      index = 0;
+    }
+  }
+  
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
