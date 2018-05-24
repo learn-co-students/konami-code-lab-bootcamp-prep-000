d141cd005925f5dc9fc57704248c43935803b8db
Christofer Chan <devchumbing@gmail.com>
2018-05-24 02:45:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    index = (codes[index] === key) ? ++index : 0
+    
+    if (index === codes.length) {
+      window.alert("Hurray!");
+      index = 0
+    }
+    
+  });
 }
