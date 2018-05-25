83b6a4c80d92c96f30e3984cda1555a43401804e
Andrew Balmer <anbalmer@gmail.com>
2018-05-25 18:22:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,33 @@ const codes = [
   "a"
 ];
 
+/*This was my first approach. It works, but after a pair programming session it became clear that this approach was more expensive than the index approach. 
 function init() {
-  // your code here
+  var current = codes.slice(0);
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key === current[0]) {
+      current.shift();
+        if (current.length === 0) {
+          alert('Konami Success');
+          current = codes.slice(0);
+        }
+    } else {
+      current = codes.slice(0);
+    } 
+  } 
+)}*/
+
+function init() {
+  let index = 0
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert('Konami Success!');
+        index = 0
+      }
+    } else {
+      index = 0;
+    } 
+  })
 }
