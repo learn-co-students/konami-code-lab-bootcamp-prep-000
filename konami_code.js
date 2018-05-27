1adec23323867cff32c95d8836fc660bd7d6c7a1
Kevin Chi <kevinchi118@gmail.com>
2018-05-27 19:19:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+
+var index = 0
+const body = document.querySelector('body')
+body.addEventListener ('keydown', function(e) {
+  var key = e.key;
+  if (key === codes[index]) {
+    index +=1;
+    console.log(index);
+    if (index === codes.length) {
+      alert('Congrats');
+      index = 0;
+    }
+  }
+  else {
+    index = 0;
+  }
+})
