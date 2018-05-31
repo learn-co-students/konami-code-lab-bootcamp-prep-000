9e65e64acf913633757014b9fb3f08047288592e
Maxim Dalton <maximcdalton@gmail.com>
2018-05-31 02:24:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  var body = document.body;
+  
+  body.addEventListener('keydown', onKeyDownHandler);
+}
+
+var onKeyDownHandler = function(e) {
+  const key = e.key;
+  
+  if (key === codes[index]) {
+    index++;
+    
+    if (index === codes.length) {
+      alert("You Win!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
