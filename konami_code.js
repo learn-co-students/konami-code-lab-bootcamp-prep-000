7d97433348f8d501ff5ad080a0cb1cfd1293e16f
Trevor Jameson <tjameson15@gmail.com>
2018-05-31 21:56:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,22 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+document.body.addEventListener('keydown', function konamiTest(press) {
+    // Initialize input array and fidelity counter
+    let userInput = press.key;
+    let fidelity = 0
 
-function init() {
-  // your code here
-}
+    // Check user input for fidelity against Konami code
+    if (codes.length === userInput.length) {
+      for (let i = 0; i < codes.length; i++) {
+        if (codes[i] === userInput[i]) {
+            fidelity++;
+        } else if (codes[i] !== userInput[i]) {
+            fidelity = 0;
+            codes.shift();
+            return false;
+        }
+      }
+    }
+    // Check fidelity count of array comparison and alert the user if successful
+    if (fidelity === codes.length) {
+        alert("You are the Konami Master of legend!");
+    }
+  });
