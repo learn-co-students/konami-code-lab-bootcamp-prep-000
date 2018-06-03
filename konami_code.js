3510e2459bfea69e583d8e3a13f4cb2fac7783fe
Sean Huang <hyprul@ucla.edu>
2018-06-03 01:44:53 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,17 +12,23 @@ const codes = [
 ];
 
 function init() {
-  let idx = 0
+  let index = 0
 
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
     
-    idx = (codes[idx] === key) ? ++idx : 0
-    
-    if (idx === codes.length) {
-      window.alert("Hurray!");
-      idx = 0
+    if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
     }
-    
+  } else {
+    index = 0;
+  }
   });
-}
+
+}//end init
+
