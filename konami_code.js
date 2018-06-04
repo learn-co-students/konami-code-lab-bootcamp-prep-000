4147b4c38e6009d14fb497493ef72aa14c5bca57
Edward Mixon <edwardchase@gatech.edu>
2018-06-04 16:18:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var count = 0;
+  document.body.addEventListener('keydown',function(e) {
+    var string = codes[count];
+    var press = e.key;
+    if (string === press) {
+      console.log(string);
+    count++;
+     if (count === codes.length) {
+      alert('Hurray!');
+      count = 0;
+    }
+    } else {
+      count = 0;
+      }
+  } )
 }
+    
+
+
