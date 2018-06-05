26ad86e309a4940297c0f2b48c650e192cdca6ae
Gabriel Valle <gvalle91@gmail.com>
2018-06-05 16:47:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,20 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0;
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e){
+    const key = e.key;
+    if (key === codes[index]){
+      index++;
+      if (index === codes.length){
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
