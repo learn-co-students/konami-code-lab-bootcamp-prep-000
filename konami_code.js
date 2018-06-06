9ced4e96e305eb2ad998e662c4d201c10561f862
Sean Logan Para <spara77@gmail.com>
2018-06-06 23:40:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+                                  
 function init() {
-  // your code here
-}
+  let codeIndex = 0
+
+  document.body.addEventListener("keydown", function (keyPressed) {
+    const key = keyPressed.key
+
+    if (codes[codeIndex] === key) {
+       codeIndex = ++codeIndex
+                                  }
+     else {
+       codeIndex = 0
+          }
+
+    if (codeIndex === codes.length) {
+      window.alert("Code entered!");
+      codeIndex = 0
+                                    }
+    }
+                            )
+              }
