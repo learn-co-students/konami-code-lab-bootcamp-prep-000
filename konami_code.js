b38741bc61aa99c9b2669fa0bbc8e02b808c518f
Harry Dibden <harry-dibden@hotmail.com>
2018-06-06 13:01:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+var i = 0;
+
+function init(e) {
+  var key = e.key;
+  console.log(key);
+  if(key === codes[i]){
+    i++;
+    if(i === codes.length){
+      alert("Well Done!");
+      i = 0;
+    } 
+  } else {
+    i = 0;
+  }
+  console.log(i);
 }
+
+document.body.addEventListener('keydown', init);
+
