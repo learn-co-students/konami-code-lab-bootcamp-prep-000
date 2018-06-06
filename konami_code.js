7fb8c2a1b794c5b2af1e15d33f76a2e7416bacfb
Bashir Ahamed <ahamedb1991@gmail.com>
2018-06-06 04:15:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+      const key = event.key;
+      
+      if(key == codes[index]) {
+        index++;
+        if(index == codes.length) {
+          alert("Hi Konami");
+          index = 0;
+        }
+      }
+      else {
+        index = 0;
+      }
+  });
 }
