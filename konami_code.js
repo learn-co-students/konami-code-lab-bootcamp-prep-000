16e1b8ac46b6af5f34ccacfd3d926b245e67aaaf
Danny O'B <dannyk85.dob@gmail.com>
2018-06-07 22:29:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  // set up a page listener
+  var listener = document.addEventListener("keydown", checkKeys)
+}
+
+function checkKeys()
+{
+  var keyPressed = event.key;
+  
+  console.log(`Keypress Detected: ${keyPressed}`)
+  
+  var count = 0
+  
+  // begin the game
+  if(keyPressed === codes[count])
+  {
+    
+  }
+  
+  
+  
 }
