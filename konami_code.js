a62bf3ce2a2d86d8073a812be579895773703eb0
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-08 00:46:01 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+
+  const body = document.querySelector('body');
+
+  body.addEventListener('keydown',
+    function onKeyDownHandler(e){
+      const key = e.key;
+
+      if(key === codes[index]){
+        index++;
+
+        if(index === codes.length){
+          alert(`KONAMI!`);
+
+          index = 0;
+        }
+      } else {
+        
+      }
+    } )
 }
