fde3b35540eb70e781a0257955a39d58e1e77af8
heii70 <williamgao70@yahoo.com>
2018-06-09 08:25:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,24 @@ const codes = [
 
 function init() {
   // your code here
+  
+  var progress = 0;
+
+  function onKeyDownHandler(e) {
+    
+    const key = e.key;
+    console.log(key + progress);
+      
+    if(key === codes[progress]) 
+      progress++;
+    else
+      progress = 0;
+      
+    if(progress === codes.length) {
+      alert("Hurray!");
+      progress = 0;
+    }
+  }
+
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
