db50deb21208c4436617d1517d49d153df99e301
Mark Noonan <markthomasnoonan@gmail.com>
2018-06-10 22:05:44 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,19 @@ const codes = [
   "b",
   "a"
 ];
-
+let matchingKeypresses = []
 function init() {
   // your code here
+
+  document.body.addEventListener('keydown', function(event) {
+    if (codes[matchingKeypresses.length] === event.key){
+      matchingKeypresses.push(event.key)
+      console.log(matchingKeypresses)
+    } else {
+      matchingKeypresses = []
+    }
+    if (matchingKeypresses.length === codes.length){
+      alert("You easter egg'd!")
+    }
+  })
 }
