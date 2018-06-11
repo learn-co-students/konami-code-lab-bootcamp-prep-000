e88a09a56bf4a480d48e11a3c8fcdcdba6c115fd
Joseph Lander <josephclander@gmail.com>
2018-06-11 20:49:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,34 @@ const codes = [
   "a"
 ];
 
+var index = 0;
+
 function init() {
   // your code here
+document.body.addEventListener('keydown', function(e) {
+
+    var keyName = e.key;
+    console.log(e.key);
+    if(keyName === codes[index]) {
+      index++;
+  
+    if(index === codes.length) {
+      alert("Congratulations - UNLOCKED");
+      index = 0;
+    } 
+
+  } else {
+    index = 0;
+  }
+
+  
+})
+
+
 }
+
+
+
+
+
+
