acc3e7f0bf3f02f8ed2cf33649beff934a30ef0a
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-06-11 10:40:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,30 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
-}
+  // Keep track of index outside of the event handler
+  let index=0;
+  // Add event listener to document.body and check for 'keydown' events
+  document.body.addEventListener('keydown', function onKeyDownHandler(e){
+    // Declare constant equal to key that was pressed
+    const key = e.key;
+    // If key is equal to codes[index]
+    if(key === codes[index]){
+      // Increment index
+      index++;
+      // If index is equal to codes.length
+      if(index === codes.length) {
+        // alert
+        alert("Congratulations!");
+        // reset index to 0
+        index = 0;
+      }
+    }
+    // otherwise
+    else {
+      // set index equal to 0
+        index = 0;
+      }
+    })
+  }
