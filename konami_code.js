5d0f03e011f383941f37a3c3de49f2dc26826a3a
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-06-12 16:19:46 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,51 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const input = document.querySelector('body')
+
+ input.addEventListener('keydown', function(e) {
+   switch(index){
+     case 0:
+       index = (e.which === 38) ? 1 : 0;
+       break;
+     case 1:
+       index = (e.which === 38) ? 2 : 0;
+       break;
+     case 2:
+       index = (e.which === 40) ? 3 : 0;
+       break;
+     case 3:
+       index = (e.which === 40) ? 4 : 0;
+       break;
+     case 4:
+       index = (e.which === 37) ? 5 : 0;
+       break;
+     case 5:
+       index = (e.which === 39) ? 6 : 0;
+       break;
+     case 6:
+       index = (e.which === 37) ? 7: 0;
+       break;
+     case 7:
+       index = (e.which === 39) ? 8 : 0;
+       break;
+     case 8:
+       index = (e.which === 66) ? 9 : 0;
+       break;
+     case 9:
+       index = (e.which === 65) ? 10 : 0;
+       break;
+     default:
+       index = 0;
+   }
+   checkKonami();
+ })
+}
+
+function checkKonami(){
+  if(index === codes.length){
+    console.log("konami");
+    window.alert("konami");
+    index = 0;
+  }
 }
