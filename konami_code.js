3942aaed21dea9bea22c2aea7c720f30f6d248fc
gmat191@gmail.com <gmat191@gmail.com>
2018-06-14 02:53:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = e.key
+  if (key === alphabet[index]) {
+    index++;
+    if (index === alphabet.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+}
+}
+
 function init() {
   // your code here
 }
