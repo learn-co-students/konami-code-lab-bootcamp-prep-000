3645625b4eabfed21e3f4f4f0ab8e910a6fe7009
gmat191@gmail.com <gmat191@gmail.com>
2018-06-14 23:39:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,31 +1,49 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
 
 let index = 0;
 
 function onKeyDownHandler(e) {
-  const key = e.key
-  if (key === alphabet[index]) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
     index++;
-    if (index === alphabet.length) {
+ 
+    if (index === codes.length) {
       alert("Hurray!");
+ 
       index = 0;
     }
   } else {
     index = 0;
-}
+  }
 }
 
 function init() {
-  // your code here
+ // your code here
+  var body = document.body;
+  body.addEventListener('keydown', function(e){
+    var key = e.key  
+    if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
 }
