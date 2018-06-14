eef962acf3ae3bd44171c76e7898e334e577f660
Andres Manco <andresmanco@gmail.com>
2018-06-14 03:00:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -14,19 +14,23 @@ var index = 0;
 
 function init() {
   // your code here
-  var body = document.querySelector(body)
-  
-  body.addEventListener('keyDown', function(e){
+  const body = document.querySelector('body')
+
+  body.addEventListener('keydown', function(e){
+    console.log("apretaste una tecla")
     var k = e.k;
-    
+    console.log(k)
+    console.log(codes[index])
     if(k === codes[index]){
       index++;
+      console.log('primer IF')
       if(index === codes.length){
         console.log("Congratulations, WUJUUU!!!")
         index = 0;
       }
     }
     else{
+      console.log('index a 0')
       index = 0;
     }
   })
