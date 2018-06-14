008eb9d489d4b3de13905cf2ca00eb309e51e34b
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-14 05:42:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+let index = 0
+function konami(e) {
+  const k = e.key
+  if (k === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert('Congratulations, you have found Narnia!');
+      index = 0
+    }
+  } else {
+    index = 0
+  }
+}
+
 function init() {
-  // your code here
+  return document.body.addEventListener('keydown', konami)
 }
