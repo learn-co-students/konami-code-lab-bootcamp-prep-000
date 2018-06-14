b799b47badbc4916b4b80ddc4096ed5162d80d35
Andres Manco <andresmanco@gmail.com>
2018-06-14 02:17:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,12 +10,24 @@ const codes = [
   "b",
   "a"
 ];
+var index = 0;
 
 function init() {
   // your code here
   var body = document.querySelector(body)
   
   body.addEventListener('keyDown', function(e){
+    var k = e.k;
     
+    if(k === codes[index]){
+      index++;
+      if(index === codes.length){
+        console.log("Congratulations, WUJUUU!!!")
+        index = 0;
+      }
+    }
+    else{
+      index = 0;
+    }
   })
 }
