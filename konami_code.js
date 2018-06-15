ad2d147f0c6e5202ad9949d57c2b20d2c06f526b
Rodolfo Guzman <rodolfoguzman147@gmail.com>
2018-06-15 23:56:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,21 @@ const codes = [
   "b",
   "a"
 ];
-
-function init() {
-  // your code here
-}
+function init(){
+newArray=[]
+let a=0
+const oye=document.querySelector('body')
+oye.addEventListener('keydown', function(e){ 
+  const key=e.key
+    if(key===codes[a]){
+      a++;
+    
+      if (a==codes.length){
+        alert('GREAT JOB! HAHAHA you did it!')
+      a=0;  
+      }
+    }else{
+      a=0;  
+      }
+    }
+  )}
