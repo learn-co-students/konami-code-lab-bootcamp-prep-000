ce26cb865c8e6dfe8bc4520bc2f1713619ad095e
Carson Kelley <carsonmkelley@gmail.com>
2018-06-15 11:53:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  document.body.addEventListener('keydown',onKeyDownHandler);
+  let index = 0;
+  function onKeyDownHandler(e) {
+  	const key = e.key;
+  	console.log(key);
+  	if(key === codes[index]) {
+  		index++;
+  		if(index === codes.length) {
+  			alert("Yay!");
+  			index = 0;
+  		}
+  	} else {
+  		index = 0;
+  	}
+  }
 }
