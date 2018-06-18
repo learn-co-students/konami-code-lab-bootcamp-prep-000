25c96a774f18866c8f237fcea622c3bf936f5549
Jonathan Hong <jonhong777@gmail.com>
2018-06-18 16:42:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  const test = document.body
+  test.addEventListener('keydown', function(event){
+      const key = event.key;
+
+      if(key === codes[index]){
+        index++;
+        if(index === 10){
+        alert("Congratulations!")
+        }
+      }else{
+        index = 0;
+      }
+  }  )
 }
