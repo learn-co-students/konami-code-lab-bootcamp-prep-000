5b96d00a4fa31166e6d2ec30f3b8a870c7f56f21
Alex Paulo da Costa <alex.callum.c@gmail.com>
2018-06-20 16:24:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  
+  document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  if(keyName === codes[index])
+  {
+    index++;
+  }
+  else
+  {
+    index = 0;
+  }
+  if(index === codes.length)
+  {
+    alert('Hurray!');
+    index = 0;
+  }
+});
 }
