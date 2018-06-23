428ebccb8c1de72a84d2369072c65a86881c0a2d
dcartist <pbannerman13@gmail.com>
2018-06-23 02:22:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -20,29 +20,54 @@ function nope(){
 
 function init() {
   // your code here
-  var doc = document.getElementsByTagName('body')
-  var newcode = []
-  doc.addEventListener('keydown', function(e) {
-  // function onKeyDownHandler(e){
-    const key = e.keyCode
-    newcode.push(key)
-    console.log(key)
-    if (newcode.length == codes.length){
-      if(newcode == codes){
-        konami()
-      }
-      else {
-        nope()
-        newcode.length = 0
-      }
-    }else {
-      nope()
-      newcode.length = 0
-    }
 
-  // if (e.which === 71) {
-  //   return e.preventDefault()
+  document.body.addEventListener('keydown',function(e){
+  var key = parseInt(e.which || e.detail)
+
+  var index=0
+
+      if(key === codes[index]){
+      index++
+
+
+      if(index === code.length){
+       konami()
+       index=0
+     }
   }
-})
+     else{
+       console.log(e.which)
+       index=0
+     }
+
+
+
+
+
+  // var doc = document.getElementsByTagName('body')
+//   var newcode = []
+//   doc.addEventListener('keydown', function(e) {
+//   // function onKeyDownHandler(e){
+//     const key = e.keyCode
+//     newcode.push(key)
+//     console.log(key)
+//     if (newcode.length == codes.length){
+//       if(newcode == codes){
+//         konami()
+//       }
+//       else {
+//         nope()
+//         newcode.length = 0
+//       }
+//     }else {
+//       nope()
+//       newcode.length = 0
+//     }
+//
+//   // if (e.which === 71) {
+//   //   return e.preventDefault()
+//   }
+// })
 
 }
+init()
