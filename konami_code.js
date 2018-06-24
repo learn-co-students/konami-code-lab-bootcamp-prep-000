5454ca9fba9e6b696c6747e768fde4d4f6040818
Alexandra Batrak <alexandrabatrak@gmail.com>
2018-06-24 19:18:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var index = 0;
+  document.body.addEventListener('keydown', function (e) {
+    var key = e.key;
+    if (codes[index] === key){
+      index++;
+      if (index === codes.length){
+        window.alert("You win!");
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  });
 }
