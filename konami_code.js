e85757a5ba685c6345ab0d98d5eb4339da82751c
Lige <lige.williamson@gmail.com>
2018-06-25 13:16:50 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,23 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const body = document.querySelector('body');
+
+  let index = 0;
+
+  body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = e.key;
+
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Congratulations, egg hunter!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
