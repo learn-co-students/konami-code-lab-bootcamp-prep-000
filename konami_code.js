f7eb8c3b39b774098916ed77c7fbd4823ad8f8f4
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-26 21:45:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,23 @@ const codes = [
 
 function init() {
   // your code here
+  var checkCode = [];
+  const input = document.querySelector('body')
+  input.addEventListener('keydown', (event) => {
+    checkCode.push(event.key)
+  if (checkCode.length === 10) {
+    var counter = 0;
+    for (var i = 0; i < 10; i++) {
+      if (checkCode[i] === codes[i]) {
+      counter++
+        }
+      }
+    if (counter === 10) {
+      alert('yes')
+      checkCode = []
+    } else {
+      checkCode = []
+    }
+  }
+})
 }
