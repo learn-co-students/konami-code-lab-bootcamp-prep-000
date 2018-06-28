4ce557ddc6d2e6a8bbc4cd5ffd318fcfd0c598f9
dcartist <pbannerman13@gmail.com>
2018-06-28 00:49:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,67 +1,102 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+	"ArrowUp",
+	"ArrowUp",
+	"ArrowDown",
+	"ArrowDown",
+	"ArrowLeft",
+	"ArrowRight",
+	"ArrowLeft",
+	"ArrowRight",
+	"b",
+	"a"
 ];
 var back = document.querySelector("body")
 var thecode = []
 var truth = 0
+var wrong = 0
 function konami(){
-  window.alert('congratulatory message')
+	return window.alert('congratulatory message')
 }
 
 function nope(){
-  window.alert('nope message')
+	return window.alert('nope message')
 }
 
+function keychange(){
+	for(var i = 0; i<thecode.length;i++){
+		if (thecode[i] == "KeyA"){
+			thecode[i] = "a"
+		}else if (thecode[i] == "KeyB"){
+			thecode[i] = "b"
+		}
+	else{
+		console.log(thecode[i])
+	}}
+}
+function theverdict(){
+	for(var i = 0; i<thecode.length;i++){
+		if (thecode[i] == codes[i]){
+			truth = truth + 1
+		} else{
+			wrong = wrong + 1
+		}
+	}
+}
 
 function init() {
-  // your code here
-back.addEventListener('keydown',function(event){
+	// your code here
+	back.addEventListener('keydown',function(event){
+	thecode.push(event.code)
+	keychange()
+	if(thecode.length == 10){
+		theverdict()
+		if (truth == 10){
+			alert('konami wins')
+		} else {
+			alert('this is 10')
+		}
+		thecode.length = 0
+	}
 
-})
+})}
 
+//back.addEventListener('keydown', function(event) {
+//	 thecode.push(event.code)
+//
+//	 if (thecode.length == 10){
+		 //Checking for KeyA and KeyB
 
-back.addEventListener('keydown', function(event) {
-   thecode.push(event.code)
-
-   if (thecode.length == 10){
-     //Checking for KeyA and KeyB
-     for(var i = 0; i<thecode.length;i++){
-       if (thecode[i]=="KeyA"){
-         thecode[i] = "a"
-       }else if (thecode[i]=="KeyB"){
-         thecode[i] = "b"
-       } else{
-         console.log("otherchar")
-       }//End of checking
-     }
-     //Checking for right code
-     for (var i = 0; i<thecode.length;i++){
-       if (thecode[i]==codes[i]){
-     	truth = truth+ 1
-       }else {
-         console.log (codes[i]+" is false")
-       }
-     }
-     //end of checking for truth
-     if(truth == 10){
-       konami()
-       thecode.length = 0
-       truth = 0
-     }else {
-       thecode.length = 0
-       truth = 0
-     }
-  })
+//		 for(var i = 0; i<thecode.length;i++){
+//			 if (thecode[i]=="KeyA"){
+//				 thecode[i] = "a"
+//			 }else if (thecode[i]=="KeyB"){
+//				 thecode[i] = "b"
+//			 } else{
+//				 console.log("otherchar")
+//
+//			 }//End of checking
+//			alert("this is at 10");
+//			thecode.length = 0
+//		 }
+		 //Checking for right code
+//		 for (var i = 0; i<thecode.length;i++){
+//			 if (thecode[i]==codes[i]){
+//		 	truth = truth+ 1
+//			 }else {
+//				 console.log (codes[i]+" is false")
+//			 }
+//		 }
+//		 //end of checking for truth
+//		 if(truth == 10){
+//			 konami()
+//			 thecode.length = 0
+//			 truth = 0
+//		 }else {
+//			 thecode.length = 0
+//			 truth = 0
+//		 }
+//	})
 //end of addEventListener
-}//end of init()
-}
+//}//end of init()
+// }
 init()
