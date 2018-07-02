1acbaf737d0f62681e7956eba25991b49ca49f4a
Felix Chan <felixchan29@gmail.com>
2018-07-02 15:17:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener("keydown", function(event){
+    const key = event.key;
+    console.log(key);
+    if(key === codes[index]) {
+      index++;
+      console.log(index);
+      if(index === codes.length){
+        alert("Congrat!!")
+      
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
