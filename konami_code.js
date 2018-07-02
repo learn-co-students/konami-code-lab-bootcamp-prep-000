92c47f0c303189c4626ab713ad29acc9b9a88f67
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-07-02 01:24:42 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,31 @@ const codes = [
   "a"
 ];
 
+const konamiCheck = function() {
+  codeIndex = 0;
+
+  return function(e){
+    if (e.key === codes[codeIndex]){
+      console.log("NICE");
+      codeIndex += 1;
+      if (e.key === "a"){
+        alert("KONAMI GOD MODE ACTIVATED!!!");
+        codeIndex = 0;
+      }
+    }
+    else {
+      console.log("WOMP WOMP");
+      codeIndex = 0;
+    }
+    console.log(e.key);
+  }
+}()
+
 function init() {
+  document.body.addEventListener('keydown', konamiCheck)
   // your code here
 }
+
+init();
+
+
