e4201fe58e72a4f008b0220037d433ee461d0726
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-07-02 13:59:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,35 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
+  
+function init(){
+  
+ let index = 0;
+ 
+  document.addEventListener('keydown', function(e){ 
+      const key = e.key;
+    
+      if (key === codes[index]) {
+        index++;
+       } else {
+         index = 0;
+       }
+        
+     if (index === codes.length) {
+       console.log("hurray!");
+      index = 0;
+    }
+  });
 
-function init() {
-  // your code here
 }
+
+
