87e509e6ab933655927387797536291b37e662e1
Abrahym Eldden <asharfeldden@gmail.com>
2018-07-03 22:51:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,31 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
 function init() {
-  var index = 0;
-  document.addEventListener('keydown', function (e) {
-    const key = parseInt(e.detail || e.which);
-
-    if (key === code[index]) {
-      index++;
-
-      if (index === code.length - 1) {
-        window.alert("YOU DID IT!");
-        index = 0;
+  var index = 0
+  document.body.addEventListener('keydown', function(e) {
+    if (e.key === codes[index]) {
+      index++
+      console.log(e.key)
+      console.log(index)
+      
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
       }
     } else {
-      index = 0;
+      index = 0
     }
-  }, false);
+  })
+
 }
