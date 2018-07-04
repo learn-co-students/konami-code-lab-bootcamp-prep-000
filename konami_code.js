85cc9ad3519e41a99253ca435fd23e38f9eb6336
Lucas Kabot <loyd.kim14@gmail.com>
2018-07-04 21:18:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,23 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a']
+  var index = 0
+  
+  function konamiCode(combination) {
+    var key = combination.key
+    if (key === code[index]) {
+      index ++
+      if(index === code.length) {
+        alert("Code entered successfully!")
+        
+        index = 0
+      }
+    }
+    else {
+      index = 0
+    }
+  }
+  
+  document.body.addEventListener('keydown', konamiCode)
 }
