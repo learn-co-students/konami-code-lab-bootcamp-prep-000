7a51f1fa0d3b84eb8507cf5355b822ec4e6368e1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-12 03:36:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,24 @@ const codes = [
 
 function init() {
   // your code here
+  const main = document.body
+  main.addEventListener('keydown', onKeyDownHandler)
+}
+
+let index = 0
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key === codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
