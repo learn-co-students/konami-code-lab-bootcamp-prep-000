ecd7f1451ed600d63965592e52a555025754d92b
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-13 05:17:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+let index = 0;
+const body = document.querySelector('body')
+body.addEventListener('keydown', function (e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Finish Him!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
 }
