013de4e9f5797e4c259c9bb6d0615c8cff06e580
Angelica Fields <angelicafields08@gmail.com>
2018-07-13 00:44:47 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  var index = 0;
+  document.body.addEventListener('keydown', function(e){
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        alert("Yes! It worked!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
+
+
