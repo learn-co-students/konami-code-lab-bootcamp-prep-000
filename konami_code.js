10abf782d3c3e8e7913bc1377226f01491561161
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-18 18:54:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,46 @@ const codes = [
   "a"
 ];
 
+// function init() {
+//   // `keySequence` will keep track of the last n keystrokes, where n === `codes.length`. `keySequence` will be compared to `codes` for each keydown event.
+//   var keySequence = []
+  
+//   var body = document.querySelector('body')
+  
+//   body.addEventListener('keydown', function(e) {
+//     if (keySequence.length >= codes.length) {
+//       keySequence.shift(e.key)
+//     }
+//     keySequence.push(e.key)
+    
+//     // console.log(keySequence)
+    
+//     if (JSON.stringify(keySequence) === JSON.stringify(codes)) {
+//       alert("Konami code activated!")
+//     }
+//   })
+// }
+
+
+
 function init() {
-  // your code here
+  let body = document.querySelector('body')
+  let index = 0
+  body.addEventListener('keydown', function(e) {
+    
+    const key = e.key
+    
+    if(key === codes[index]) {
+      index++
+      
+      if(index === codes.length) {
+        alert('Konami code activated!')
+        index = 0
+      }
+      
+    } else {
+      index = 0
+    }
+    
+  })
 }
