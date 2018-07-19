bb0b150b3775915533c56fc1e6c9fe29e3b1aff3
Andrew Sprague <andrewsprague@outlook.com>
2018-07-19 19:10:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,31 @@ const codes = [
   "a"
 ];
 
-function init() {
+let index = 0;
+
+const input = document.getElementById('mocha');
+
+function init(e) {
   // your code here
+  
+  let currentEvent = e;
+  
+  if (currentEvent === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert('Hurray!');
+      
+      index = 0;
+    } 
+  } else {
+      console.log('fired');
+      index = 0;
+  }
 }
+
+document.body.addEventListener ('keydown', (event) => {
+  let keyName = event.key;
+  init(keyName);
+});
+
+
