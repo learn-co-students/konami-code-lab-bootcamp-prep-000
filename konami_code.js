36f0a2cd3e38d3fe502340f6d619af730b7da253
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-22 15:22:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var index = 0;
+  var getElement = document.querySelector("body")
+  getElement.addEventListener('keydown', function(e)
+  {
+    var key = e.key
+    if(key === codes[index])
+    {
+      index++
+      if(index === codes.length)
+      {
+        alert("Congrats")
+      }
+    }
+    else
+    {
+      index = 0
+    }
+  })
 }
