543a1d6679142dc9b8b0163ff432b8d80e80779c
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 15:05:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
+let index = 0
+
 function init() {
   // your code here
+  document.body.addEventListener("keydown", function (e) {
+
+    console.log(`Button pressed is ${e.key} and index is ${index}`)
+    
+    if (e.key === codes[index])
+      index++
+    else
+      index = 0
+
+    if (index === codes.length)
+    {
+      alert("You did it!")
+    }
+  })
 }
