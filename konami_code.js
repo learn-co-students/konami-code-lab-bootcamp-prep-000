29aae829c24bf371a726441278708a8e4d0dc02a
Erin Green <erin.y.green@gmail.com>
2018-07-26 19:53:29 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,16 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+
 function init() {
-  // your code here
-}
+  document.body.addEventListener('keydown', (e) => {
+  if (e.key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert("Woohoo!");
+      index = 0; 
+    }
+    } else { index = 0;}}
+    );}
