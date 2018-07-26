ad2125615e04aa32be0f7647a44bc3489aaef846
Mark Baker <mrbaker1917@gmail.com>
2018-07-26 03:45:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      console.log(e.key)
+      index++;
+      console.log(index);
+      if (index === codes.length) {
+        window.alert("Hurray!");
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+init();
