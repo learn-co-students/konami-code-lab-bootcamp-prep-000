6c504ef30a91e1f6c2dcc5921b9c5d6e65e01921
Jin Ai Yap <jinai.jyap@gmail.com>
2018-07-27 15:21:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+// var main = document.getElementById("myBtn")
+var main = window.document
+main.addEventListener("keydown", function() {
+    myPress(event)
+})
+
+var index = 0
+
+function myPress(e) {
+    // alert(`${e.key} was pressed!`)
+    const key = e.key
+    if (key === codes[index]) {
+        index ++
+        // document.getElementById('demo').innerHTML = index
+        if (index === codes.length) {
+            alert("Hurray!")
+            index = 0
+        }
+    }
+    else {
+        index = 0
+    }
 }
