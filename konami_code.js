6ef055214f685dcfe359069d8c1c82a142a620b8
Brian Zhang <brianzhangw@gmail.com>
2018-07-27 03:51:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+  document.body.addEventListener("keydown", checkClicks);
+}
+
+let index = 0;
+
+function checkClicks(e){
+  const key = e.key;
+  
+  if(key === codes[index]){
+    index++; 
+    if (index === codes.length){
+      alert("Hurray!");
+      index = 0;
+    }
+  }
+  else{
+    index = 0;
+  }
 }
