924846a86b49c410f0e063296e90187c04a9ee2e
Taylor J Young <tjy14@scarletmail.rutgers.edu>
2018-07-29 20:30:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+ let index = 0
+ document.body.addEventListener('keydown',function(event){
+let key = event.key
+    if (codes[index] === key){
+    index++ 
+      if (index === codes.length){
+     alert("Congratulations, Sir, on cracking the Konami Code.")
+}
+}
+   else {
+     index = 0;
+   } 
+  
+})
 }
