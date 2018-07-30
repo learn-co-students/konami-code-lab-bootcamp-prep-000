1a0f5f80fe5b887d9a5edeec4881e6242a627bf0
Ben Kudler <bkudler@gmail.com>
2018-07-30 15:07:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let pressed = [];
+
+  document.body.onkeydown = (e) =>{
+    const key = e.detail || e.which
+
+    pressed.push(key);
+    pressed = pressed.length > 10 ?  pressed.slice(pressed.length - 10, pressed.length) : pressed;
+
+    if(pressed === code){
+      alert("whoopie")
+    }
+  }
 
 }
+
+
+init()
