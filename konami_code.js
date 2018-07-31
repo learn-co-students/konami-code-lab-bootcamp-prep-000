6271f71b2366505fea528db0c248f9aec32457d1
Mai Campbell <jirallc@gmail.com>
2018-07-31 02:37:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  let index = 0
+
+  document.body.addEventListener('keydown', function(e) {
+    // for (let i = 0; i < codes.length; i++) {
+    // }
+
+    const key = e.key;
+
+    if (key === codes[index]) {
+      console.log(key)
+      index++;
+    } else if (key !== codes[index]) {
+       index = 0;
+     }
+    if (index === codes.length) {
+      alert("Congratulation, You just trigger konami code");
+      index = 0;
+    }
+  })
 }
