d353eb368afc40dbc5c0e0fe14c1aecb97fac339
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-08-03 20:21:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,30 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+ var index=0
+ document.body.addEventListener('keydown',function(e){
+   var key=e.key
+
+   if(key===codes[index]){
+   
+     index++
+
+     if(index===codes.length){
+    console.log("before")
+      window.alert("KONAMI!")
+      console.log("skipped")
+       index=0
+     }
+   }
+     else{
+    
+       index=0
+     }
+     
+   
+
+ })
 }
