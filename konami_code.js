c728cb55f88ee6ebc1b38492990a67a40f9be49a
Hilary Sturges <hilarysturges@gmail.com>
2018-08-08 22:57:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,23 @@ const codes = [
 function init() {
   // your code here
 }
+
+const keys = ['a', 'b', 'ArrowUp','ArrowDown','ArrowLeft','ArrowRight'];
+
+let index = 0;
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === keys[index]) {
+    index++;
+ 
+    if (index === keys.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
