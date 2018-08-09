cba15951310f80b958bc708e21fa4d398865fb70
jaredianmills <jaredianmills@gmail.com>
2018-08-09 18:08:40 -0400

    Done

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 
+let index = 0
+
 function init() {
-  const body = document.querySelector('body');
-  var i = 0;
-  body.addEventListener('keydown', function(e) {
-    var keyPress = parseInt(e.which || e.detail);
-    if (keyPress === code[i]) {
-      i++;
-        if (i === code.length) {
-          alert("Congratulations! You found the code!");
-          i = 0;
-        }
+  let index = 0
+
+  document.addEventListener("keydown", (e) => {
+    if (e.which === code[index]) {
+      index++
+      // debugger
+      if (index === code.length) {
+        alert("You got the code!")
+      }
     } else {
-      i = 0;
+      index = 0
     }
   })
 }
