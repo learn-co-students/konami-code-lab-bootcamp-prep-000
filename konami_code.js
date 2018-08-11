fc86de7e01adddc1eecc092451ef1e2ad2fd5abd
Leslie Turis <lturis@gmail.com>
2018-08-11 22:19:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -18,17 +18,16 @@ function init() {
   let i = 0;
   document.body.addEventListener('keydown', function(e){
   let key = e.key;
+  console.log(key);
     if (key === codes[i]) {
       i++;
+    }
+    else {
+          i = 0;
+    }
       if (i === codes.length) {
         alert('Congratulations!');
         i = 0;
       }
-    }else {
-      i = 0;
-    }
-      return index;
-  });
-
+  })
 }
-init();
