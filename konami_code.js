9c39bf54e5b11bf660cc640f00544f2aead49c22
Alyssa Vongries <alyssavongries@gmail.com>
2018-08-12 19:45:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+ function init() {
+   let index = 0;
+  document.body.addEventListener('keydown', function(e){
+    const key = e.key;
+    if (key === codes[index]){
+      index++;
+    } else{
+      index = 0;
+    }
+    if(index === codes.length){
+        alert('Yes!');
+        index = 0;
+    }
+    })
+ }
+ 
+ 
