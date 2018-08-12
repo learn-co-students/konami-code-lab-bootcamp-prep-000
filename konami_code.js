51a1624d5c153952aec7c19ea6a8d2e4fdce6621
John Werenski <jweredc@gmail.com>
2018-08-12 19:30:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let i = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[i]) {
+      i++;
+    
+      if (i === codes.length) {
+        alert('Congrats!');
+        i = 0;
+      }
+    }
+    else {
+      i = 0;
+    }
+  });
 }
