74f00d77b760f243e78e70ca905e089dbe7b76da
Renan Góes <renangoes12@gmail.com>
2018-08-14 00:11:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,25 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
-
-function init() {
-  // your code here
-}
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
+let index = 0;
+const main = document.querySelector('.main')
+ 
+main.addEventListener('keydown', function(event){
+    const keyPress = event.key;
+    if (keyPress === codes[index]){
+        index++;
+        if (index === codes.length){
+            alert('Konami Code Done!');
+            index = 0;
+        }} else { index = 0;}
+  // alert(`${event.key} was clicked!`)
+})
