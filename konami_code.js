9ead55e533a13198301044b356f855d135a2c976
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 10:56:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,37 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+
+const alphabet = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"];
+ 
+let index = 0;
+
+function onKeyDownHandler(event) {
+  const key = event.key;
+  if (key === alphabet[index]) {
+    index++;
+    if (index === alphabet.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+document.body.addEventListener('keydown', onKeyDownHandler);
+
 }
