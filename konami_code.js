82a5a2cfa69f02f4b7370cf5d0458fdc998ad9a2
Ryan <eckerle_r@protonmail.com>
2018-08-15 05:46:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+var index = 0;
+
 function init() {
   // your code here
+  var body = document.body
+  
+  body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which)
+    
+    if(key === code[index]) {
+      index++
+      
+      if(index === code.length -1) {
+        index = 0
+        
+        alert("Contrats!")
+      } else {
+        index = 0
+      }
+    }
+  })
 }
+
+init()
