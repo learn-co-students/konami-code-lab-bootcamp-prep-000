ee2a490dbe5bee5d85fa77ddc6fe994a7a245493
Christopher Benedict <chrisjbenedict@gmail.com>
2018-08-15 17:15:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let idx = 0
+}
+
+document.body.addEventListener("keydown", e) {
+  const key = e.key;
+
+  idx = (codes[idx] === key) ? ++idx : 0
+    
+    if (idx === codes.length) {
+      window.alert("You did it!");
+      idx = 0
+    } else {
+    index = 0;
+  }
+  });
 }
