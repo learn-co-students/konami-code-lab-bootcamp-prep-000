be164c131e83bbc03853cc92e459cf721c622964
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-16 23:20:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,24 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0
+
+function onKeyDownHandler(e) {
+  const key = e.key
+  if(key === codes[index]) {
+    index++
+  }
+  else {
+    index = 0
+  }
+  if(index === codes.length) {
+    index = 0
+    alert("Hurray!")
+  }
+}
 
 function init() {
   // your code here
+  var main = document.querySelector("body")
+  main.addEventListener("keydown", onKeyDownHandler, false)
 }
