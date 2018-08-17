7f84999ba4b732cff7c605352fe182520ec1a85d
Lee Simon <shinoblee@gmail.com>
2018-08-17 18:20:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener('keydown',function (e){
+    const key = e.key;
+    if(codes[index] === key){
+      index++;
+      console.log(index);
+        if (index === codes.length){
+    alert('Hurray!');
+    console.log('Hurray!')
+    index = 0;
+  }
+    }else{
+      index = 0;
+    }
+  });
 }
