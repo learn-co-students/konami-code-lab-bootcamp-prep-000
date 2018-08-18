c9f71beb41e392a194670d526900713d4e30cc4b
Ryan <eckerle_r@protonmail.com>
2018-08-18 05:30:30 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -18,19 +18,8 @@ function init() {
   // your code here
   var body = document.body
   
-  body.addEventListener('keydown', function(e) {
-    var key = parseInt(e.detail || e.key)
-    
-    if(key === konamiCode[index]) {
-      index++
-      
-      if(index === konamiCode.length -1) {
-        
-        alert("Hurray!")
-        index = 0
-      } else {
-        index = 0
-      }
-    }
-  })
+  body.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    alert('keydown event\n\n' + 'key: ' + keyName);
+  });
 }
