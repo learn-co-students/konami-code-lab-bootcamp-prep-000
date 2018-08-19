8e1e6f4584519dd1f240f286839d059199bc7607
Michael Tompkins <mtompkins82@gmail.com>
2018-08-19 21:02:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -9,8 +9,26 @@ const codes = [
   "ArrowRight",
   "b",
   "a"
-];
+]; 
 
-function init() {
-  // your code here
+function init(){
+  
+let index = 0;
+
+const body = document.querySelector('body');
+body.addEventListener('keydown', function
+ onKeyDownHandler(e){
+  const key = e.key;
+  if (key === codes[index]){
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
 }
