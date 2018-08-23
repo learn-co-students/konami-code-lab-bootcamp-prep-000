09f604d69323c32d928cf870d1aed2e5a97f2b65
Dan N <dan.noh@gmail.com>
2018-08-23 20:40:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,29 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        
+        alert('Congratulations!');
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
+      }
+    
+  }
+const body = document.body;
+
+body.addEventListener('keydown', onKeyDownHandler)  
+  
 }
+
+
