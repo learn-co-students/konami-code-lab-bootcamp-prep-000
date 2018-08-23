4875c583484677544427b39507c903a1d33236dc
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-23 02:36:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+let index = 0
+  document.body.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+  
+  if (keyName === codes[index] ) {
+    index++;
+    
+      if (index === codes.length) {
+        window.alert("Hurray!");
+        
+        index = 0;
+      }
+  } else {
+    index = 0;
+  }
+  
+} );
+
+
 }
+
