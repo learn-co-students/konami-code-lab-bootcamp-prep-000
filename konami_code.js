d09839e9b53894674565d6c5bdeb73cd1a5f69a8
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-23 01:14:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,18 @@ const codes = [
 ];
 
 function init() {
+var index = 0;  
   document.body.addEventListener('keydown', function(e) {
     console.log(e.key)
+    if (codes[index] === e.key) {
+      index++;
+    } 
+    else {
+      index = 0;
+    }
+    if (index === codes.length) {
+      alert('Congrats');
+      index = 0;
+    }
   })
    }
