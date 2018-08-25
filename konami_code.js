ada5849080f40b9d297a65e8c3d85e61ae9fbe3b
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-08-25 17:15:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,17 @@ const codes = [
   "b",
   "a"
 ];
-
+let i = 0
 function init() {
-  // your code here
-}
+  document.body.addEventListener('keydown', function(e) {
+   let key = e.key
+   if (key === codes[i]) {
+     i++;
+     if (i === codes.length){
+       alert('You did it bb');
+       index = 0
+     }
+   } else {
+     index = 0
+   } 
+  })}
