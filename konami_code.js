c140a5ce22931cc41d1be49dfed0019b622ac965
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-25 19:50:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+var index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', kbdHandler);
+}
+ 
+function kbdHandler(e) {
+  var key = e.key;
+  if (key === codes[index]) {
+    index ++;
+    
+    if (index === codes.length) {
+      alert("Hurray!");
+      
+      index = 0;
+    } 
+    return true
+  } else {
+    index = 0;
+    return false
+  }
 }
