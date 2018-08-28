1425cdf6d9128c43777338436cf4a05dd8e32119
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-28 17:50:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var index = 0;
+  document.addEventListener('keydown', Function (e) {
+    var key = e.key;
+    if (key === codes[index]){
+      index++
+      
+      if (index === codes.length-1){
+        alert('Secret Solved!')
+        index = 0;
+      }
+      } else {
+        index = 0
+      }
+})
 }
