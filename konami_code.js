18e980925f4ae0f38244f1a567fb8d36eb501a5c
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-01 08:30:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', keyPressed)
+  
+  function keyPressed(event) {
+    const aKey = event.key
+    if (aKey === codes[index]) {
+      index++
+      
+    if (index === codes.length) {
+      alert("Woohoo!")
+      
+      index = 0
+    }
+    } else {index = 0}
+  }
 }
