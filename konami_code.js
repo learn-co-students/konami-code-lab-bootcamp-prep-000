7665d9cdca6264f5ab3ae1e51f10934d76c59072
jill zhang <talktojillzhang@gmail.com>
2018-09-02 01:42:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
+index =0;
 function init() {
   // your code here
+  document.body.addEventListener('keydown',checkKonami)
+}
+
+function checkKonami(e){
+  const key = e.key;
+  console.log(key)
+  if (key === codes[index]){
+    index++
+    if (index === codes.length){
+      alert()
+      index =0
+    }
+  } else{
+    index =0 
+  }
 }
