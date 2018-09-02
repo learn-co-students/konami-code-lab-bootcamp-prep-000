a7e0554deda34d0de7ffd01a880c3c2f4a7f1641
Sasha Hart <sashanicolehart@gmail.com>
2018-09-02 05:06:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,33 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+let index = 0;
+
+function triggerKeyDown(key) {
+  const keyboardEvent = new KeyboardEvent("keydown", { key });
+  document.body.dispatchEvent(keyboardEvent);
 }
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key = codes[index]) {
+    index++;
+
+    if (index === codes.length) {
+      alert("Congratulations");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+function init() {
+  const body = document.body;
+
+  body.addEventListener('keydown', onKeyDownHandler);
+}
+
+init();
