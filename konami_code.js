4d6e46e6e5351fed861d008d0829e1fc43fe5573
Matthew Etress <mattetress@me.com>
2018-09-04 21:33:24 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+
+      if (index === codes.length) {
+        alert("Perfect!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
