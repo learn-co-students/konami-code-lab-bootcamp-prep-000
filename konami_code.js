c9911d40967d06547a3ffb564f983bacea12fec7
Austin  Burke <aburk3@gmail.com>
2018-09-04 00:26:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
+function onKeyyDownHandler(e) {
+  const key = e.key
+  
+  if (key === codes[index]) {
+      index++
+      
+      if (index === code.length) {
+        alert("Hurray!")
+        
+        index = 0
+      }
+  }   else {
+    index = 0
+  }
+}
+
 function init() {
   // your code here
 }
