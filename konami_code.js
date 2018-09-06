78af85b24ba5b3e894199cbdea293729cf777a02
Joon Jang <joon.jang95@gmail.com>
2018-09-06 20:39:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,30 +11,37 @@ const codes = [
   "a"
 ];
 
- const body = document.querySelector('body')
 
-function init() {
+
+function init() {  
   
+ const body = document.querySelector('body')
+ var index = 0
+ 
  body.addEventListener('keydown', function(event) {
-  var index = 0
-  
-  function konamiCode(e) {
-    const key = e.key;
+    
+    const key = event.key;
+    console.log(key);
+    
     
     if (key === codes[index]) {
       index++;
-    
-    if (index === codes.length) {
-      alert("Hurray!");
-      
-      index = 0;
-      }
+  
+       if (index === codes.length) {
+         alert("Hurray!");
+         index = 0; 
+       }
     } else {
-      index = 0;
+       index = 0;
     }
-  }
- })
-} 
+  })
+}
+ 
+
+ 
+
+  
+   
  
 
 
