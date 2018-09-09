c87998091e8e395ba7107016c9331cc761698f4e
yoan ante <yoanante@gmail.com>
2018-09-09 20:01:25 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,29 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+
+// Keep track of index outside of the event handler.
+let index = 0;
+   document.addEventListener('keydown', function(e){
+        console.log("key  pressed = " + e.key)
+       const key = e.key;
+   
+    if (key === codes[index]) {
+      console.log("index =: " + index + "key =: "+key + " codes[index] =:" + codes[index])
+      index++;
+      if (index === codes.length) {
+        console.log("This logs!")
+        alert("Hurray!"); //alert doesn't show up.
+        
+   
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    })
 }
+
+init();
