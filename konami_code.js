9ed01ec0f93b2b9f383eaea89c1627caa1d0ee39
Jae Park <wjp241@nyu.edu>
2018-09-09 20:53:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+  let index = 0
+  document.body.addEventListener('keydown', function (e){ 
+  const key = e.key;
+  console.log(key);
+  if (key === codes[index]) {
+    index++;
+    console.log(index);
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+  })
 }
+
+
+ 
+
+
