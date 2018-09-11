4920bc6edb128b1ab2ce2200591e357c0c25977b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-11 01:54:47 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,34 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-  document.body.addEventListener('keydown', onKeyDownHandler) //{
-  //alert('Congratulations!')
+  document.body.addEventListener('keydown', onKeyDownHandler)
 }
 
-
-//You'll want to attach an event listener to document.body and check for 'keydown' events. If the user enters this special code, pressing all ten of the keys in the correct order, alert() a congratulatory message. However, if they press a key out of sequence or a key that isn't part of the Konami code, don't alert() anything and simply keep listening for all ten keydowns in the correct order.
-//When you're testing the code out in the browser, be sure to call init() to attach the event listener and set everything up!
-
-
-
-//Stuck on how to get started? Here's a contrived, short example:
-// Keys for A, B, and C keys.
-//const alphabet = ['a', 'b', 'c'];
- 
-  // Keep track of index outside of the event handler.
 let index = 0;
  
-// This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = e.key;
- 
   if (key === codes[index]) {
     index++;
- 
     if (index === codes.length) {
       alert("Hurray!");
- 
       index = 0;
     }
   } else {
