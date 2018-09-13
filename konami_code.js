14199b5de2068c934cc57fb21817a2c954adf315
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-13 17:10:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,46 @@
 const codes = [
   "ArrowUp",
+  
   "ArrowUp",
+  
   "ArrowDown",
+  
   "ArrowDown",
+  
   "ArrowLeft",
+  
   "ArrowRight",
+  
   "ArrowLeft",
+  
   "ArrowRight",
+  
   "b",
+  
   "a"
+  
 ];
 
-function init() {
-  // your code here
-}
+  function init(){
+    
+var index = 0;
+document.body.addEventListener('keydown',(event) => {
+  
+  var key = event.key;
+    
+    if (key === codes[index]){
+      index ++;
+      
+      if(index === codes.length) {
+        alert( 'Yay you did it !!!');
+        index = 0;
+      }
+    } else {   
+      index = 0;
+      
+    }
+} )
+  
+  }    
+  
+
