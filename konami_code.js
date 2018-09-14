010f52de26cee3dc6ddb9875ec39e1eb6c1ae36d
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-14 13:34:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,25 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var index = 0;
+  var ckey;
+  document.body.addEventListener('keydown', function (keey) {
+        ckey=keey.key;
+        console.log(ckey);
+
+        if (index < codes.length ) {
+          if(ckey == codes[index]) {
+            index++;
+          }
+          else index = 0;
+        }
+
+        if (index == codes.length) {
+          window.alert();
+          index = 0;
+          }
+
+  })
 }
+
+init();
