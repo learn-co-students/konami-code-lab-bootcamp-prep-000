730b2c246e57646467ecd82e35ef46cede1224a7
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-09-17 14:50:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,20 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  
+   let count = 0;
+   document.body.addEventListener('keydown', (event) => {
+    const key = event.key;
+   
+    count = (codes[count] === key) ? ++count : 0 
+   
+      if (count === codes.length) {
+        alert("Hurray!");
+        count = 0;
+    }
+    
+  
+});
 }
+
+//init();
