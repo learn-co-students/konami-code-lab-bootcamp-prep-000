667542c8744165d26ada29a1bc3c9182edc7b406
sarahcalande <scalande@yahoo.com>
2018-09-18 17:17:45 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,16 @@ const codes = [
 ];
 
 function init() {
+  let index= 0
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+
+    index = (codes[index] === key) ?++ index :0
+
+    if (index === codes.length){
+      window.alert("Hurray!");
+      index = 0
+    }
+  });
   // your code here
 }
