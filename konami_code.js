b8cb1d5e0a46ec19504ed9d1a95bd1da6f9bc77b
Matteo Muscas <thatman.j@gmail.com>
2018-09-21 10:45:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+ 
+ const main = document.body;
+ 
+  main.addEventListener('keydown', function(event) {
+  const key = event.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+});
 }
