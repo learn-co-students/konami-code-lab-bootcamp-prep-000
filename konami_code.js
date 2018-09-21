db77dd89a4bf00d5d5b6fe71ff1860673cd22f4b
Laura Felce <crepu83@yahoo.it>
2018-09-21 10:45:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init () {
+const main = document.body;
+let index = 0;
+main.addEventListener('keydown', function(event) {
+  const key = event.key;
+
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Bravo!");
+ 
+      index = 0;
+    }
+  } else {
+  index = 0;
+  }
+} );
 }
