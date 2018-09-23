06179cc6db195ee93173ac0a3fceed17a061ace9
Ezra T Huffman <joshooha@gmail.com>
2018-09-23 16:50:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', function (e) {
+   const key = e.key;
+   if (key === codes[index]){
+     index++;
+     
+     if (index === codes.length) {
+       alert("you did it")
+     }
+   } else {
+     index = 0;
+   }
+  })
 }
