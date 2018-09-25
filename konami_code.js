a3da6e4372feac478d2d7df754b1d6407b3740f6
Diego Vasquez <vasquezd_cis68@yahoo.com>
2018-09-25 05:56:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,19 +12,17 @@ const codes = [
 ];
 
 function init() {
-  let index = 0;
-  
-  document.body.addEventListener("keydown", function(e) {
-   if (e.key === codes[index]) {
-     index++;
-     
-     if (index === codes.length) {
-     alert('hurray');
-     index = 0;
-   }
-   }
-    else {
-     index = 0;
-   }
+  let idx = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    idx = (codes[idx] === key) ? ++idx : 0
+    
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+    
   });
-  }
+}
