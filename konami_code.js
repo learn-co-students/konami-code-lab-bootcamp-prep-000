b33d0a34531e8e2c6bc3091585eaf221653c2f92
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-26 20:14:56 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,16 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init(){
+  documentBody = document.body;
+  let index = 0;
+  documentBody.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert("Congratulations!");
+      }
+    }
+  });
 }
