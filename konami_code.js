b30d47285684c310d77c3d5d1208774821ca569c
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 20:41:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,33 @@ const codes = [
   "a"
 ];
 
+var values = {
+  "ArrowUp": 38,
+  "ArrowDown": 40,
+  "ArrowLeft": 37,
+  "ArrowRight": 39,
+  "a": 65,
+  "b": 66
+}
+
 function init() {
   // your code here
+  body = document.querySelector('body')
+ 
+ var index = 0;
+  body.addEventListener('keydown', function(e) {
+    if(values[codes[index]] === e.which){
+      index++;
+      
+      if(index === codes.length){
+        alert("You did it!!");
+        index = 0;
+      }
+    }
+    else{
+      index = 0;
+    }
+  });
 }
+
+init();
