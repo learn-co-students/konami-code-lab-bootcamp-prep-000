8bd12ae44c69ee86e4e2f6b7d91486b3caffe6e2
Johnathon Wood <jaywood128@gmail.com>
2018-10-02 16:32:16 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
-}
+  document.body.addEventListener('keydown', function (event) {
+    const key = event.key;
+    console.log(event.key);
+  if (key === codes[index]) {
+      alert("You cheated!")
+    index++;
+  }
+      if (index ===codes.length) {
+
+          index = 0;
+   }
+
+ else {
+             index = 0;
+   }
+})}
+    init()
