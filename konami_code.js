58f06068efa6cf29167884a232ba3fd51e219104
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-04 19:38:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  var typed = []
+  document.body.addEventListener('keydown', (event) => {
+    // add key to current sequence
+    let key = event.key
+    typed.push(key)
+    console.log(typed)
+    for (var i = 0; i < typed.length; i++) {
+      if (typed[i] !== codes[i]) {
+        typed = []
+      }
+      if (typed.length && typed.length === codes.length) {
+        alert('Konamiiiiied!')
+        return
+      }
+    }
+
+    // iterate over sequence
+    // if any element doesn't match, reset sequence
+    // if we get to the end of the sequence (if the sequences are equal in length) then send an alert
+  })
 }
