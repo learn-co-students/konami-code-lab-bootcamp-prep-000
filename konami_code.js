4b24e8444a636c8012cb6a586924bf6834e33a15
devinkapelanski <devinkapelanski@yahoo.com>
2018-10-06 11:24:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  var index = 0;
+  
+  document.body.addEventListener('keydown', onKeyDownHandler);
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
+
+ 
