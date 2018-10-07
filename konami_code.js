1396dc9cfaff6dfaa15a02763e0080ce09a9130f
severus889700 <severus889700@gmail.com>
2018-10-07 03:38:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,14 +2,17 @@ const code = [ "38", "38", "40", "40", "37", "39", "37", "39", "66", "65"];
 function init(){
   var index = 0
    document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which)
+    const key = parseInt(e.which)
      if (code[index] === key) {
       index++
        if (index === code.length - 1) {
         alert('congraduation')
         index=0;
-       }
-            } else {
-              index = 0;
-            }})
+      }
+      else{
+      index=0;
+      }
+    } else {
+      index=0;
     }
+  })}
