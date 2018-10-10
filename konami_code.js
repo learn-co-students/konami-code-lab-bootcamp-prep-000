12949d81cb1d1e972f1151c0da6b09909dbaacdb
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-10 17:15:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+   // your code herelet index = 0; 
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key; 
+    if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  //  alert("Wrong Codes!")
+  }
+  })
 }
