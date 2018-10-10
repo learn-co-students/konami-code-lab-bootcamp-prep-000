fb41624518148fedebb87533ab7f0aa3dbce14f4
Joe Quattrone <joequattrone5@gmail.com>
2018-10-10 16:00:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,45 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+//const codes = [
+  //"ArrowUp",
+  //"ArrowUp",
+  //"ArrowDown",
+  //"ArrowDown",
+  //"ArrowLeft",
+  //"ArrowRight",
+  //"ArrowLeft",
+  //"ArrowRight",
+  //"b",
+  //"a"
+//];
 
 function init() {
-  // your code here
+  console.log("test")
+  //var body = document.querySelector('body');
+  
+ const arrow = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a']
+
+ var index = 0;
+
+ 
+  
+  document.body.addEventListener('keydown', (e) => {
+ const key = e.key;
+ 
+ console.log("key", key)
+ console.log("arrow", arrow[index])
+  if (key === arrow[index]) {
+   index++;
+  
+   if (index === arrow.length) {
+     alert("Hurray!")
+     
+     index = 0;
+   }
+  }   else {
+       index = 0;
+     } 
+ }); 
+  
 }
+
+
+
