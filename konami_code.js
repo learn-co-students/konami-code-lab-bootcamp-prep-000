dc294128a0e146da74104c53018085a41b90e545
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-12 14:56:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,22 +11,30 @@ const codes = [
   "a"
 ];
 
+//we dont accept the event inside this function
 function init() {
   
   let index = 0;
   
-  document.body.addEventListener('keydown',(e)=>{
-    const key=e.key;
+  //move the event listener into here and this is where we pass the event inside 
   
-    if(key===codes[index]){
-      index++;
-        if(index===codes.length){
-          alert("Congrats!");
-          
-          index = 0;
-        }
-    } else{
-      index=0;
+  document.body.addEventListener('keydown', (e) =>{
+  
+  const key=e.key;
+  
+  if(key===codes[index]){
+    index++;
+    
+    if(index===codes.length){
+      alert("Congrats!");
+      
+      index = 0;
     }
-  })
+  }else{
+    index=0;
+  }
+    
+    })//close the listener down here 
+    
+    
 }
