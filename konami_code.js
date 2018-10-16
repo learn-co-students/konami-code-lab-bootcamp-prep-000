5192d97e7314684e4a3d4f73856a97f4de909a18
Jonathan Chan <jonchan816@gmail.com>
2018-10-16 21:39:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,43 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+const bodyPress = document.getElementById('body');
+
+
 function init() {
   // your code here
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === 10) {
+        alert("CHEAT CODE ENABLED!");
+        
+        index = 0;
+      }
+      
+    } else {
+      index = 0;
+    }
+  }
 }
+
+document.body.addEventListener('keydown',   function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === 10) {
+        alert("CHEAT CODE ENABLED!");
+        
+        index = 0;
+      }
+      
+    } else {
+      index = 0;
+    }
+  });
