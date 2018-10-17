232dd957c0efa615abe737d474335cc908614ad1
janerishbaluca <janerishbaluca@gmail.com>
2018-10-17 22:59:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index=0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key===codes[index]) {
+      index++;
+      if (index===codes.length) {
+        alert('Hurray!');
+        index=0;
+      }
+    } else {
+      index=0;
+    }
+  })
 }
