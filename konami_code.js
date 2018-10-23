433bf17ec9da11a867e8f652d33f11dc5403ef75
Michael Colgan <michaelwcolgan@gmail.com>
2018-10-23 01:19:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,4 @@
+
 const codes = [
   "ArrowUp",
   "ArrowUp",
@@ -11,6 +12,32 @@ const codes = [
   "a"
 ];
 
+document.addEventListener('keydown', onKeyDownHandler, true);
+
+let index = 0;
+
 function init() {
-  // your code here
+
+}
+
+
+function onKeyDownHandler(e)
+{
+  init()
+  const key = e.key;
+
+  if(key === codes[index])
+  {
+    index ++;
+
+    if(index === codes.length){
+
+      alert("you found the code!");
+      index = 0
+    }
+  }
+  else
+  {
+    index = 0;
+  }
 }
