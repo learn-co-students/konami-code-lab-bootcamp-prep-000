388f5afb32d95d0a80c1aaeb13b92562bb4b5e78
David Choi <daaave@gmail.com>
2018-10-24 03:51:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+
+let codeIndex = 0;
+
+function onKeyDownHandler(e){
+  let key = e.key;
+  
+  let targetKey = codes[codeIndex];
+  if(key === targetKey){
+    codeIndex++;
+    if(codeIndex === codes.length){
+      alert("Hurray!");
+      codeIndex = 0;
+    }
+  }
+  else{
+    codeIndex = 0;
+  }
+}
+
 function init() {
   // your code here
+  document.body.onkeydown = onKeyDownHandler;
 }
