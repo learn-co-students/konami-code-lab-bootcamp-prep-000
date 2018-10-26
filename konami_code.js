a64b0bda06d6451d65f7ca94d546b66e23b137e0
Lucy Suddenly <suddenlylucy@gmail.com>
2018-10-26 22:44:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+
+let index = 0;
+
 function init() {
-  // your code here
+ document.body.addEventListener('keydown', function(e){
+ let key = e.key
+ if(key === codes[index]) { 
+   index++ 
+   if(index === codes.length){
+     alert("Video games woo!")
+     
+     index = 0
+   }
+ } else {
+   index = 0
+ }});
 }
