11eeb169e61a5786a220abedc92dc8259270660d
Jarrod Dyer <jarrod@gasganno.com>
2018-10-29 20:41:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  const main = document.querySelector('body');
+
+  main.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+        index++;
+        if (index === codes.length) {
+            alert("You did it!");
+            index = 0;
+        }
+    } else {
+        index = 0;
+    }
+})
+
 }
