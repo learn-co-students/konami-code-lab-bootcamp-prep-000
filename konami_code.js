2d04db635216efb29ac479a5bfca4b1770a02a9d
Jarrod Dyer <jarrod@gasganno.com>
2018-10-29 20:45:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -30,3 +30,22 @@ function init() {
 })
 
 }
+
+/*OFFICIAL SOLUTION
+function init() {
+  let idx = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+
+    idx = (codes[idx] === key) ? ++idx : 0
+
+
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+
+  });
+}
+*/
