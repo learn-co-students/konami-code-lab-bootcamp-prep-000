8d441c00d08bd148c57804a2916052b658568d46
Spike Burton <ralphburton3@gmail.com>
2018-10-30 07:14:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+var i = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(e) {
+    if(e.key === codes[i]) {
+      i++;
+      if(i === codes.length) {
+        alert('Congratulations!');
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  })
 }
