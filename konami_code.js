9739f217d87175cc0e8f277582134371410ba4ae
Brian <briandsalemi@gmail.com>
2018-11-02 00:05:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+const body = document.body;
+
+let i = 0;
+
+  function init() {
+    body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[i]) {
+      i++;
+      if (i === codes.length) {
+        alert("Hurray!");
+        i = 0;
+      }
+    } else {
+      i = 0; 
+    }
+  });
+  }
+  
