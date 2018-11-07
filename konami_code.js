85afb2a74049bc3e331587c44105f398b4463502
eric-klimowich <eklimowich@hotmail.com>
2018-11-07 13:52:38 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+console.log(index);
+  document.body.addEventListener("keydown", function(event) {
+    const pressedKey = event.key;
+
+    if (pressedKey === codes[index]) {
+      ++index;
+
+      if (index === codes.length) {
+        window.alert("CONTRA!!!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
