ded4bafff17682ea2a87164ca3a7bf0a343fe040
Edwin Lin <edwinlin@gmail.com>
2018-11-07 07:38:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,30 @@ const codes = [
 
 function init() {
   // your code here
+  var body = document.body
+  
+  let counter = 0
+
+  body.addEventListener('keydown', function(e){
+    console.log(e.key)
+
+      const key = e.key;
+
+      if(key === codes[counter]){
+        counter += 1
+        console.log(counter)
+
+      }else{
+        counter = 0
+        console.log('back to 0')
+      }
+
+      if(counter === codes.length){
+        counter = 0
+        alert("Hurray!")
+        console.log("Hurray!")
+      }
+    
+  });
+  
 }
