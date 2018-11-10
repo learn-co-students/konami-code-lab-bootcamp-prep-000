7d3305371d45fc0efc1df862dfea18ef5082eaa0
Nick Smith <smthnck@gmail.com>
2018-11-10 22:43:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+let index = 0;
 function init() {
-  // your code here
+  document.body.addEventListener('KeyboardEvent',keycodes)
+}
+
+function keyCodes(e){
+  const key=e.key;
+  if (key === codes[index]) {
+    index++;
+  } 
+  if (index === codes.length){
+    alert('Hooray!');
+    index = 0;
+  } else {
+    index = 0;
+  }
 }
