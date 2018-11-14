fc4ecf73535f65f766e798ab9610d95c82dca7a2
Menachem Aber <abermendel@gmail.com>
2018-11-14 19:39:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,17 @@ const codes = [
 
 function init() {
   // your code here
+  var index = 0;
+  function onKeyDownHandler(e) {
+    if (e.key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+    alert('Great!');
+    index = 0; }
+    } else {
+    index = 0;
+    }
+  };
+  document.body.addEventListener('keydown', onKeyDownHandler);
+  return onKeyDownHandler(e);
 }
