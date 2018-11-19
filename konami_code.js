17e2cec288fd9d11616bb75ad7fb65ac7dcb19b9
Stuart Sewell <sewelly@gmail.com>
2018-11-19 20:24:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+  let index = 0
+
+  document.body.addEventListener("keydown", function(e){
+    const key = e.key
+
+    if(codes[index] === key){
+      ++index
+    } else {
+      index = 0
+    }
+
+    if(index === codes.length){
+      window.alert("Hurray!");
+      index = 0
+    }
+  })
 }
