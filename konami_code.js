1dfaba5ba26ff14df7758378dad180a4c13d208a
ali.mohammed200 <ali.mohammed200@gmail.com>
2018-11-19 09:11:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,29 @@ const codes = [
   "b",
   "a"
 ];
+var newDiv = document.createElement("div");
+newDiv.style.backgroundColor = '#f9f9f9';
+newDiv.style.textAlign = 'center';
+document.body.appendChild(newDiv);
+let index = 0;
 
 function init() {
-  // your code here
+  document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    //by adding the following line, any key pressed is added to the div
+    //newDiv.innerHTML += keyName;
+    if (keyName === codes[index]) {
+      index++;
+      newDiv.innerHTML += `KC CODE`;
+      if (index === codes.length) {
+        alert("Hurray Konami Code Activated!");
+        index = 0;
+        newDiv.innerHTML = "";
+      }
+    } else {
+      index = 0;
+      newDiv.innerHTML = "";
+    }
+  });
 }
+init();
