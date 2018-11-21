ca7ad0cff79b66d222d994faf6fc835aced7887e
Eugenia <eugenia.kaloshina@gmail.com>
2018-11-21 01:19:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,19 @@ const codes = [
   "b",
   "a"
 ];
-
+ 
 function init() {
-  // your code here
+    let index = 0 
+   document.body.addEventListener('keydown', function(e) {
+    const key=e.key
+     if (key===codes[index]) {
+     index++
+     if (index===codes.length)  { 
+       index=0
+       alert('Hurray!')
+      } 
+   } else {
+     index=0  
+   } 
+ } )
 }
