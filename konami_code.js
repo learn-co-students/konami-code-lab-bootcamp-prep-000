b0aee52b910f39741ffad3c2f252c29e832413e3
Bianca Zanardi <bianca.zanardi@gmail.com>
2018-11-25 11:05:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+// Keep track of input outside of the event handler.
+const array = [];
+
+
+document.querySelector('body').addEventListener('keydown', onKeyDownHandler);
+
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  array.push(e.key);
+  if (array.length > 10) {
+    array.shift()
+  }
+  if (JSON.stringify(array) === JSON.stringify(codes)) {
+    alert("Hurray!");
+  }
 }
