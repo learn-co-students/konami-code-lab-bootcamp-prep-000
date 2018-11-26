acfc1ef8aaa48c8d9de8fc938cd9eb3b8123bba6
FX Wood <fxwood2@gmail.com>
2018-11-26 07:33:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,32 @@ const codes = [
   "a"
 ];
 
+let index = 0
+let ignorer = 0
+
 function init() {
-  // your code here
+  const outputTarget = document.querySelector('#output-pane')
+
+  function konamiKeyHandler(e) {
+    if (ignorer%2 != 0) {
+      ignorer++
+      outputTarget.innerHTML = `${e.key}`;
+      console.log(e.key)
+      if (codes[index] === e.key) {
+        index++
+        if (index === 10){
+          alert("It's an older code, sir-- but it checks out.")
+        }
+      } else {
+        index = 0
+      }
+    } else {
+      console.log("I'm ignoring you.")
+      ignorer++
+      console.log(ignorer%2)
+    }
+  }
+  document.body.addEventListener('keydown', konamiKeyHandler)
 }
+
+init()
