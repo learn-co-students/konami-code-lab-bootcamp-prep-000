a73eadab92f031cfa2adb0977d7a3f8e85fc8996
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-11 03:53:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,31 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+  // document.body.addEventListener('keydown', function(e){
+  //   console.log(e.which)
+  // });
+ 
+  // Keep track of index outside of the event handler.
+  let index = 0;
+   
+  // This is the function that would be invoked by the event listener.
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+   
+      if (index === codes.length) {
+        alert("You Entered the right codes")
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+
