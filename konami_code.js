8f1c7371c4a4543648768ac8e551a2842e11b665
Michael Moravetz <moravemv@gmail.com>
2018-12-14 04:48:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+const konami = document.body
+
 function init() {
-  // your code here
+  
+  konami.addEventListener('keydown', function(e) {
+  
+    var key = code [e.keycode];
+  
+    var requiredkey = code [index];
+    
+    if (key === requiredkey) { 
+      index++;
+    
+    if (index === code.length) { 
+      alert("Hurray! You get 30 lives!") ;
+      index = 0;
+      }
+    } else { 
+      index = 0;
+    }
+  })
 }
