8d90e573d9b9327dc013f297886ff8640b4c7004
jameseclement <jameseclement@gmail.com>
2018-12-18 16:07:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,16 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener('keydown', function onKeyDownHandler(e){
+    const key = e.key;
+    if (key === codes[index]){
+      index++; 
+      if (index === codes.length){
+        alert("Way to go!")}
+        else {index = 0}
+      }
+  }
+)
 }
+
