4ef59b08d2a34dd2118d9d3aab25ec9dc7d48bb2
Maximillian Schulte <maximilliangschulte@gmail.com>
2018-12-18 19:18:51 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  let counter = 0
+  let count = document.createElement("div")
+  count.innerHTML = toString(0)
+  document.querySelector('body').appendChild(count);
+  document.querySelector('body').addEventListener('keydown', function(e){
+    if (e.key === codes[counter]){
+      counter ++
+    }else{
+      counter = 0
+    }
+    if (counter == codes.length){
+      alert("acheivement unlocked")
+    }
+    count.innerHTML = toString(counter)
+  })
 }
