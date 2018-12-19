e19c66a635467039cba64c1edc5486ed8a158e1e
eselbeus <eselbeus@gmail.com>
2018-12-19 23:58:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const body = document.getElementById('body');
+  let i = 0;
+  body.addEventListener('keydown', function(event){
+    const key = event.key;
+    if (key === codes[i]){
+      i++;
+      if (index === codes.length){
+        alert("Yay!");
+      }
+    }
+    else {
+      i = 0;
+    }
+  });
 }
