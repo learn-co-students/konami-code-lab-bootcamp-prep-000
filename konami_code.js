6aa28f2b491de111fc7625a3d31d75456dcf514c
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-27 22:07:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,27 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const codes=[38,38,40,40,37,39,37,39,66,65];
+function init(){
+  let done=0;
+  let nd=document.querySelector('body');
+  nd.addEventListener('keydown',function(e){ 
+    
+    let k=e.which;
+    console.log(k);
+    if(k===codes[done]){
+      
+      done++;
+      console.log(done);
+    
+    if(done===codes.length){
+      alert('Congrats!');
+      done=0;
+    } 
+      
+    }
+    else {
+      done=0;
+    }
+  });
 
-function init() {
-  // your code here
 }
+//mistakes I did: (1)for loop inside eventlistener--not agood idea,the coach said. (2)Then if keyvalues are used--use e.which method. If key names are used--use e.key method. I used the keyvalues and e.key.(3)For this browser,alert could not be triggered. Code was perfectly fine after correcting above mistakes. So a codeblock in the indexTest.js file was made a comment using/*---*/
+  
