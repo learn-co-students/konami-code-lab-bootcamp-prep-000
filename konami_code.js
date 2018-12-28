7b0693dc557481182936fd3ecd04b9879a628e67
kovner chalumo <kchalumo@gmail.com>
2018-12-28 03:12:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  index = 0;
+  const body = document.getElementsByTagName("body")[0];
+  body.addEventListener('keydown', function onKeyDownHandler(e){
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length){
+        alert("HURRAY");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    
+  } )
 }
