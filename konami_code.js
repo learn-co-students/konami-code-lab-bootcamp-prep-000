9594bdbce5349429d21cd2fe53ab86b7235e25d6
Roundabout Rider <ntm.email@icloud.com>
2018-12-31 12:58:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,21 @@ const codes = [
   "b",
   "a"
 ];
-
-function init() {
-  // your code here
 }
+
+  var counter = 0;
+	  const key = document.addEventListener('keydown', function(e) {
+	    const key2 = e.key;
+	    //debugger;
+	      if (key2 === codes[counter]) {
+	        counter++;
+	        if (counter === codes.length) {
+	          window.alert("Congratulations, you have correctly inputed the cheat code!");
+	          return;
+	        }
+	      }
+	      else {
+	        counter = 0;
+	      }
+	  });
+	}
