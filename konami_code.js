ca348eebb30c003ea4ab5ad62ae0a39d3d56f4e8
shawn a meighn <smeighn@gmail.com>
2019-01-01 21:45:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const codes = [
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   "ArrowUp",
   "ArrowUp",
   "ArrowDown",
@@ -9,34 +9,20 @@ const codes = [
   "ArrowRight",
   "b",
   "a"
-];
-
+let index=0;
 function init() {
-  // your code here
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-init();
-
-function init() {
-  document.body.addEventListener('keydown',onKeyDownHandler);
-   
-  // keep track of index outside of the event handler
-  var index = 0
-   
-  // unattached event handler
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which)
-   
-    if (key === code[index]) {
-      index++
-   
-      if (index === code.length - 1) {
-        alert("YOU DID IT!")
-        index = 0
+  window.addEventListener("keydown", function(event){
+    const key=parseInt(event.which);
+    console.log(event.which);
+    if(key===code[index]){
+      index++;
+      if(index===code.length){
+        window.alert("Cheat activated, you cheater")
       }
-    } else {
-      index = 0
     }
-  }
+    else{
+      index=0;
+    }
+  })
 }
-}
+init();
