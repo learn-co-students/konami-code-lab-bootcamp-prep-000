ed4362a566ef5326705c72f84508c51fa6ba744b
Nicholas Stano <nicholasstano@gmail.com>
2019-01-03 19:41:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,23 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+  "ArrowUp",  "ArrowUp",  "ArrowDown",  "ArrowDown",  "ArrowLeft",
+  "ArrowRight",  "ArrowLeft",  "ArrowRight",  "b",  "a"];
+
+
 
 function init() {
-  // your code here
+{
+  let index = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    index = (codes[index] === key) ? ++index : 0
+    
+    if (index === codes.length) {
+      window.alert("Hurray!");
+      index = 0
+    }
+    
+  });
+}
 }
