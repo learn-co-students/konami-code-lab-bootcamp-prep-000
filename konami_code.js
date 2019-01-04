710607fb739407262cea5dca11756111be12b4a3
Aneesh Venuturumilli <veeaneesh@gmail.com>
2019-01-04 00:37:58 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,20 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  document.body.addEventListener('keydown',function(event){
+    const key = event.key;
+    if(key === codes[index]){
+      index++;
+    
+      if(index === codes.length){
+        alert("Finished the code!");
+        index = 0;
+      }
+    }else{
+      index = 0;
+    }
+    
+  });
+
 }
