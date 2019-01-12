35a69c0ba8454d7bcfa7adca0c3c61cf7fedb5b9
Sean F <sean.flanagan@flatironschool.com>
2019-01-12 22:43:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -66,4 +66,23 @@ function init() {
 
 init();
 
+ init();
 
+    function init() {
+
+        var index = 0;
+        const getElement = document.querySelector('body');
+
+        getElement.addEventListener('keydown', function(e) {
+            if (code[index] === e.which || code[index] === e.detail) {
+                index++;
+
+                if (index === code.length) {
+                    alert("CHEAT EXECUTED!!");
+                    index = 0;
+                }
+            } else {
+                index = 0;
+            }
+        })
+    }
