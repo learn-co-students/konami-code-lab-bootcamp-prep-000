af7297c826889dff727aa57ca94b2ebd5ce5d71c
Paris Dolfman <parisdolfman@gmail.com>
2019-01-16 20:04:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+ var index = 0;
+
 function init() {
-  // your code here
+  document.addEventListener('keydown', function(e) {
+    var key = e.key;
+    if (key === codes[index]) {
+      index++;
+      if (index === alphabet.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
