9e6d245958061ebd7c8ca1160d572e5b161a35f9
Peter Choi <jc4883@columbia.edu>
2019-01-22 15:29:36 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+function onKeyDownHandler(e){
+  const key = e.key
+  console.log(key)
+  if (key === codes[index]) {
+    index++
+    if (index === codes.length) {
+      console.log("Hurray!")
+      alert ("Hurray!")
+      index = 0
+    }
+  }
+  else {
+    index = 0;
+  }
+}
+
 function init() {
   // your code here
+  alert("hello!")
+  document.body.addEventListener('keydown', onKeyDownHandler, true)
 }
