783c7fd3db090bb3e84f148a3df0e066ffc5d1cf
Dennis Chung <chungdennis6@gmail.com>
2019-01-24 03:11:43 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,28 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
-
+ const codes = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
 function init() {
-  // your code here
-}
+  let idx = 0
+
+
+ } 	  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+
+     idx = (codes[idx] === key) ? ++idx : 0;
+
+     if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0;
+    }
+
+   }
+};
