aa431a2cb587a6cca4e15775ce7e61debbdb3866
github username <github email address>
2019-01-29 18:18:45 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,24 @@ const codes = [
 
 function init() {
   // your code here
+  const body = document.querySelector("body");
+  let index = 0;
+  let code = [];
+  console.log("in init");
+
+  body.addEventListener("keydown", function(e) {
+    let key = e.key;
+    if (key === codes[index]) {
+      code.push(e.key);
+      index++;
+      console.log(code);
+      if (code.length === codes.length) {
+        alert("KONAMI CODE");
+        console.log("ayyyyy")
+      }
+    } else {
+      index = 0;
+      code = [];
+    }
+  });
 }
