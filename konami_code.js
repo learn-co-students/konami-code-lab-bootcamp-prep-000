871e8124b49b920f6808ae1735e5ff9c8375e3ac
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-30 01:30:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,31 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  
+  let index = 0;
+
+document.body.addEventListener('keydown',function(event){
+  const key = event.key;
+  
+ if(codes[index] === key){
+    index++;
+    } else {
+    index = 0;
+    }
+  if(index === codes.length){
+   window.alert("Konami code entered");
+    index = 0;
+
+    }
+  });
 }
+
+
+
+
+
+
+
