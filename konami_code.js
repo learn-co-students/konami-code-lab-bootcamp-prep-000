cda78cf1c1b28918fdd80f6aea1bb4b75c93457a
Jason Velez <jvelez20@yahoo.com>
2019-01-30 18:35:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,21 @@ const codes = [
 function init() {
   // your code here
 }
+
+const team = ['Celtics', 'Lakers', 'Knicks'];
+let index = 0;
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
