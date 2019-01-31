6d48134f2887ca8d852a334b983f70ecc45773a2
aberm <abermendel@gmail.com>
2019-01-31 11:58:08 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,17 +13,16 @@ const codes = [
 
 function init() {
   // your code here
-  var index = 0;
+  let index = 0;
   function onKeyDownHandler(e) {
     if (e.key === codes[index]) {
     index++;
     if (index === codes.length) {
-    alert('Great!');
-    index = 0; }
+      alert('Great!');
+    }
     } else {
     index = 0;
     }
   };
   document.body.addEventListener('keydown', onKeyDownHandler);
-  return onKeyDownHandler(e);
 }
