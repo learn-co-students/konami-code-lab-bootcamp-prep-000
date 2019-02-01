d05d9ee613963058ca96d08b8879d3e5b04d4198
kendall willard <kendallwillard@hotmail.com>
2019-02-01 21:10:31 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,32 @@ const codes = [
   "a"
 ];
 
+
 function init() {
   // your code here
+
+ var ndx = 0;
+ 
+ document.body.addEventListener('keydown', onKeyDownHandler);
+ 
+ function onKeyDownHandler(e) {
+   const key = e.key;
+   console.log('key:', key)
+   if( key === codes[ndx]) {
+     ndx++;
+     
+     // Found code
+     if( ndx === codes.length) {
+      alert("Hurray!");
+       ndx = 0;
+     }
+     
+   }
+   else {
+     ndx = 0;
+   }
+ }
+  
 }
+
+
