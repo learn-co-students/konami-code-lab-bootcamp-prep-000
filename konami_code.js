cd67eb419a806925ecf810c4cd5da50071866b67
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-03 20:11:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  const body = document.body
+  var index = 0
+  body.addEventListener('keydown', function(e) {
+    if(e.key === codes[index]){
+      window.alert("Hurray!");
+      index++
+      console.log(index)
+      console.log(e.key)
+      if(index === codes.length){
+        console.log(index)
+        window.alert("Hurray!");
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  });
 }
