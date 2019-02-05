f0ee83c9322cbd3e581854eec261372d0350f417
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-05 20:45:13 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
+window.onload = init;
+
 function init() {
-  // your code here
+  let access = 0;
+  document.body.addEventListener('keydown', function(e){
+    if(e.key == codes[access]){
+      access++;
+    }
+    else {
+      access = 0;
+    }
+    
+    if(access == 10){
+      alert("Hurray!");
+      access = 0;
+    }
+  });
 }
