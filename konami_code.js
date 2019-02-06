2c2e35e9b6fb6e09cddabce5fe572cca5d173a83
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-02-06 02:47:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+//var index = 0;
 function init() {
-  // your code here
+
+  var index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e){
+    const key = e.key;
+    //console.log('key', key);
+
+    if (key === codes[index]){
+      index++;
+
+      if (index === codes.length){
+        alert('YOU DID IT!');
+        index = 0;
+      }
+    }else{
+      //alert('NOPE!');
+      index = 0;
+    }
+  }
 }
+//init();
