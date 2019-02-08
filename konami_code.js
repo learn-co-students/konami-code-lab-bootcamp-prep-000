2aca2025d15c7fae848e429a96483a3082a4fac2
Antonina Maj <antoninajanina@gmail.com>
2019-02-08 23:23:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  let index = 0;
+  
+  function KeyDown(e) {
+    if (e.key === codes[index]) {
+      index++
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+  
+  document.body.addEventListener('keydown', KeyDown);
+  
 }
