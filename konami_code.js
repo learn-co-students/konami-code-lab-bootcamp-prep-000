3c23dff633346f7440f76fa32f151f2fbc0406bd
Desalegn Berihun <desunew@gmail.com>
2019-02-11 03:04:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,20 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  // your code here	  
+  var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = e.key;
+
+     if (key === code[index]) {
+      index++;
+
+       if (index === code.length) {
+        window.alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
