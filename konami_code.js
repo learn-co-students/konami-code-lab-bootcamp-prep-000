9c74998408eb432eba299c42169cedb92c2b896f
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-02-12 18:05:52 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index=0;
+  document.body.addEventListener('keydown', myEventListener);
+  function myEventListener(e){
+    const key= e.key;
+    if (key=== codes[index]){
+      index++;
+      if (index===codes.length){
+        alert("Congratulations!");
+      }
+    }else{
+      index=0;
+    }
+  }
 }
