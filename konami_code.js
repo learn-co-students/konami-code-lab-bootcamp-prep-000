a3593ed2ff28ca6aa3d6792cec1ba96271c44618
kyle smith <kylemclellan@gmail.com>
2019-02-13 13:27:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,20 @@ const codes = [
 
 function init() {
   // your code here
+  var index = 0
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', function(e){
+    console.log(e.key)
+    if (e.key === codes[index]){
+      index++
+      console.log(index)
+      if (index === codes.length){
+        console.log('code matched')
+        alert('Success!');
+      }
+    } else {
+      index = 0
+    }
+  })
 }
+//init()
