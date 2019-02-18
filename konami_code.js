dcb5eca840589cf87f2ad3c957b053f1cdd92ec5
Cecelia Smith <ceceliacreates@gmail.com>
2019-02-18 02:27:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,22 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener("keydown", checkKey);
+  
+  function checkKey(e) {
+    const key = e.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        window.alert("Congrats!");
+        
+      }
+      else {
+        index = 0;
+      }
+    }
+  }
 }
