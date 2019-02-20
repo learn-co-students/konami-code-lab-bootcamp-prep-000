58b6b36a9cda98032c8df67090d3a2c5f34bc985
Mary Selig <seligmar@gmail.com>
2019-02-20 14:05:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;  
+  document.addEventListener('keydown', function(e) {     
+    const key = e.detail;
+  if (key === codes[index]) {
+    index++; 
+  
+    if (index === codes.length) {
+     
+      alert("Hurray!");
+
+      index = 0; 
+    }
+  }   
+ else index = 0} );    
 }
+
