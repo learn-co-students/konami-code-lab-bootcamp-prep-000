e588345d573c28d040ee7533d32d1fb582f89f91
Erini George <dr.erinigeorge@yahoo.com>
2019-03-04 18:27:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,29 @@ const codes = [
   "a"
 ];
 
-function init() {
+function init(e) {
   // your code here
+  
+  // console.log(`e = ${e.key} --- ${index} --- ${codes[index]}`);
+  
+  if (e.key === codes[index])
+  {
+    if (index < 9)
+    {
+      index ++;
+    }
+    else if (index == 9)
+    {
+      alert("HADOUKEN!!!");
+      index = 0;
+    }
+  }
+  else
+  {
+    index = 0;
+  }
 }
+
+var index = 0;
+
+document.body.addEventListener('keydown', init); 
