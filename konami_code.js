a69c0c49fda4d293677d83b8a63fe21fe4161bdd
David Cantoran <davidcantoran36@gmail.com>
2019-03-05 22:48:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+  document.body.addEventListener(`keydown`, function(e) {
+    const key = e.key;
+  
+    if (key === codes[index]) {
+      index++;
+      if (index === 10) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
