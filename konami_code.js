2d473b6439247939ccb1fa1f8a3f83cd81a68401
Henry Koehler <henryk1229@gmail.com>
2019-03-05 23:28:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init() { 
+  let index = 0;
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    if (key === codes[index]) {
+      index++;
+      
+    } else {
+      index = 0;
+    }
+    
+    if (index === codes.length) {
+      window.alert("Congrats!");
+      
+      index = 0;
+    }
+    
+  });
 }
