f4c63a7d46037b229cdd6ca7796df580ca0f893d
Aleksandar Gakovic <algakovic@gmail.com>
2019-03-05 00:41:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,25 @@ const codes = [
   "b",
   "a"
 ];
-
+let index = 0;
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(e){
+  const key = e.key;
+    console.log(e.key)
+  if (key === codes[index]) {
+    index++;
+      
+    if (index === codes.length) {
+    alert("Hurray!");
+        
+      index = 0;
+    }
+      
+    } else {
+      index = 0;
+    }
+  })
+   
+    
+    
 }
