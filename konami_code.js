01b38c517397e55adb4da70b5e5bdd83c21fb555
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-15 01:39:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,30 @@ const codes = [
 
 function init() {
   // your code here
+  var body = document.body;
+  var index = 0;
+  var codesTemp = codes;
+  
+  body.addEventListener('keydown', function(e)
+  {
+    const key = e.key;
+    console.log(`Last Key Pressed: ${key}, waiting for ${codesTemp[index]}`);
+    if(key == codesTemp[index])
+    {
+      if(index == codesTemp.length - 1)
+      {
+        index = 0;
+        alert('Well Played!');
+      }
+      else index++;
+    }
+    
+    else
+    {
+      index = 0;
+      console.log("RESET CODE");
+    }
+  });
+  
+  
 }
