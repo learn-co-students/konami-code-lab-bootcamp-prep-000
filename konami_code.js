274d2e6e7095cbb9c1392a61a0fc973456e3f39c
Olivia Kesler <oakesler@protonmail.ch>
2019-03-18 19:49:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init () {
+
+  let index = 0;
+ 
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+document.body.addEventListener('keydown', function(event) {onKeyDownHandler (event) 
+  });
 }
