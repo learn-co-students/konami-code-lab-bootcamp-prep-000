a601136cd7b13380237e63c4c9fc16aa0635f0d8
Jackson Prince <jdprince555@gmail.com>
2019-03-21 03:49:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+var i = 0;
+var body = document.body;
+
 function init() {
-  // your code here
+  if (i === codes.length - 1) {
+    window.alert("you did it!");
+    console.log("Booyah!")
+    i = 0;
+  }
 }
+
+body.addEventListener('keydown', function(e) {
+    if (e.key === codes[i]) {
+        console.log(`key ${i} has been pressed. ${codes[i]}`)
+        init();
+        i++;
+    } else {
+        i = 0;
+        console.log("back to square one");
+    }
+});
