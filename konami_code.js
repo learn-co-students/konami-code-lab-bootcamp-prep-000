e7c6dd6cd607e33cf5d37d92b9bc6816437042f8
Anshul Negi <anshul0217@gmail.com>
2019-03-22 14:29:21 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  var count = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[count]) {
+        count++;
+  } else {
+       count = 0;
+    }
+  if (count === codes.length) {
+         alert('Congratulations!');
+         count = 0;
+    }
+      }
+)
 }
