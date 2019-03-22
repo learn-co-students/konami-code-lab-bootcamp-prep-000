b26e1c0bbd19ec9cdb5f295fcbc75d3edd70d405
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-03-22 18:25:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,43 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+
+function init () {
+  // Initializing a count of commands done in the correct sequence
+  let index = 0;
+  
+  document.body.addEventListner('keydown', function (e) {
+    //Code below attempts to match the user input with the correct character sequence
+  
+    const key = e.key;
+    
+  // First if statement adds to the count on the condition that it's the correct character
+    
+    if (key === codes[index]) {
+      index ++;
+  
+  // Second if statement initializes an alert once the index matches the length of the codes array, i.e. the cheat code was entered fully and in the correct order
+      
+      if (index === codes.length) {
+        alert ('Contra Code');
+        
+  // Resets the count once the cheat code was executed      
+        index = 0;
+      }
+    } else {
+      
+  // Arbitrary statement to do nothing if there is no input    
+      
+      index = 0;
+    }
+  });
+  
+  
 }
+
+
+
+
+
+
+
