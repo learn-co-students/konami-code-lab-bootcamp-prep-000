90d5f97c7f1dfb4fb2f6cb9ca0f208207f270ca9
Ay Has <ahashim983@yahoo.com>
2019-03-24 18:40:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,31 @@ const codes = [
   "b",
   "a"
 ];
+var i = 1
+
+function checkSequence(codes,i){
+  m.addEventListener('keydown',function(e){
+    if (e.key == codes[i] && i < codes.length-1){
+      checkSequence(codes,i=i+1)
+    } else if (e.key == codes[i] && i == codes.length-1){
+      alert('Congratulations')
+    } else {
+      init()
+    }
+
+
+  })
+}
 
 function init() {
-  // your code here
+  const m = document.getElementsByTagName('body')
+
+  m.addEventListener('keydown',function(e) {
+    if (e.key == codes[0]){
+      checkSequence(codes,i)
+    }
+  })
+
 }
+
+puts init()
