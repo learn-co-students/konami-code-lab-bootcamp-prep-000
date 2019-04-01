d8e290f949eb3a62421a5729eb637ac4e28fa825
Dale Sublett <djsublett04@gmail.com>
2019-04-01 19:25:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,34 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
 let index = 0;
 function init() {
-  documnt.body.addEventListener("keydown", function(event){
-    const key=parseInt(event.which);
-    console.log(event.which);
-    if(key===code[index]){
-      index++;
-      if(index===code.length){
-        window.alert("Cheat activated, you cheater")
-      }
-    }
-    else{
-      index=0;
-    }
-  })
+document.body.addEventListener("keydown", (thingPressed) => {
+  const key = thingPressed.key 
+  console.log(key) 
+  
+  if (codes[index] == key) {
+    ++index 
+  } 
+  
+  else {
+    idx = 0 
+  }
+  
+  console.log(index)
+  if (index == 10) {
+    alert("Hurray!");
+    index = 0 
+  }
+}); 
 }
-init();
