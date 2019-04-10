2639bc92b0ff307b351a4aafac2d4d1289420166
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-10 02:54:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,20 @@ const codes = [
 ];
 
 function init() {
+  let index = 0;
   
+  document.body.addEventListener('keydown', function(e) {
+    let key = e.key;
+    
+    if(key === codes[index]) {
+      index++;
+      
+      if(index === codes.length) {
+        alert('YESSSSS!!!!');
+      }
+    }
+    else{
+      index = 0;
+    }
+  });
 }
