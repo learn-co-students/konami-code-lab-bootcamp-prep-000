784a87fb84ca66fc5da783bd0ab0941cd1c0b251
Ethan Collins <ethantcollins98@gmail.com>
2019-04-18 18:58:09 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-}
+  const body = document.body
+  let index = 0;
+  body.addEventListener('keydown', function(e){
+    if (e.key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert("Hooray!")
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
+  }
