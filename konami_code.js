810965ae8d8b4d4c69d490b9c14bb216fe66ca2a
Keith Burgie <krburgie@gmail.com>
2019-04-22 04:52:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,24 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+  "ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+
+    if (key === codes[index]) {
+      index++;
+      console.log(`(${index} of ${codes.length}) ${key}`);
+
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+
+    } else {
+      index = 0;
+    }
+  });
 }
