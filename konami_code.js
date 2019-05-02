4829160d0d094fb594f935f1f9a947e03731ee30
Huy do <huyddo40@gmail.com>
2019-05-02 03:42:53 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+let index = 0;
+function init(e) {
+  const key = e.key;
+  
+  if (key === codes[index]){
+    index++;
+    if (index === codes.length){
+      alert('Congratulation!')
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+const input = document.querySelector('mocha');
+input.addEventListener('keydown', init); 
+ 
