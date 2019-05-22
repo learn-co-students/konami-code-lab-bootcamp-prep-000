fe7a29ae97e95263dd80b44c66e60e3d43b80fd4
Eric Aniag <ericaniag@gmail.com>
2019-05-22 03:36:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  document.addEventListener('keydown',function(e) {
+    console.log(e.key)
+    let i = 0
+    if (e.key === codes[i]) {
+      i++
+      if (i === codes.length) {
+        alert("Congratulations")
+        i = 0
+        }
+    } else {
+      i = 0
+    }
+  })
 }
