b823f2ef69d702836c69f94b6aa0d2d39354a26a
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-24 02:52:39 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", onKeyDownHandler);
 }
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+  if(key === codes[index]) {
+    index++;
+  } else {
+    index = 0;
+  }
+  if(index === codes.length) {
+    alert("You are a true master!");
+    index = 0;
+  }
+}
+
+init();
