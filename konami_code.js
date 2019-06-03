3983fe6a1e3ffc41a646a36372e78612117053fc
Andrew Davisson <akdavisson4@gmail.com>
2019-06-03 00:44:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,23 @@ const codes = [
 
 function init() {
   // your code here
+
+  let i = 0;
+  
+  function konamiCode(e){
+    const key = e.key;
+    
+    if (key === codes[i]){
+      i++
+      if(i === codes.length){
+        alert('Congrats');
+        i = 0;
+      }
+    }else{
+      i = 0;
+    }
+  }
+  
+  document.body.addEventListener('keydown', konamiCode);
+  
 }
