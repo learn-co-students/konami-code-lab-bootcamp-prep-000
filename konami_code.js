7bb26c9d585234b908d9012a8f358bbfc66bf039
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-07 14:41:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,20 @@ const codes = [
   "b",
   "a"
 ];
+init();
+let index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler)
 
-function init() {
-  // your code here
-}
+function onKeyDownHandler(e){
+  const key = e.key;
+  if (key === codes[index]){
+    index++;
+
+    if (index === codes.length){
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+
