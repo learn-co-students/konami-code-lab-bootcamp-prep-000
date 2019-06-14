965bed42d1b405c427c19386f7d51c814365ceee
Danny Lee <dreamer@panix.com>
2019-06-14 15:57:28 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,29 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+function isThisIt(c) {
+  const key = c.key;
+
+  if (key === codes[index]) { 
+    index++;
+    log.textContent += index;
+    log.textContent += " " + c.key + " ";
+    if (index === codes.length) 
+    {
+        log.textContent += 'success!';
+        window.alert("Success!");
+        index = 0;
+    }
+  } 
+  else { 
+    index = 0;
+  }
+}
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', isThisIt);
 }
+
