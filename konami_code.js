7e4203a84f0c06d3bc5226a62894279fdcf44fc9
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-17 15:21:27 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,21 @@ const codes = [
 
 function init() {
   // your code here
+  const body = document.querySelector('body');
+  let index = 0;
+  body.addEventListener('keydown',function(e){
+    const key = e.key;
+    if(key === codes[index]){
+      index++;
+      
+      if(index === codes.length){
+        alert('Congratulations!!')
+        
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
