431dd011a14884f907f58077f4a014b701ca79bb
David Strauch <dmaster18@gmail.com>
2019-06-17 21:02:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+ document.body.addEventListener('keydown', onKeyDownHandler); 
 }
+
+let i = 0;
+function onKeyDownHandler(e) {
+  const key = e.key;
+  if(key === codes[i]){
+      i++;
+      if(i===codes.length)
+      {
+      alert("Congratulations! You typed the Konami Code");
+      i = 0;
+      }
+    }
+    else{ 
+      i = 0;
+  }
+}
+
