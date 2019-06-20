d028b3e01c7e1cc82e6ffd2b34d5ea1b3309fd16
Austin Trow <austintrow@gmail.com>
2019-06-20 14:01:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,22 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+let index = 0;
+
+function codeTest(e) {
+  if (e.key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert("What it do?");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+function init() {
+  document.body.addEventListener("keydown", codeTest);
+}
+
+init();
