bb069c50140418276a631bebe1c10076951e5d26
srikar kaliki <kalikisrikar@gmail.com>
2019-06-23 23:30:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,22 @@ const codes = [
   "b",
   "a"
 ];
-
 function init() {
-  // your code here
+var page = document.body;
+  var keysPressed = [];
+  page.addEventListener('keydown', function(event) {
+    keysPressed.push(event.key);
+    (keysPressed[0] === codes[0]) ? compare(keysPressed) : keysPressed.shift();
+  })
+
+ function compare(array) {
+  var result;
+  if (array.length === codes.length) {
+    for (var i = 0; i < codes.length;) {
+      array[i] === codes[i]
+      i++
+    }    
+    alert('Congrats!')
+  }
 }
+ }	
