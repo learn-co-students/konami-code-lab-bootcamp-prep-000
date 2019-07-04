02e9c1b045186a788a1e402b733067eff7360d2d
Richard Pitts <richardepitts@gmail.com>
2019-07-04 00:48:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+const code = [38, 38, 40, 40, 37, 39, 37 ,39, 66, 65];
+
+function init() {
+  var index = 0;
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+   function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    // console.log('key', key)
+    if (key === code[index]) {
+      index++;
+      console.log(index);
+       if (index === code.length) {
+        alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+}
+init();
