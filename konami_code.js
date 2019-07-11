ed6637459b9e6ac06aefad2869c1b917d64c255d
Clark Chen <yyunqingchenn@gmail.com>
2019-07-11 22:49:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,16 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init() { 
+  let idx = 0;  
+  
+  document.body.addEventListener("keydown", (e) => {     const key = e.key;  
+  
+  idx = (codes[idx] === key) ? ++idx : 0; 
+  
+  if (idx === codes.length) {  
+    window.alert("Hurray!");  
+    idx = 0
+  }
+  });
 }
