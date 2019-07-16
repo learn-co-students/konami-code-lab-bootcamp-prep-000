d386b0e2187cada9af7d4d311b71ddf5b9034be9
Mae Rine <mae.s.rine@gmail.com>
2019-07-16 20:25:08 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,29 @@ const codes = [
   "b",
   "a"
 ];
+ 
+
 
 function init() {
-  // your code here
+  
+  let index = 0;
+  
+  document.body.addEventListener('keydown', onKeyDownHandler);
+  
+  function onKeyDownHandler(e) {
+    const key = e.key;
+ 
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        alert("Hurray!");
+ 
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+  
 }
