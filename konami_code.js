785066ef57305e8a5112e1e4b6887208aa6890b5
John Plantamura <plantamurajk@gmail.com>
2019-07-21 21:07:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+const body = document.querySelector('body');
+
+let index = 0;
+
+function onKeyDownHandler(e){
+  const key = e.key;
+  
+  if (key === codes[index]){
+    index++;
+    
+    if (index === codes.length){
+      alert("Hurray!");
+      
+      index = 0;
+    }
+  } else {
+    index = 0
+  }
+}
+
+
 function init() {
-  // your code here
+body.addEventListener('keydown', onKeyDownHandler)
 }
