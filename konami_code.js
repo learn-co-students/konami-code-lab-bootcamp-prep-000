58479483c8411ecf8566ae64b05aff5843461fdc
Chris Lyons <chris@lyonsgroup.family>
2019-07-22 20:34:26 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
+  var keysPressed=[];
+  document.body.addEventListener("keydown", function(e) {
+    const key=e.key;
+    keysPressed.push(key);
+    var success=keysPressed.length == codes.length ? true : false;
+    for (var x=0;x < keysPressed.length;x++) {
+      if (keysPressed[x] != codes[x]) {
+        keysPressed.length=0;
+        success=false;
+      }
+    }
+    if (success) {
+      alert("You are now invincible!");
+    }
+  });
 }
