3629656fcdaf94594e4e7cd59af1a4f079e31628
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-12 17:22:02 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+   var index = 0
+document.body.addEventListener("keydown", (e) => {
+  if (codes[index] === e.key) {
+    index += 1
+  } 
+  else {
+    idx = 0 
+  }
+  if (index === 10) {
+    alert("Hurray!");
+    index = 0 
+  }
+})
 }
+
