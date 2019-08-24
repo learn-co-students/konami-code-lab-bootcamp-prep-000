7af2d24f8153fe9d09576ebf4f926c00e7215406
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-24 01:25:21 +0000

    Done.

--- README.md
+++ README.md
@@ -21,6 +21,28 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+let codeStore = []
+
+function keyPress(e){
+  const key = e.key;
+
+  if(key === codes[index]){
+    index++;
+    console.log(index, 'correct');
+
+    if(index.length === codes.length ) {
+      alert('Congratulations, you cracked the code!');
+      index = 0;
+    }
+  } else {
+    index = 0;
+    console.log(index, 'restart');
+  }
+
+}
+document.body.addEventListener('click', keyPress(e));
+
 function init() {
   // your code here
 }
--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,43 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+
+  let index = 0;
+  let codeStore = [];
+
+  function keyPress(e){
+    e.stopPropagation();
+
+    const key = e.key;
+    console.log(key);
+    if(key === codes[index]){
+
+
+      codeStore.push(key);
+
+      if(codeStore.length === codes.length){
+        console.log('codeStore Length', codeStore.length);
+      if(JSON.stringify(codeStore) === JSON.stringify(codes)) {
+          console.log('CONGRATS')
+            alert('Congratulations, you cracked the code!');
+
+            codeStore = [];
+            index = 0;
+
+        }
+      }
+      index++;
+
+    } else {
+      index = 0;
+      codeStore = [];
+      console.log(index, 'restart');
+
+    }
+
+  }
+  document.body.addEventListener('keydown', keyPress, true);
+
 }
