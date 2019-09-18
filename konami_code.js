42635fec4db099099749d2d46205d3dee48ba092
github username <github email address>
2019-09-18 07:26:50 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,27 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e){
+      const key = e.key;
+
+      if (key === codes[index]) {
+        index++;
+
+        if (index === codes.length) {
+          alert("Hurray!");
+
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+  })
+
 }
+//init()  // you want this in file to test out in browser, but it needs to be removed when running the tests
+
+// if the student has already run the tests, the tests will now likely interfere with the alert.
+// the student needs to comment out everything in test file but init(), and should remove the init() in their
+// own code file. may also need to refresh the page.
