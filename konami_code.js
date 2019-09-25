f585d331795e205b0e32f63753e91b8be0b57f2d
Mark Kauffman <markkauffman22@gmail.com>
2019-09-25 05:23:07 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ const codes = [
 function init() {
   
 }
-<<<<<<< HEAD
+
 /*
 You'll want to attach an event listener to document.body and check for 'keydown' events. If the user enters this special code, pressing all ten of the keys in the correct order, alert() a congratulatory message.
 */
