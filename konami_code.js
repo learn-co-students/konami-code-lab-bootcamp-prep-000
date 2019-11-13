6626908ea46f37acc943771d83ba7f1ad919f2a1
jonathan brierre <jonathanbrierre@yahoo.com>
2019-11-13 23:31:12 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,21 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+let index = 0;
+
+function init(e) {
+const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+document.body.addEventListener('keydown',init)
