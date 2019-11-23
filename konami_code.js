87a3533c4a7aefd139882d79aa9f5e3d26fc0f86
Liam Argent <liargent@hotmail.com>
2019-11-23 13:19:11 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  
+  let i = 0;
+  let body = document.body;
+  
+  body.addEventListener("keydown", (event) => {
+    if (event.key === codes[i]) {
+      i++; 
+      if (i === codes.length) {
+        alert("RIP Konami!"); 
+      }
+    }
+    else {
+      i = 0;
+    }
+  })
 }
