a7cfc7cc4e839b2446e2ae4d7f021fd1a405c7ac
Richard Vernon Dyson <dyson77z@gmail.com>
2020-02-01 05:33:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,25 @@ const codes = [
 
 function init() {
   // your code here
+var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
 }
+
+
+
+init();
