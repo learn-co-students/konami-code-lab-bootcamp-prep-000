dc0e85d056eac15504abc8d3d5326e1c69fc4cc1
comical-tag-7436 <shawtynow2008@gmail.com>
2020-02-02 04:14:17 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,25 @@ const codes = [
 function init() {
   // your code here
 }
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+function init() {
+  // your code here
+  var konamiCode = 0;
+//added event listener
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which)
+
+    if (key === code[konamiCode]) {
+      konamiCode++
+
+      if (konamiCode === code.length -1) {
+        alert('Yeyi it worked!')
+
+        konamiCode = 0
+      }
+    } else {
+      konamiCode = 0
+    }
+  })
+}
