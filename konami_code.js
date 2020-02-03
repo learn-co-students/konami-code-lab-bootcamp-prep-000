c829363d2d4d8eada56d34629543d2887a534aa4
Tomas Perez <tomasitopg6@gmail.com>
2020-02-03 20:34:33 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,29 @@ const codes = [
   "a"
 ];
 
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
 function init() {
-  // your code here
-}
+   var index = 0;
+   var userEntry = []
+   
+   function compare2Arrays(array1, array2){
+     if(array1.length !== array2.length){
+       return false
+     }
+     for(var i = 0; i < array1.length; i++){
+       if(array1[i] !== array2[i]){
+         return false
+       }
+     }
+     return true
+   }
+
+document.body.addEventListener('keydown', function (e) {
+     const key = e.key;      // parseInt(e.detail || e.which);
+      userEntry.push(key)
+     if(compare2Arrays(userEntry,codes)){
+       alert('YOU DID IT!')
+     }
+   });
+ }		 
