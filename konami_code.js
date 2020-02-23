1136f49614e8267594f67826931778cfee98dc5d
Dave Patel <ddp2134@gmail.com>
2020-02-23 16:43:23 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -14,3 +14,25 @@ const codes = [
 function init() {
   // your code here
 }
+
+
+let index = 0;
+
+document.body.addEventListener("keydown", (thingPressed) => {
+  const key = thingPressed.key 
+  console.log(key) 
+  
+  if (codes[index] == key) {
+    ++index 
+  } 
+  
+  else {
+    idx = 0 
+  }
+  
+  console.log(index)
+  if (index == 10) {
+    alert("Hurray!");
+    index = 0 
+  }
+}); 
