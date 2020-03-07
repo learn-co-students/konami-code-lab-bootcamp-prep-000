b4dae8f5d6007066750269c78b72ceb5fede7959
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-03-06 19:23:21 -0800

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,49 +1,49 @@
-// const expect = chai.expect;
+const expect = chai.expect;
 
-// describe("index.js", () => {
+describe("index.js", () => {
 
-//   const codes = [
-//     "ArrowUp",
-//     "ArrowUp",
-//     "ArrowDown",
-//     "ArrowDown",
-//     "ArrowLeft",
-//     "ArrowRight",
-//     "ArrowLeft",
-//     "ArrowRight",
-//     "b",
-//     "a"
-//   ];
+  const codes = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
+  ];
 
-//   function triggerKeyDown(key) {
-//     const keyboardEvent = new KeyboardEvent("keydown", { key });
-//     document.body.dispatchEvent(keyboardEvent);
-//   }
+  function triggerKeyDown(key) {
+    const keyboardEvent = new KeyboardEvent("keydown", { key });
+    document.body.dispatchEvent(keyboardEvent);
+  }
 
-//   init()
+  init()
 
-//   describe("Konami code", () => {
+  describe("Konami code", () => {
 
-//     const spy = sinon.stub(window, "alert");
+    const spy = sinon.stub(window, "alert");
 
-//     it("triggers an alert if the right code is entered", () => {
+    it("triggers an alert if the right code is entered", () => {
 
-//       for (let i = 0, l = codes.length; i < l; i++) {
-//         triggerKeyDown(codes[i]);
-//       }
-//       expect(spy.called).to.equal(true)
-//       expect(spy.callCount).to.equal(1)
-//     });
+      for (let i = 0, l = codes.length; i < l; i++) {
+        triggerKeyDown(codes[i]);
+      }
+      expect(spy.called).to.equal(true)
+      expect(spy.callCount).to.equal(1)
+    });
 
-//     it("does not trigger an alert if the wrong code is entered", () => {
-//       spy.reset()
+    it("does not trigger an alert if the wrong code is entered", () => {
+      spy.reset()
 
-//       for (let i = 0; i < codes.length - 1; i++) {
-//         triggerKeyDown(codes[i])
-//       }
-//       triggerKeyDown("ArrowUp")
+      for (let i = 0; i < codes.length - 1; i++) {
+        triggerKeyDown(codes[i])
+      }
+      triggerKeyDown("ArrowUp")
 
-//       expect(spy.notCalled).to.equal(true);
-//     });
-//   });
-// });
+      expect(spy.notCalled).to.equal(true);
+    });
+  });
+});
