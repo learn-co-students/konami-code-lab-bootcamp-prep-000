cac22712f58b0e50338ea36ceb9f7ac57e1479c0
maxwellbenton <maxwellbenton@gmail.com>
2020-04-08 12:18:06 -0400

    update tests for mocha-jsdom / sinon

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,5 @@
-const expect = chai.expect;
+require('./helpers.js')
+const sinon = require('sinon')
 
 describe("index.js", () => {
 
@@ -20,30 +21,45 @@ describe("index.js", () => {
     document.body.dispatchEvent(keyboardEvent);
   }
   
-  init()
-
   describe("Konami code", () => {
     
-    const spy = sinon.stub(window, "alert");
-    
+
     it("triggers an alert if the right code is entered", () => {
-      
-      for (let i = 0, l = codes.length; i < l; i++) {
-        triggerKeyDown(codes[i]);
-      }
-      expect(spy.called).to.equal(true)
-      expect(spy.callCount).to.equal(1)
+      var savedAlert = window.alert
+
+      try {
+        var spy = sinon.spy(window, 'alert');
+        init()
+
+        for (let i = 0, l = codes.length; i < l; i++) {
+          triggerKeyDown(codes[i]);
+        }
+
+        expect(spy.called).to.equal(true);
+        expect(spy.callCount).to.equal(1);
+       } 
+    
+      finally { window.alert = savedAlert; }
     });
 
     it("does not trigger an alert if the wrong code is entered", () => {
-      spy.reset()
+      var savedAlert = window.alert
 
-      for (let i = 0; i < codes.length - 1; i++) {
-        triggerKeyDown(codes[i])
-      }
-      triggerKeyDown("ArrowUp")
+      try {
+        var spy = sinon.spy(window, 'alert');
+        init()
+
+        for (let i = 0; i < codes.length - 1; i++) {
+          triggerKeyDown(codes[i])
+        }
+        triggerKeyDown("ArrowUp")
+
+        expect(spy.notCalled).to.equal(true);
+       } 
+       
       
-      expect(spy.notCalled).to.equal(true);
+      finally { window.alert = savedAlert; }
+
     });
   });
 });
