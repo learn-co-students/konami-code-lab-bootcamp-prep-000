c82db34205ca1f704c4c643ae35cbbed739254ee
maxwellbenton <maxwellbenton@gmail.com>
2020-04-08 12:17:50 -0400

    add jsdom helpers setup

---
+++ test/helpers.js
@@ -0,0 +1,22 @@
+const chai = require('chai')
+global.expect = chai.expect
+const fs = require('file-system')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require('babel-core');
+
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+
+const babelResult = babel.transformFileSync(
+  path.resolve(__dirname, '..', 'konami_code.js'), {
+    presets: ['env']
+  }
+);
+
+const src = babelResult.code
+
+jsdom({
+  html, 
+  src,
+  url: "http://localhost"
+});
