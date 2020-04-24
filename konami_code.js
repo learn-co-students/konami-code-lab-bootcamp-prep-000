8aa27398fbf795d23b56de68e4626a73690fced3
Erin Shields <elshields92@gmail.com>
2020-04-24 18:35:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const codes = [
   "ArrowUp",
   "ArrowUp",
   "ArrowDown",
-  "ArrowDown",
+ "ArrowDown",
   "ArrowLeft",
   "ArrowRight",
   "ArrowLeft",
@@ -10,7 +10,18 @@ const codes = [
   "b",
   "a"
 ];
+let index = 0
 
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", function(e) {
+    if (e.key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert("Hurray");
+        index = 0;
+      }
+    } else {
+        index = 0;
+    }
+  })
 }
