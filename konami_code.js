fd7865ae2835b5e0226d1cc388a402895620d92d
Rene Santana <renesantana10@gmail.com>
2020-05-05 19:12:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  
+  document.body.addEventListener("keydown", (event) => {
+  
+  const key = event.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  } 
+  });
 }
+
+
