d22864f0ccb0e0a7824e65691328c16fa49de3ae
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 06:01:20 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', handleEvent)
 }
+
+var index = 0
+handleEvent = (event) => {
+  const key = event.key
+  
+  if (key === codes[index]) {
+    index++
+    if (index == codes.length) {
+      alert("Hurray")
+      index = 0
+    }
+  }else {index = 0}
+}
+
+
+init()
