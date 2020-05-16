6d88f45c7a01a5cc651c29a167fe9ff0d0feb85a
Jastien Sandhu <superjastin@gmail.com>
2020-05-16 01:42:05 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -17,10 +17,10 @@ const codes = [
 function init(){
   let index = 0
   //Initial index
-  document.body.addEventListener("keydown", onKeyDownHandler);
+  
   //adds an event regarding keydown presses
   
-  function onKeyDownHandler(e){
+  document.body.addEventListener("keydown", (e) => {
     const key = e.key;
     //sets a const variable to the key presses
     //This if statements compares the presses with the indexes of the code list to see if it fits, then if it does , increments, and if it hits the end of the list, sends the alert out, and resets back to zero
@@ -36,7 +36,7 @@ function init(){
       
     }
     console.log(key);
-  }
+  });
 
 }
 
