fd9dce46a07a1291e18b2207a58a7b6f284cceb1
Catherine Straus <vnair611@gmail.com>
2020-06-28 00:42:35 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+let i= 0
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", (event) =>{
+      if (event.key === codes[i]) {
+	    i++
+    	
+    	if (i === codes.length) {
+    	  alert ('Hurray');
+    	  
+    	  i = 0
+    } else {
+      return;
+    } 
+  }
+})
 }
