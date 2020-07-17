9685e37e49f470498e7eff1e6340fc1ffacb8ad9
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-17 03:48:55 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,23 @@ const codes = [
 
 function init() {
   // your code here
+
+  var index = 0;
+  document.body.addEventListener("keydown", function oneKeyDownHandler(e) {
+    const key = e.key;
+    if(key===codes[index]){
+      index++;
+    
+      if(index === codes.length){
+        alert("Hurray!");
+      }
+    }
+    else{
+      index = 0;
+    }
+  })
+  
+
 }
+
+
