6fb638fdd7e2de266694d2e4fc7cf7260ee6046e
christian <cschmidt@jahnelgroup.com>
2020-09-05 23:18:42 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,14 @@ const codes = [
   "a"
 ];
 
+window.pressed = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", (event) => {
+    let key = event.key;
+    console.log(key);
+    if (key === codes[window.pressed]) window.pressed++;
+    else window.pressed = 0;
+    if (window.pressed === codes.length) alert("Hurray");
+  });
 }
