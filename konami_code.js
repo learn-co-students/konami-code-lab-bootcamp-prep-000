5ab0cc3a2373edd61567f43fc540e33ab66c959f
mattbmoorin <mattbmoorin@gmail.com>
2020-10-09 13:18:37 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,33 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a"
 ];
 
+
 function init() {
-  // your code here
+    let index = 0;
+    document.body.addEventListener("keydown", function(e) {
+        // function onKeyDownHandler(e) {
+        const key = e.key;
+
+        if (key === codes[index]) {
+            index++;
+            if (index === codes.length) {
+                alert("Hurray!");
+
+                index = 0;
+            }
+        } else {
+            index = 0;
+        }
+        // }
+    })
 }
