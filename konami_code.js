85421b6fd451dc5a614314647990d694b3bb4715
Rachid Abu-Hassan <rachid@platform78.com>
2020-11-02 07:55:59 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,17 @@ const codes = [
   "a"
 ];
 
+let index = 0
 function init() {
-  // your code here
+  document.body.addEventListener("keydown", e => {
+    if (codes[index] === e.key) {
+      index++
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+      } else {
+        index = 0
+      }
+  })
 }
