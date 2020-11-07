8597a03746f0022a8b4fb37791fb882c1f18e5ae
William Damm <wdamm@powerhrg.com>
2020-11-07 03:04:18 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,27 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+
+let test_c = [];
+
+document.body.addEventListener('keydown',function(e) {
+test_c.push(e.key);
+
+const index = test_c.slice(test_c.length-10,test_c.length);
+
+let test_1 = codes.toString();
+let test_2 = index.toString();
+if (test_1 === test_2) {
+  alert('Hurray');
 }
+});
+
+}
+
+
+
+
+
