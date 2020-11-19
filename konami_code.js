80290621ac5a1e1690061d7c672e49c858f9b4f1
Dom Siravo <domcode88@gmail.com>
2020-11-19 23:03:37 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,28 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+  
+ let index = 0;
+  document.body.addEventListener("keydown", (event) => {
+    
+    const key = event.key;
+    console.log('key', key);
+    
+    if (key === codes[index]) {
+      index++;
+      
+        if (index === codes.length){
+          alert("Hurray");
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+      console.log(index);
+    
+    }
+    
+  );
 }
+init();
