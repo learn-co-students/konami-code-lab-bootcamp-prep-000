de28fcd4bc97e62f71fbc47dbb6e4fde58b5dde6
Nahomie ND <nducasse3@gmail.com>
2020-12-06 02:39:06 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,49 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-  // Keep track of index outside of the event handler.
+
   let index = 0;
-  const body = document.getElementsByTagName('body')[0];
 
-  body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.detail || e.which);
-    console.log(key);
-
-  if (key === code[index]) {
-      index++;
-
-      if (index === code.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
+ document.body.addEventListener('keydown', function(e) {
+  const key = parseInt(e.detail || e.which);
+  console.log(key);
+  
+  if (key === code[index]) {    
+    index++;
+    if (index === code.length) {
+      alert("Hurray!");
       index = 0;
-    }
-  });
+      }
+    } else {     
+      index = 0;
+      }
+    });
+  
 }
+
+
+//   const body = document.getElementsByTagName('body')[0];
+  
+//   body.addEventListener('keydown', function(e) {
+//     console.log(e.which);
+    
+//     if (e.detail === code[i]) {
+      
+    
+//         i++;
+//         console.log('Talk to me nice');
+        
+//       if(i === code.length){
+ 
+//         alert("Hurray!");
+        
+//           i=0;
+          
+//         }
+//       }
+   
+//       else {
+//           i=0; 
+//     }
+//   });
+// }
