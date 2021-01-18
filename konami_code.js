1eb670fbf0fa988a4f55be319a406e0b8be3ba5d
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-18 20:26:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-}
+  // your code here	  var index = 0
+  document.body.addEventListener('keydown', function(e) {
+    var key = e.key
+    if (key === codes[index]) {
+      console.log("Key logged:" + key);
+      ++index
+      if (index === codes.length) {
+        alert("Hurray!!! You found the EasterEgg!")
+      }
+    } else {
+      index = 0
+    }
+  });
+}	}
